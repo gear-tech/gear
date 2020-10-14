68188e14e16ed94c9118054a552f225b3d25dc98
NikVolf <nikvolf@gmail.com>
2020-10-14 18:48:58 +0300

    further messaging

--- demo/src/main.rs
+++ demo/src/main.rs
@@ -1,16 +1,36 @@
 
 mod msg {
-    extern "C" {
-        pub fn send(program: i64, data_ptr: *const u8, data_len: usize);
-        pub fn size() -> i32;
-        pub fn read(at: usize, len: usize, dest: *mut u8);
+    mod sys {
+        extern "C" {
+            pub fn send(program: i64, data_ptr: *const u8, data_len: usize);
+            pub fn size() -> usize;
+            pub fn read(at: usize, len: usize, dest: *mut u8);
+        }
+    }
+
+    pub fn load() -> Vec<u8> {
+        unsafe {
+            let message_size = sys::size();
+            let mut data = Vec::with_capacity(message_size);
+            data.set_len(message_size);
+            sys::read(0, message_size, data.as_mut_ptr() as _);
+            data
+        }
+    }
+
+    pub fn send(program: u64, payload: &[u8]) {
+        unsafe {
+            sys::send(program as _, payload.as_ptr(), payload.len())
+        }
     }
 }
 
 
 #[no_mangle]
 pub unsafe extern "C" fn handle() {
-    assert_eq!(msg::size(), 0)
+    assert_eq!(msg::load().len(), 0);
+
+    msg::send(0, &[0u8]);
 }
 
 fn main() {
