ae1cc5144e3ff43885538250636df889431cc759
NikVolf <nikvolf@gmail.com>
2020-11-17 18:26:50 +0300

    add save/load

--- saver.rs
+++ saver.rs
@@ -1,10 +1,12 @@
+use std::path::Path;
+
 use codec::{Encode, Decode};
 
 use crate::message::Message;
 use crate::program::{Program, ProgramId};
 use crate::memory::PageNumber;
 
-#[derive(Decode, Encode, Clone, Debug)]
+#[derive(Decode, Default, Encode, Clone, Debug)]
 struct State {
     programs: Vec<Program>,
     queued_messages: Vec<Message>,
@@ -12,3 +14,15 @@ struct State {
     allocations: Vec<(PageNumber, ProgramId)>,
 }
 
+fn load_from_file<P: AsRef<Path>>(path: P) -> State {
+    std::fs::read(path).map(|bytes| {
+        State::decode(&mut &bytes[..]).expect("Failed to decode binary")
+    }).unwrap_or_default()
+}
+
+fn save_to_file<P: AsRef<Path>>(path: P, state: &State) {
+    let mut bytes = vec![];
+    state.encode_to(&mut bytes);
+
+    std::fs::write(path, bytes).expect("Failed to write data");
+}
