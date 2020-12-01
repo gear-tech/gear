d60101ba017aa99e7d12a48488a05845e035029e
NikVolf <nikvolf@gmail.com>
2020-12-01 14:34:44 +0300

    use clap

--- demo/src/main.rs
+++ demo/src/main.rs
@@ -5,8 +5,18 @@ static mut MESSAGE_LOG: Vec<String> = vec![];
 #[no_mangle]
 pub unsafe extern "C" fn handle() {
     let new_msg = String::from_utf8(msg::load()).expect("Invalid message: should be utf-8");
+
+    if &new_msg == "PING" {
+        msg::send(msg::source(), b"PONG");
+    }
+
     MESSAGE_LOG.push(new_msg);
-    ext::debug(&format!("{:?} total message(s) stored.", MESSAGE_LOG.len()));
+
+    ext::debug(&format!("{:?} total message(s) stored: ", MESSAGE_LOG.len()));
+
+    for log in MESSAGE_LOG.iter() {
+        ext::debug(log);
+    }
 }
 
 fn main() {
