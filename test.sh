6cfbd9f8816e061166433f704ba9602d9914531d
NikVolf <nikvolf@gmail.com>
2021-01-18 17:53:47 +0300

    fix build

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-cargo +nightly build --manifest-path=demo/Cargo.toml --target=wasm32-unknown-unknown --release
+cargo +nightly build --manifest-path=demo-ping/Cargo.toml --target=wasm32-unknown-unknown --release
 cargo run -- 1 ./demo/target/wasm32-unknown-unknown/release/demo1.wasm
