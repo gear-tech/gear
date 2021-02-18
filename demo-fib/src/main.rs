1aa5dfa794db141efff995be7523c7724c304038
Vadim <ukintchanel@gmail.com>
2021-02-18 16:57:23 +0300

    Add demo-fib

---
+++ demo-fib/src/main.rs
@@ -0,0 +1,40 @@
+#![feature(num_as_ne_bytes)]
+
+use gstd::{ext, msg};
+use std::convert::TryInto;
+
+static mut MESSAGE_LOG: Vec<String> = vec![];
+
+fn make_fib(n: usize) -> Vec<i32> {
+    let mut x = vec![1, 1];
+    for i in 2..n {
+        let next_x = x[i - 1] + x[i - 2];
+        x.push(next_x)
+    }
+    x
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn handle() {
+    let new_msg = i32::from_ne_bytes(msg::load().try_into().expect("Should be i32"));
+    MESSAGE_LOG.push(format!("New msg: {:?}", new_msg));
+
+    msg::send(
+        msg::source(),
+        make_fib(new_msg as usize)[new_msg as usize - 1].as_ne_bytes(),
+    );
+
+    ext::debug(&format!(
+        "{:?} total message(s) stored: ",
+        MESSAGE_LOG.len()
+    ));
+
+    for log in MESSAGE_LOG.iter() {
+        ext::debug(log);
+    }
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn init() {}
+
+fn main() {}
