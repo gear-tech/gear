966d577293cfdffa3fc9b0570d5bc37099e5b49e
Vadim <ukintchanel@gmail.com>
2021-03-08 16:18:51 +0300

    Print hex string

--- demo-chat/src/encode.rs
+++ demo-chat/src/encode.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::fmt::Write;
 
 mod shared;
 
@@ -13,25 +14,47 @@ fn main() {
     }
     match args[1].as_str() {
         "room" => match args[2].as_str() {
-            "join" => println!(
-                "{:?}",
-                RoomMessage::Join {
-                    under_name: args[3].to_string()
+            "join" => {
+                let out = RoomMessage::Join {
+                    under_name: args[3].to_string(),
                 }
-                .encode()
-            ),
-            "yell" => println!(
-                "{:?}",
-                RoomMessage::Yell {
-                    text: args[3].to_string()
+                .encode();
+                let mut s = String::from("0x");
+                for byte in out {
+                    write!(s, "{:02x}", byte);
                 }
-                .encode()
-            ),
+                println!("{:?}", s);
+            }
+            "yell" => {
+                let out = RoomMessage::Yell {
+                    text: args[3].to_string(),
+                }
+                .encode();
+                let mut s = String::from("0x");
+                for byte in out {
+                    write!(s, "{:02x}", byte);
+                }
+                println!("{:?}", s);
+            }
             _ => (),
         },
         "member" => match args[2].as_str() {
-            "private" => println!("{:?}", MemberMessage::Private(args[3].to_string()).encode()),
-            "room" => println!("{:?}", MemberMessage::Room(args[3].to_string()).encode()),
+            "private" => {
+                let out = MemberMessage::Private(args[3].to_string()).encode();
+                let mut s = String::from("0x");
+                for byte in out {
+                    write!(s, "{:02x}", byte);
+                }
+                println!("{:?}", s);
+            }
+            "room" => {
+                let out = MemberMessage::Room(args[3].to_string()).encode();
+                let mut s = String::from("0x");
+                for byte in out {
+                    write!(s, "{:02x}", byte);
+                }
+                println!("{:?}", s);
+            }
             _ => (),
         },
         _ => (),
