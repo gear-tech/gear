54c63fa5f34ffaca922eb0f27db563a25e5503c4
Vadim <ukintchanel@gmail.com>
2021-03-10 13:50:01 +0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
-cargo +nightly build --manifest-path=demo-ping/Cargo.toml --target=wasm32-unknown-unknown --release
-cargo run -- 1 ./demo/target/wasm32-unknown-unknown/release/demo1.wasm
+cargo +nightly build --manifest-path=demo-ping/Cargo.toml --target=wasm32-unknown-unknown --release &&
+cargo +nightly build --manifest-path=demo-fib/Cargo.toml --target=wasm32-unknown-unknown --release &&
+cargo +nightly build --manifest-path=demo-sum/Cargo.toml --target=wasm32-unknown-unknown --release &&
+cargo +nightly build --manifest-path=demo-vec/Cargo.toml --target=wasm32-unknown-unknown --release &&
+cargo +nightly build --manifest-path=demo-chat/Cargo.toml --target=wasm32-unknown-unknown --release
+cargo run sample/*.json
