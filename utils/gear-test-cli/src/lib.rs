d79c895818054fe00d9b30b60725b0ee18d49d75
Vadim <ukintchanel@gmail.com>
2021-04-01 15:02:29 +0300

    Update description

--- utils/gear-test-cli/src/lib.rs
+++ utils/gear-test-cli/src/lib.rs
@@ -3,13 +3,13 @@ use std::fmt::Debug;
 mod command;
 mod test_runner;
 
-/// The `benchmark` command used to benchmark FRAME Pallets.
+/// The `runtests` command used to test gear with json.
 #[derive(Debug, structopt::StructOpt)]
 pub struct GearTestCmd {
-	/// Input dir/file with json for testing.
-	pub input: Option<std::path::PathBuf>,
-	
-	#[allow(missing_docs)]
-	#[structopt(flatten)]
-	pub shared_params: sc_cli::SharedParams,
+    /// Input dir/file with json for testing.
+    pub input: Option<std::path::PathBuf>,
+
+    #[allow(missing_docs)]
+    #[structopt(flatten)]
+    pub shared_params: sc_cli::SharedParams,
 }
