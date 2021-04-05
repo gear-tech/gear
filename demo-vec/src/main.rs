aa2f25af4b3e8b1a34d65e7c037b9080f81826f7
Vadim <ukintchanel@gmail.com>
2021-04-05 14:04:56 +0300

    Update demos

--- demo-vec/src/main.rs
+++ demo-vec/src/main.rs
@@ -17,7 +17,7 @@ pub unsafe extern "C" fn handle() {
         &v[new_msg as usize - 1],
         v[new_msg as usize - 1]
     ));
-    msg::send(msg::source(), (v.len() as i32).as_ne_bytes());
+    msg::send(msg::source(), (v.len() as i32).as_ne_bytes(), u64::MAX);
     ext::debug(&format!(
         "{:?} total message(s) stored: ",
         MESSAGE_LOG.len()
