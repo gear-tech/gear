5ef56ed27b4192ac8ebdd7dbf61ec37f1c9bd084
Vadim Smirnov <ukintchanel@gmail.com>
2021-04-12 19:33:04 +0300

    remove decode_hex()

--- src/runner.rs
+++ src/runner.rs
@@ -16,13 +16,6 @@ use regex::Regex;
 type InMemoryRunner =
     Runner<InMemoryAllocationStorage, InMemoryMessageQueue, InMemoryProgramStorage>;
 
-fn decode_hex(s: &str) -> Result<Vec<u8>, ParseIntError> {
-    (0..s.len())
-        .step_by(2)
-        .map(|i| u8::from_str_radix(&s[i..i + 2], 16))
-        .collect()
-}
-
 fn encode_hex(bytes: &[u8]) -> String {
     let mut s = String::with_capacity(bytes.len() * 2);
     for &b in bytes {
