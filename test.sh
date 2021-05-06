ae2e3824a0b8666d66eafc5cbd6ea474be701569
Nikolay Volf <nikvolf@gmail.com>
2021-05-06 19:41:18 +0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-cargo +nightly build --manifest-path=demo-ping/Cargo.toml --target=wasm32-unknown-unknown --release &&
-cargo +nightly build --manifest-path=demo-fib/Cargo.toml --target=wasm32-unknown-unknown --release &&
-cargo +nightly build --manifest-path=demo-sum/Cargo.toml --target=wasm32-unknown-unknown --release &&
-cargo +nightly build --manifest-path=demo-vec/Cargo.toml --target=wasm32-unknown-unknown --release &&
+cargo +nightly build --manifest-path=demo-ping/Cargo.toml --target=wasm32-unknown-unknown --release
+cargo +nightly build --manifest-path=demo-fib/Cargo.toml --target=wasm32-unknown-unknown --release
+cargo +nightly build --manifest-path=demo-sum/Cargo.toml --target=wasm32-unknown-unknown --release
+cargo +nightly build --manifest-path=demo-vec/Cargo.toml --target=wasm32-unknown-unknown --release
 cargo +nightly build --manifest-path=demo-chat/Cargo.toml --target=wasm32-unknown-unknown --release
 cargo +nightly build --manifest-path=demo-condensator/Cargo.toml --target=wasm32-unknown-unknown --release
 cargo run json/*.json
