1e7fdad72ce9f02a36c81900f7ec2eae5ac19adb
NikVolf <nikvolf@gmail.com>
2021-05-14 17:15:48 +0400

    remove unused

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-cargo +nightly build --release --workspace --exclude=test-gear --target=wasm32-unknown-unknown
+cargo +nightly build --release --workspace --exclude=test-gear --target=wasm32-unknown-unknown --verbose
 cargo run --release -- json/*.json
