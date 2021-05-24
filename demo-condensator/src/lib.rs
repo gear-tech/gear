1a3d3baeaa72f3bd7a19425e96960aa63899e76a
Peter Volynsky <peter@approvalmax.com>
2021-05-24 18:59:35 +0300

    add test for demo condensator

--- demo-condensator/src/lib.rs
+++ demo-condensator/src/lib.rs
@@ -4,11 +4,12 @@
 #[macro_use]
 extern crate alloc;
 
+
 use alloc::string::String;
 use alloc::vec::Vec;
-
-use gstd::msg;
 use alloc::str::FromStr;
+
+use gstd::{ext, msg};
 
 // Begin of demo
 static mut CHARGE: u32 = 0;
@@ -19,13 +20,28 @@ static mut DISCHARGE_HISTORY: Vec<u32> = Vec::new();
 
 #[no_mangle]
 pub unsafe extern "C" fn handle() {
+
     let new_msg = String::from_utf8(msg::load()).expect("Invalid message: should be utf-8");
 
-    let to_add = u32::from_str(&new_msg).expect("Invalid number");
+    let to_add = u32::from_str(new_msg.as_ref()).expect("Invalid number");
 
     CHARGE += to_add;
 
+    
+    ext::debug(&format!(
+        "Charage condensator with {}, new charge {}",
+        to_add,
+        CHARGE
+    ));
+
     if CHARGE >= LIMIT {
+
+        ext::debug(&format!(
+            "Discharge #{} due to limit {}",
+            CHARGE,
+            LIMIT
+        ));
+
         msg::send(0.into(), format!("Discharged: {}", CHARGE).as_bytes(), 1000000000, 0);
         DISCHARGE_HISTORY.push(CHARGE);
         CHARGE = 0;
@@ -35,12 +51,21 @@ pub unsafe extern "C" fn handle() {
 
 #[no_mangle]
 pub unsafe extern "C" fn init() {
+    let initstr = String::from_utf8(msg::load()).expect("Invalid message: should be utf-8");
     let limit =
         u32::from_str(
-            String::from_utf8(msg::load()).expect("Invalid message: should be utf-8").as_ref()
+            initstr.as_ref()
         ).expect("Invalid number");
 
+        
     LIMIT = limit;
+
+    ext::debug(&format!(
+        "Init condensator with limit capacity {}, {}",
+        LIMIT,
+        initstr
+    ));
+
 }
 
 #[panic_handler]
