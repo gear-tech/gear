7161d81ddd3e6dfb20511acabfc856b0d8e03e52
Vadim <ukintchanel@gmail.com>
2021-06-01 18:44:18 +0300

    explicitly set custom types

--- rpc-tests/index.js
+++ rpc-tests/index.js
@@ -346,6 +346,28 @@ async function main() {
         value: 'Message',
         next: 'Option<H256>',
       },
+      IntermediateMessage: {
+        _enum: {
+          InitProgram: {
+            external_origin: 'H256',
+            program_id: 'H256',
+            code: 'Vec<u8>',
+            payload: 'Vec<u8>',
+            gas_limit: 'u64',
+            value: 'u128',
+          },
+          DispatchMessage: {
+            id: 'H256',
+            route: 'MessageRoute',
+            payload: 'Vec<u8>',
+            gas_limit: 'u64',
+            value: 'u128',
+          },
+        },
+      },
+      MessageError: {
+        _enum: ['ValueTransfer', 'Dispatch'],
+      },
     },
   });
 
