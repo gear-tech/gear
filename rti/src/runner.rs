4459b6d5db93bb6f18862e1bcd4cc59fbf9d9617
Vadim Smirnov <ukintchanel@gmail.com>
2021-07-05 16:06:46 +0300

    Use gear-runner in rti

--- rti/src/runner.rs
+++ rti/src/runner.rs
@@ -16,10 +16,8 @@
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-use gear_core::{
-    runner::{Config, Runner},
-    storage::Storage,
-};
+use gear_core::storage::Storage;
+use gear_runner::runner::{Config, Runner};
 
 use crate::ext::*;
 
