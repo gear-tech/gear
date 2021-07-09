67a8183f304ab215021c9179882a1a9f77cdda2c
Nikolay Volf <nikvolf@gmail.com>
2021-07-09 19:56:13 +0300

    add reply and message id ffi (#18)

--- src/lib.rs
+++ src/lib.rs
@@ -34,6 +34,23 @@ impl ProgramId {
     }
 }
 
+#[derive(Clone, Copy, Debug, Default, Hash, PartialEq, Eq)]
+pub struct MessageId([u8; 32]);
+
+impl MessageId {
+    pub fn from_slice(s: &[u8]) -> Self {
+        assert_eq!(s.len(), 32);
+        let mut id = Self([0u8; 32]);
+        id.0[..].copy_from_slice(s);
+        id
+    }
+
+    pub fn as_slice(&self) -> &[u8] {
+        &self.0[..]
+    }
+}
+
+
 #[macro_export]
 macro_rules! bail {
     ($some:expr, $expl:expr, $fmt:expr, $($arg:tt)*) => {
@@ -50,7 +67,7 @@ macro_rules! bail {
 }
 
 pub mod msg {
-    use super::ProgramId;
+    use super::{ProgramId, MessageId};
     use alloc::vec::Vec;
 
     mod sys {
@@ -66,6 +83,13 @@ pub mod msg {
             pub fn gr_read(at: u32, len: u32, dest: *mut u8);
             pub fn gr_source(program: *mut u8);
             pub fn gr_value(val: *mut u8);
+            pub fn gr_msg_id(val: *mut u8);
+            pub fn gr_reply(
+                data_ptr: *const u8,
+                data_len: u32,
+                gas_limit: u64,
+                value_ptr: *const u8,
+            );
         }
     }
 
@@ -97,12 +121,29 @@ pub mod msg {
         program_id
     }
 
+    pub fn id() -> MessageId {
+        let mut msg_id = MessageId::default();
+        unsafe { sys::gr_msg_id(msg_id.0.as_mut_ptr()) }
+        msg_id
+    }
+
     pub fn value() -> u128 {
         let mut value_data = [0u8; 16];
         unsafe {
             sys::gr_value(value_data.as_mut_ptr());
         }
         u128::from_le_bytes(value_data)
+    }
+
+    pub fn reply(payload: &[u8], gas_limit: u64, value: u128) {
+        unsafe {
+            sys::gr_reply(
+                payload.as_ptr(),
+                payload.len() as _,
+                gas_limit,
+                value.to_le_bytes().as_ptr(),
+            )
+        }
     }
 }
 
