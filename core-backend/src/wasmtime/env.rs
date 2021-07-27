bd7b429292d1b67c813c94ee0d6433279656ca89
Dmitry Novikov <novikov.dm.al@gmail.com>
2021-07-28 02:31:01 +0300

    Implementing function in `core-backend/wasmtime`

--- core-backend/src/wasmtime/env.rs
+++ core-backend/src/wasmtime/env.rs
@@ -42,6 +42,7 @@ impl<E: Ext + 'static> Environment<E> {
         result.add_func_i32_i32_i32_i64_i32_to_i32("gr_init", funcs::init);
         result.add_func_i32("gr_msg_id", funcs::msg_id);
         result.add_func_i32_i32_i32("gr_push", funcs::push);
+        result.add_func_i32_i32("gr_push_reply", funcs::push_reply);
         result.add_func_i32_i32_i32("gr_read", funcs::read);
         result.add_func_i32_i32_i64_i32("gr_reply", funcs::reply);
         result.add_func_i32_i32_i32_i64_i32("gr_send", funcs::send);
