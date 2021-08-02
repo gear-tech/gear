4a0b852def71c315d516ba6665ec2b03e306783b
NikVolf <nikvolf@gmail.com>
2021-08-02 10:31:15 +0300

    Add wasm processor for post-processing artifacts

---
+++ utils/wasm-proc/src/main.rs
@@ -0,0 +1,37 @@
+use clap::{App, Arg};
+use pwasm_utils::{self as utils, parity_wasm};
+use std::path::PathBuf;
+
+fn main() {
+    let matches = App::new("wasm-proc")
+        .arg(
+            Arg::with_name("input")
+                .index(1)
+                .required(true)
+                .help("Input WASM file"),
+        )
+        .get_matches();
+
+    let input = matches.value_of("input").expect("is required; qed");
+
+    let module = parity_wasm::deserialize_file(&input).unwrap();
+
+    // Invoke optimizer for the chain
+    let mut binary_module = module.clone();
+    let binary_file_name = PathBuf::from(input).with_extension("opt.wasm");
+    utils::optimize(&mut binary_module, vec!["handle", "init"]).expect("Optimizer failed");
+    parity_wasm::serialize_to_file(binary_file_name.clone(), binary_module)
+        .expect("Serialization failed");
+
+    println!("Optimized wasm: {}", binary_file_name.to_string_lossy());
+
+    // Invoke optimizer for the metadata
+    let mut metadata_module = module.clone();
+    let metadata_file_name = PathBuf::from(input).with_extension("meta.wasm");
+    utils::optimize(&mut metadata_module, vec!["meta_input", "meta_output"])
+        .expect("Metadata optimizer failed");
+    parity_wasm::serialize_to_file(metadata_file_name.clone(), metadata_module)
+        .expect("Serialization failed");
+
+    println!("Metadata wasm: {}", metadata_file_name.to_string_lossy());
+}
