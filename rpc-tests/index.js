1c0bbc7ff2978df2ca2c5033e38f21b48b0cc187
Nikolay Volf <nikvolf@gmail.com>
2021-08-04 11:13:19 +0300

    Convert type registration to orthdox representantino (#70)

--- rpc-tests/index.js
+++ rpc-tests/index.js
@@ -339,40 +339,40 @@ async function main() {
   const api = await ApiPromise.create({
     provider,
     types: {
-      Message: {
-        id: 'H256',
-        source: 'H256',
-        dest: 'H256',
-        payload: 'Vec<u8>',
-        gas_limit: 'u64',
-        value: 'u128',
-        reply: 'Option<(H256, i32)>',
+      "Message": {
+        "id": "H256",
+        "source": "H256",
+        "dest": "H256",
+        "payload": "Vec<u8>",
+        "gas_limit": "u64",
+        "value": "u128",
+        "reply": "Option<(H256, i32)>"
       },
-      Node: {
-        value: 'Message',
-        next: 'Option<H256>',
+      "Node": {
+        "value": "Message",
+        "next": "Option<H256>"
       },
-      IntermediateMessage: {
-        _enum: {
-          InitProgram: {
-            external_origin: 'H256',
-            program_id: 'H256',
-            code: 'Vec<u8>',
-            payload: 'Vec<u8>',
-            gas_limit: 'u64',
-            value: 'u128',
+      "IntermediateMessage": {
+        "_enum": {
+          "InitProgram": {
+            "external_origin": "H256",
+            "program_id": "H256",
+            "code": "Vec<u8>",
+            "payload": "Vec<u8>",
+            "gas_limit": "u64",
+            "value": "u128"
           },
-          DispatchMessage: {
-            id: 'H256',
-            route: 'MessageRoute',
-            payload: 'Vec<u8>',
-            gas_limit: 'u64',
-            value: 'u128',
-          },
-        },
+          "DispatchMessage": {
+            "id": "H256",
+            "route": "MessageRoute",
+            "payload": "Vec<u8>",
+            "gas_limit": "u64",
+            "value": "u128",
+          }
+        }
       },
-      MessageError: {
-        _enum: ['ValueTransfer', 'Dispatch'],
+      "MessageError": {
+        "_enum": ["ValueTransfer", "Dispatch"]
       },
     },
   });
