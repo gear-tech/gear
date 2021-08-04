53d2fd598f47eaa67c866edc57a6509e0276eabb
Dmitry Novikov <novikov.dm.al@gmail.com>
2021-08-04 10:27:13 +0300

    `general.rs` module for global structs

--- gstd/src/msg.rs
+++ gstd/src/msg.rs
@@ -1,49 +1,5 @@
 use crate::prelude::Vec;
 
-#[derive(Clone, Copy, Debug, Default, Hash, PartialEq, Eq)]
-pub struct ProgramId(pub [u8; 32]);
-
-impl From<u64> for ProgramId {
-    fn from(v: u64) -> Self {
-        let mut id = ProgramId([0u8; 32]);
-        id.0[0..8].copy_from_slice(&v.to_le_bytes()[..]);
-        id
-    }
-}
-
-impl ProgramId {
-    pub fn from_slice(s: &[u8]) -> Self {
-        assert_eq!(s.len(), 32);
-        let mut id = ProgramId([0u8; 32]);
-        id.0[..].copy_from_slice(s);
-        id
-    }
-
-    pub fn as_slice(&self) -> &[u8] {
-        &self.0[..]
-    }
-
-    pub fn as_mut_slice(&mut self) -> &mut [u8] {
-        &mut self.0[..]
-    }
-}
-
-#[derive(Clone, Copy, Debug, Default, Hash, PartialEq, Eq)]
-pub struct MessageId(pub [u8; 32]);
-
-impl MessageId {
-    pub fn from_slice(s: &[u8]) -> Self {
-        assert_eq!(s.len(), 32);
-        let mut id = Self([0u8; 32]);
-        id.0[..].copy_from_slice(s);
-        id
-    }
-
-    pub fn as_slice(&self) -> &[u8] {
-        &self.0[..]
-    }
-}
-
 mod sys {
     extern "C" {
         pub fn gr_send(
