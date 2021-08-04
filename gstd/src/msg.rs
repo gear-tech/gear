5619e4e2f6a49d20c58a90c7a8473f643b9772e1
Dmitry Novikov <novikov.dm.al@gmail.com>
2021-08-04 10:27:57 +0300

    Expand `msg.rs` module

--- gstd/src/msg.rs
+++ gstd/src/msg.rs
@@ -1,7 +1,23 @@
 use crate::prelude::Vec;
+use crate::{MessageId, ProgramId};
 
 mod sys {
     extern "C" {
+        pub fn gr_charge(gas: u64);
+        pub fn gr_commit(handle: u32);
+        pub fn gr_init(
+            program: *const u8,
+            data_ptr: *const u8,
+            data_len: u32,
+            gas_limit: u64,
+            value_ptr: *const u8,
+        ) -> u32;
+        pub fn gr_msg_id(val: *mut u8);
+        pub fn gr_push(handle: u32, data_ptr: *const u8, data_len: u32);
+        pub fn gr_push_reply(data_ptr: *const u8, data_len: u32);
+        pub fn gr_read(at: u32, len: u32, dest: *mut u8);
+        pub fn gr_reply(data_ptr: *const u8, data_len: u32, gas_limit: u64, value_ptr: *const u8);
+        pub fn gr_reply_to(dest: *mut u8);
         pub fn gr_send(
             program: *const u8,
             data_ptr: *const u8,
@@ -10,13 +26,36 @@ mod sys {
             value_ptr: *const u8,
         );
         pub fn gr_size() -> u32;
-        pub fn gr_read(at: u32, len: u32, dest: *mut u8);
-        pub fn gr_reply_to(dest: *mut u8);
         pub fn gr_source(program: *mut u8);
         pub fn gr_value(val: *mut u8);
-        pub fn gr_msg_id(val: *mut u8);
-        pub fn gr_reply(data_ptr: *const u8, data_len: u32, gas_limit: u64, value_ptr: *const u8);
-        pub fn gr_charge(gas: u64);
+    }
+}
+
+pub fn charge(gas: u64) {
+    unsafe {
+        sys::gr_charge(gas);
+    }
+}
+
+pub fn commit(handle: usize) {
+    unsafe { sys::gr_commit(handle as u32) }
+}
+
+pub fn id() -> MessageId {
+    let mut msg_id = MessageId::default();
+    unsafe { sys::gr_msg_id(msg_id.0.as_mut_ptr()) }
+    msg_id
+}
+
+pub fn init(program: ProgramId, payload: &[u8], gas_limit: u64, value: u128) -> usize {
+    unsafe {
+        sys::gr_init(
+            program.as_slice().as_ptr(),
+            payload.as_ptr(),
+            payload.len() as _,
+            gas_limit,
+            value.to_le_bytes().as_ptr(),
+        ) as usize
     }
 }
 
@@ -30,36 +69,12 @@ pub fn load() -> Vec<u8> {
     }
 }
 
-pub fn send(program: ProgramId, payload: &[u8], gas_limit: u64, value: u128) {
-    unsafe {
-        sys::gr_send(
-            program.as_slice().as_ptr(),
-            payload.as_ptr(),
-            payload.len() as _,
-            gas_limit,
-            value.to_le_bytes().as_ptr(),
-        )
-    }
+pub fn push(handle: usize, payload: &[u8]) {
+    unsafe { sys::gr_push(handle as u32, payload.as_ptr(), payload.len() as _) }
 }
 
-pub fn source() -> ProgramId {
-    let mut program_id = ProgramId::default();
-    unsafe { sys::gr_source(program_id.as_mut_slice().as_mut_ptr()) }
-    program_id
-}
-
-pub fn id() -> MessageId {
-    let mut msg_id = MessageId::default();
-    unsafe { sys::gr_msg_id(msg_id.0.as_mut_ptr()) }
-    msg_id
-}
-
-pub fn value() -> u128 {
-    let mut value_data = [0u8; 16];
-    unsafe {
-        sys::gr_value(value_data.as_mut_ptr());
-    }
-    u128::from_le_bytes(value_data)
+pub fn push_reply(payload: &[u8]) {
+    unsafe { sys::gr_push_reply(payload.as_ptr(), payload.len() as _) }
 }
 
 pub fn reply(payload: &[u8], gas_limit: u64, value: u128) {
@@ -73,15 +88,38 @@ pub fn reply(payload: &[u8], gas_limit: u64, value: u128) {
     }
 }
 
-/// Transfers gas from program caller.
-pub fn charge(gas: u64) {
-    unsafe {
-        sys::gr_charge(gas);
-    }
-}
-
 pub fn reply_to() -> MessageId {
     let mut message_id = MessageId::default();
     unsafe { sys::gr_reply_to(message_id.0.as_mut_ptr()) }
     message_id
+}
+
+pub fn send(program: ProgramId, payload: &[u8], gas_limit: u64) {
+    send_with_value(program, payload, gas_limit, 0u128);
+}
+
+pub fn send_with_value(program: ProgramId, payload: &[u8], gas_limit: Gas, value: u128) {
+    unsafe {
+        sys::gr_send(
+            program.as_slice().as_ptr(),
+            payload.as_ptr(),
+            payload.len() as _,
+            gas_limit.0,
+            value.to_le_bytes().as_ptr(),
+        )
+    }
+}
+
+pub fn source() -> ProgramId {
+    let mut program_id = ProgramId::default();
+    unsafe { sys::gr_source(program_id.as_mut_slice().as_mut_ptr()) }
+    program_id
+}
+
+pub fn value() -> u128 {
+    let mut value_data = [0u8; 16];
+    unsafe {
+        sys::gr_value(value_data.as_mut_ptr());
+    }
+    u128::from_le_bytes(value_data)
 }
