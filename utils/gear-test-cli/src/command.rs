6e111a5d34f0255a2500aa20159823ab40856fa0
Vadim Smirnov <78953304+ukint-vs@users.noreply.github.com>
2021-08-05 08:58:50 +0300

    Log result in test runner (#81)
    * use log for test runner
    print RunResult for each step
    
    * Update gear-test-cli

--- utils/gear-test-cli/src/command.rs
+++ utils/gear-test-cli/src/command.rs
@@ -34,7 +34,7 @@ impl GearTestCmd {
     /// Runs tests from `.yaml` files.
     pub fn run(&self, _config: Configuration) -> sc_cli::Result<()> {
         new_test_ext().execute_with(|| {
-            gear_test::check::check_main(self.input.to_vec(), true, false, false, false, || {
+            gear_test::check::check_main(self.input.to_vec(), true, false, false, || {
                 sp_io::storage::clear_prefix(b"g::code");
                 sp_io::storage::clear_prefix(b"g::alloc");
                 sp_io::storage::clear_prefix(b"g::msg");
