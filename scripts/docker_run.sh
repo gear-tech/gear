460ef5b9dd07263cb1e681f06d1ecf6713c76e44
Mikhail Pogretskiy <mikhail.pogretskiy@gmail.com>
2021-08-06 15:46:13 +0300

    Improve script compatibility (#89)

--- scripts/docker_run.sh
+++ scripts/docker_run.sh
@@ -1,10 +1,10 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 set -e
 
 echo "*** Start Substrate node template ***"
 
-cd $(dirname ${BASH_SOURCE[0]})/..
+cd "$(dirname "$0")/.."
 
 docker-compose down --remove-orphans
 docker-compose run --rm --service-ports dev $@
