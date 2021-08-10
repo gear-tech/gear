1904dd08e0afbf42bf50707c32a752b758fa6210
NikVolf <nikvolf@gmail.com>
2021-08-10 12:31:50 +0300

    Make 'tests' directory, and split common framework

--- tests/btest/src/lib.rs
+++ tests/btest/src/lib.rs
@@ -97,15 +97,12 @@ mod wasm {
 #[cfg(test)]
 #[cfg(feature = "std")]
 mod tests {
-    use codec::{Decode, Encode};
-
     use super::native;
 
-    use gear_core::message::MessageId;
     use gear_core::storage::{
         InMemoryMessageQueue, InMemoryProgramStorage, InMemoryWaitList, Storage,
     };
-    use gear_core_runner::{Config, ExtMessage, MessageDispatch, ProgramInitialization, Runner};
+    use gear_core_runner::{Config, ExtMessage, ProgramInitialization, Runner};
 
     #[test]
     fn binary_available() {
@@ -149,86 +146,24 @@ mod tests {
         );
     }
 
-    fn do_requests_in_order(requests: Vec<super::Request>) -> Vec<super::Reply> {
-        let mut runner = new_test_runner();
-
-        runner
-            .init_program(ProgramInitialization {
-                new_program_id: 1.into(),
-                source_id: 0.into(),
-                code: wasm_code().to_vec(),
-                message: ExtMessage {
-                    id: 1000001.into(),
-                    payload: "init".as_bytes().to_vec(),
-                    gas_limit: u64::MAX,
-                    value: 0,
-                },
-            })
-            .expect("failed to init program");
-
-        let mut nonce = 0;
-
-        let mut data: Vec<(u64, MessageId, Option<super::Reply>)> = Vec::new();
-
-        for request in requests {
-            let message_id: MessageId = (nonce + 1000002).into();
-            data.push((nonce, message_id, None));
-            runner.queue_message(MessageDispatch {
-                source_id: 0.into(),
-                destination_id: 1.into(),
-                data: ExtMessage {
-                    id: message_id,
-                    gas_limit: u64::MAX,
-                    value: 0,
-                    payload: request.encode(),
-                },
-            });
-            nonce += 1;
-        }
-
-        while runner.run_next(u64::MAX).handled != 0 {}
-
-        let Storage { message_queue, .. } = runner.complete();
-
-        assert_eq!(
-            message_queue.log().first().map(|m| m.payload().to_vec()),
-            Some(b"CREATED".to_vec())
-        );
-
-        for message in message_queue.log().iter() {
-            for (_, search_message_id, ref mut reply) in data.iter_mut() {
-                if message
-                    .reply
-                    .map(|(msg_id, _)| msg_id == *search_message_id)
-                    .unwrap_or(false)
-                {
-                    *reply = Some(
-                        super::Reply::decode(&mut message.payload.as_ref())
-                            .expect("Failed to decode reply"),
-                    );
-                }
-            }
-        }
-
-        data.into_iter()
-            .map(|(_, _, reply)| reply.expect("No reply for message"))
-            .collect()
-    }
-
     #[test]
     fn simple() {
-        use super::{Reply, Request::*};
+        use super::{Reply, Request::{self, *}};
         assert_eq!(
-            do_requests_in_order(vec![
-                Insert(0, 1),
-                Insert(0, 2),
-                Insert(1, 3),
-                Insert(2, 5),
-                Remove(1),
-                List,
-                Clear,
-                List,
-            ]),
+            common::do_requests_in_order::<Request, Reply>(
+                new_test_runner(),
+                wasm_code().to_vec(),
+                vec![
+                    Insert(0, 1),
+                    Insert(0, 2),
+                    Insert(1, 3),
+                    Insert(2, 5),
+                    Remove(1),
+                    List,
+                    Clear,
+                    List,
+                ]
+            ),
             vec![
                 Reply::Value(None),
                 Reply::Value(Some(1)),
