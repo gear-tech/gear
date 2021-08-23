cec8e115f44679a0c1502d12fb32b0d7430e512a
Dmitry Novikov <novikov.dm.al@gmail.com>
2021-08-23 19:52:03 +0300

    Demo showing the functionality of sending in parts (#120)
    * Write decoder program
    
    * Fix examples logic and write `.yaml`
    
    * Debug and format
    
    * Avoid using `sort_unstable()`
    
    * Update demo according latest api changes
    
    Co-authored-by: Nikolay Volf <nikvolf@gmail.com>

---
+++ examples/decoder/src/lib.rs
@@ -0,0 +1,68 @@
+#![no_std]
+#![feature(default_alloc_error_handler)]
+
+use gstd::{msg, prelude::*};
+
+#[no_mangle]
+pub unsafe extern "C" fn handle() {
+    let new_msg = String::from_utf8(msg::load()).expect("Invalid message: should be utf-8");
+
+    let code: Vec<usize> = new_msg
+        .split_whitespace()
+        .map(|v| {
+            v.parse::<usize>()
+                .expect("Not a number was sent in sequence")
+                - 1
+        })
+        .collect();
+
+    let nodes = code.len() + 2;
+
+    let mut degrees = vec![1; nodes];
+    for vertex in &code {
+        degrees[*vertex] += 1;
+    }
+
+    let mut leaves = vec![];
+    for vertex in 0..nodes {
+        if degrees[vertex] == 1 {
+            leaves.push(vertex);
+        }
+    }
+
+    let handle = msg::send_init();
+
+    for vertex in &code {
+        leaves.sort();
+        leaves.reverse();
+        let leaf = leaves.pop().expect("An error occured during calculating");
+
+        msg::send_push(
+            handle,
+            format!("[{}, {}];", leaf + 1, vertex + 1).as_bytes(),
+        );
+
+        degrees[*vertex] -= 1;
+
+        if degrees[*vertex] == 1 {
+            leaves.push(*vertex);
+        }
+    }
+
+    msg::send_push(
+        handle,
+        format!("[{}, {}]", leaves[0] + 1, leaves[1] + 1).as_bytes(),
+    );
+
+    msg::send_commit(handle, msg::source(), 10_000_000, 0);
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn init() {}
+
+#[panic_handler]
+fn panic(_info: &panic::PanicInfo) -> ! {
+    unsafe {
+        core::arch::wasm32::unreachable();
+    }
+}
