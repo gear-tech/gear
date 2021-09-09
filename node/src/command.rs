b5778da53c30d4c49929b3593e6644fcb4509fb3
NikVolf <nikvolf@gmail.com>
2021-09-09 13:45:12 -0500

    add staging net json chainspec

--- node/src/command.rs
+++ node/src/command.rs
@@ -50,11 +50,12 @@ impl SubstrateCli for Cli {
     fn load_spec(&self, id: &str) -> Result<Box<dyn sc_service::ChainSpec>, String> {
         Ok(match id {
             "dev" => Box::new(chain_spec::development_config()?),
-            "" | "local" => Box::new(chain_spec::local_testnet_config()?),
+            "local" => Box::new(chain_spec::local_testnet_config()?),
             "staging" => Box::new(chain_spec::staging_testnet_config()?),
-            path => Box::new(chain_spec::ChainSpec::from_json_file(
-                std::path::PathBuf::from(path),
+            "test" | "" => Box::new(chain_spec::ChainSpec::from_json_bytes(
+                &include_bytes!("../res/staging.json")[..],
             )?),
+            path => Box::new(chain_spec::ChainSpec::from_json_file(path.into())?),
         })
     }
 
