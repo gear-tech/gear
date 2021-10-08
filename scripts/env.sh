8938103cae1888940e9bc258cad5f51cde8a80fe
Dmitry Novikov <novikov.dm.al@gmail.com>
2021-10-08 13:09:33 +0300

    Provide `env` script

---
+++ scripts/env.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env sh
+
+ROOT_DIR="$(cd "$(dirname "$0")"/.. && pwd)"
+
+bold() {
+    tput bold
+}
+
+normal() {
+    tput sgr0
+}
+
+wasm_init() {
+    if [ -z $CI_PROJECT_NAME ] ; then
+        rustup update nightly
+        rustup update stable
+    fi
+
+    rustup target add wasm32-unknown-unknown --toolchain nightly
+}
+
+js_init() {
+    npm --prefix $ROOT_DIR/utils/wasm-proc/metadata-js install
+    npm --prefix $ROOT_DIR/utils/wasm-proc/metadata-js update
+    npm --prefix $ROOT_DIR/gtest/src/js install
+    npm --prefix $ROOT_DIR/gtest/src/js update
+}
+
+show() {
+    rustup show
+
+    bold && echo "node.js\n-------\n" && normal
+	node -v
+
+    bold && echo "\nnpm\n---\n" && normal
+	npm -v
+}
+
+case "$1" in
+    init)
+            bold && echo "*** Initializing WASM build environment\n" && normal
+            wasm_init
+            bold && echo "\n*** Installing and updating JS dependencies\n" && normal
+            js_init
+            ;;
+    wasm)
+            bold && echo "*** Initializing WASM build environment\n" && normal
+            wasm_init
+            ;;
+    js)
+            bold && echo "*** Installing and updating JS dependencies\n" && normal
+            js_init
+            ;;
+    show)
+            bold && echo "Your environment:\n" && normal
+            show
+            ;;
+esac
