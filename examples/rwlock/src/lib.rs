57cb4b6d5cc5d7e79e44d0e3d4119ce3aea82cd0
Dmitry Novikov <novikov.dm.al@gmail.com>
2021-10-26 17:49:32 +0300

    Provide extra test for readers

--- examples/rwlock/src/lib.rs
+++ examples/rwlock/src/lib.rs
@@ -1,7 +1,13 @@
 #![no_std]
 
+use core::{
+    future::Future,
+    pin::Pin,
+    ptr,
+    task::{Context, RawWaker, RawWakerVTable, Waker},
+};
 use gstd::{exec, msg, prelude::*, ProgramId};
-use gstd_async::msg as msg_async;
+use gstd_async::{msg as msg_async, rwlock::RwLockReadGuard};
 
 static mut PING_DEST: ProgramId = ProgramId([0u8; 32]);
 static RWLOCK: gstd_async::rwlock::RwLock<u32> = gstd_async::rwlock::RwLock::new(0);
@@ -47,9 +53,63 @@ async fn main() {
             msg_async::send_and_wait_for_reply(unsafe { PING_DEST }, b"PING", GAS_LIMIT, 0).await;
             msg::reply(*val, exec::gas_available() - GAS_LIMIT, 0);
         }
+        "check readers" => {
+            let mut storage: Vec<RwLockReadGuard<u32>> = Vec::new();
+            for _ in 0..32 {
+                storage.push(RWLOCK.read().await);
+            }
+
+            let waker = unsafe {
+                Waker::from_raw(RawWaker::new(
+                    ptr::null(),
+                    &RawWakerVTable::new(clone_waker, wake, wake_by_ref, drop_waker),
+                ))
+            };
+            let mut cx = Context::from_waker(&waker);
+
+            // Read future just for extra testing
+            let mut write_future = RWLOCK.write();
+
+            if Pin::new(&mut write_future).poll(&mut cx).is_ready() {
+                panic!("I am ready, but I should't");
+            }
+            //
+
+            let mut another_future = RWLOCK.read();
+
+            if Pin::new(&mut another_future).poll(&mut cx).is_ready() {
+                panic!("I am ready, but I should't");
+            }
+
+            storage.pop();
+
+            // Read future just for extra testing
+            if Pin::new(&mut write_future).poll(&mut cx).is_ready() {
+                panic!("I am ready, but I should't");
+            }
+            //
+
+            if !Pin::new(&mut another_future).poll(&mut cx).is_ready() {
+                panic!("I am not ready, but I should be");
+            }
+
+            let val = another_future.await;
+
+            msg::reply(*val, exec::gas_available() - GAS_LIMIT, 0);
+        }
         _ => {
             let _write = RWLOCK.write().await;
             RWLOCK.read().await;
         }
     }
 }
+
+unsafe fn clone_waker(ptr: *const ()) -> RawWaker {
+    RawWaker::new(
+        ptr,
+        &RawWakerVTable::new(clone_waker, wake, wake_by_ref, drop_waker),
+    )
+}
+unsafe fn wake(_ptr: *const ()) {}
+unsafe fn wake_by_ref(_ptr: *const ()) {}
+unsafe fn drop_waker(_ptr: *const ()) {}
