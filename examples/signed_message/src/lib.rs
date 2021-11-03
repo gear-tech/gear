818907a654694118f45b9d7a04dd43913954fad3
Georgy Shepelev <geo.shepelev@gmail.com>
2021-11-03 22:06:04 +0400

    check that "PING" is signed

--- examples/signed_message/src/lib.rs
+++ examples/signed_message/src/lib.rs
@@ -1,23 +1,53 @@
 #![no_std]
 
 use codec::Decode;
-use gstd::{debug, msg};
+use gstd::{debug, msg, prelude::*};
 use sp_core::{
     crypto::{AccountId32, UncheckedFrom},
     H256,
+    sr25519,
 };
 
-static mut SIGNATORY: Option<AccountId32> = None;
+use scale_info::TypeInfo;
+
+#[derive(Debug, Decode, TypeInfo)]
+struct InitArgs {
+    account: AccountId32,
+}
+
+#[derive(Debug, Decode, TypeInfo)]
+struct HandleArgs {
+    // now it just "PING"
+    // message: Vec<u8>,
+    signature: Vec<u8>,
+}
+
+gstd::metadata! {
+    title: "SignedMessage",
+        init:
+            input: InitArgs,
+            output: (),
+        handle:
+            input: HandleArgs,
+            output: (),
+}
+
+static mut SIGNATORY: Option<<sr25519::Pair as sp_core::Pair>::Public> = None;
 
 #[no_mangle]
 pub unsafe extern "C" fn handle() {
-    let source = msg::source();
-    let h256 = H256::from_slice(source.as_slice());
-    let account = AccountId32::unchecked_from(h256);
-    debug!("{:?}", account);
+    let args: HandleArgs = msg::load().expect("HandleArgs decode failed");
+    debug!("{:?}", args);
 
-    if account == SIGNATORY.clone().unwrap() {
-        msg::reply_bytes("Accepted!", 10_000_000, 0);
+    let mut signature = <sr25519::Pair as sp_core::Pair>::Signature::default();
+    if args.signature.len() != AsRef::<[u8]>::as_ref(&signature).len() {
+        debug!("if args.signature.len() != signature.as_ref().len() {");
+        return;
+    }
+
+    signature.as_mut().copy_from_slice(&args.signature);
+    if <sr25519::Pair as sp_core::Pair>::verify(&signature, &b"PING", &SIGNATORY.unwrap()) {
+        msg::reply_bytes("Authorized PONG", 10_000_000, 0);
     }
 }
 
@@ -26,10 +56,12 @@ pub unsafe extern "C" fn handle_reply() {}
 
 #[no_mangle]
 pub unsafe extern "C" fn init() {
-    let msg = msg::load_bytes();
-    debug!("{:?}", msg);
-    let account = AccountId32::decode(&mut &msg[..]).expect("AccountId::decode failed");
-    debug!("{:?}", account);
+    let args: InitArgs = msg::load().expect("InitArgs decode failed");
+    debug!("{:?}", args);
 
-    SIGNATORY = Some(account);
+    let bytes: [u8; 32] = args.account.into();
+    let pub_key = <sr25519::Pair as sp_core::Pair>::Public::unchecked_from(bytes);
+    debug!("{:?}", pub_key);
+
+    SIGNATORY = Some(pub_key);
 }
