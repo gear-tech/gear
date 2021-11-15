102a01b995e329db5ccd4347705ee52ad9857bbf
Dmitry Novikov <novikov.dm.al@gmail.com>
2021-11-15 16:00:15 +0300

    rename `async_sending` to just `async`

--- gstd/src/msg/mod.rs
+++ gstd/src/msg/mod.rs
@@ -16,8 +16,8 @@
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-mod async_sending;
-pub use async_sending::*;
+mod r#async;
+pub use r#async::*;
 
 mod basic;
 pub use basic::*;
