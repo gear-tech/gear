99534c09d1583cc939798d6464cd0b3ccead1b24
Vadim Smirnov <vs@gear-tech.io>
2021-11-23 14:00:23 +0300

    fmt

--- examples/sum/src/lib.rs
+++ examples/sum/src/lib.rs
@@ -35,7 +35,12 @@ pub unsafe extern "C" fn handle() {
     MESSAGE_LOG.push(format!("(sum) New msg: {:?}", new_msg));
     debug!("sum gas_available: {}", exec::gas_available());
 
-    msg::send(STATE.send_to(), new_msg + new_msg, exec::gas_available() - 1_000_000_000, 0);
+    msg::send(
+        STATE.send_to(),
+        new_msg + new_msg,
+        exec::gas_available() - 1_000_000_000,
+        0,
+    );
 
     debug!("{:?} total message(s) stored: ", MESSAGE_LOG.len());
 
