cc0bddcfcbaa75f1c73689dd94cf8e9aa24ed2ad
Gregory Sobol <grishasobol@mail.ru>
2021-12-03 18:23:59 +0300

    add script to run only one example build

---
+++ scripts/src/build_one_example.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+# Go into gear source root dir and run:
+# $ script/src/build_one_example.sh /examples/your_test_name
+
+set -e
+
+ROOT_DIR="$(pwd)"
+TARGET_DIR="$ROOT_DIR/target"
+
+cd "$ROOT_DIR"/$1
+CARGO_TARGET_DIR="$TARGET_DIR" cargo +nightly hack build --release
+cd -
+
+"$TARGET_DIR"/release/wasm-proc -p "$TARGET_DIR"/wasm32-unknown-unknown/release/*.wasm
+
