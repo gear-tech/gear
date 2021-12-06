1d73f6fa44f81b2ac10bd66572ae14b2b6d70a49
Gregory Sobol <grishasobol@mail.ru>
2021-12-06 23:38:09 +0300

    rebase to master index.js

--- utils/wasm-proc/metadata-js/index.js
+++ utils/wasm-proc/metadata-js/index.js
@@ -1,84 +1,82 @@
 exports.getWasmMetadata = async (wasmBytes) => {
-  const memory = new WebAssembly.Memory({ initial: 256 });
+    const memory = new WebAssembly.Memory({ initial: 256, maximum: 512 });
 
-  const importObj = {
-    env: {
-      abortStackOverflow: () => {
-        throw new Error('overflow');
-      },
-      table: new WebAssembly.Table({ initial: 0, maximum: 0, element: 'anyfunc' }),
-      tableBase: 0,
-      memory: memory,
-      memoryBase: 1024,
-      STACKTOP: 0,
-      STACK_MAX: memory.buffer.byteLength,
-      alloc: (pages) => {
-        return memory.grow(pages);
-      },
-      free: (_pages) => {},
-      gr_debug: (msg, len) => {
-        console.log('GR_DEBUG', len, ab2str( memory.buffer.slice(msg, msg + len) ) );
-      },
-      gr_msg_id: () => {},
-      gr_size: () => {},
-      gr_read: () => {},
-      gr_source: () => {},
-      gr_gas_available: () => {},
-      gr_send: () => {},
-      gr_send_commit: () => {},
-      gr_send_init: () => {},
-      gr_send_push: () => {},
-      gr_reply: () => {},
-      gr_reply_push: () => {},
-      gr_reply_to: () => {},
-      gr_value: () => {},
-      gr_wait: () => {},
-      gr_wake: () => {},
-    },
-  };
-  let module = await WebAssembly.instantiate(wasmBytes, importObj);
+    const importObj = {
+        env: {
+            abortStackOverflow: () => { throw new Error('overflow'); },
+            table: new WebAssembly.Table({ initial: 0, maximum: 0, element: 'anyfunc' }),
+            tableBase: 0,
+            memory: memory,
+            memoryBase: 1024,
+            STACKTOP: 0,
+            STACK_MAX: memory.buffer.byteLength,
+            alloc: (pages) => { return memory.grow(pages) },
+            free: (_pages) => { },
+            gr_debug: (msg) => {
+                console.log('GR_DEBUG', len, ab2str( memory.buffer.slice(msg, msg + len) ) );
+            },
+            gr_msg_id: () => { },
+            gr_size: () => { },
+            gr_read: () => { },
+            gr_source: () => { },
+            gr_gas_available: () => { },
+            gr_send: () => { },
+            gr_send_commit: () => { },
+            gr_send_init: () => { },
+            gr_send_push: () => { },
+            gr_reply: () => { },
+            gr_reply_push: () => { },
+            gr_reply_to: () => { },
+            gr_value: () => { },
+            gr_wait: () => { },
+            gr_wake: () => { },
+        }
+    };
 
-  let metadata = {
-    // init_input: '',
-    // init_output: '',
-    // async_init_input: '',
-    // async_init_output: '',
-    // handle_input: '',
-    // handle_output: '',
-    // async_handle_input: '',
-    // async_handle_output: '',
-    // state_input: '',
-    // state_output: '',
-    registry1: readMeta(memory, module.instance.exports.meta_registry()),
-    // registry2: readMeta(memory, module.instance.exports.meta_registry()),
-  };
+    let metadata = {
+        init_input: "",
+        init_output: "",
+        async_init_input: "",
+        async_init_output: "",
+        handle_input: "",
+        handle_output: "",
+        async_handle_input: "",
+        async_handle_output: "",
+        state_input: "",
+        state_output: "",
+        registry: "",
+        title: ""
+    }
 
-  // metadata.init_input = readMeta(memory, module.instance.exports.meta_init_input());
-  // metadata.init_output = readMeta(memory, module.instance.exports.meta_init_output());
-  // metadata.async_init_input = readMeta(memory, module.instance.exports.meta_async_init_input());
-  // metadata.async_init_output = readMeta(memory, module.instance.exports.meta_async_init_output());
-  // metadata.handle_input = readMeta(memory, module.instance.exports.meta_handle_input());
-  // metadata.handle_output = readMeta(memory, module.instance.exports.meta_handle_output());
-  // metadata.async_handle_input = readMeta(memory, module.instance.exports.meta_async_handle_input());
-  // metadata.async_handle_output = readMeta(memory, module.instance.exports.meta_async_handle_output());
-  // metadata.state_input = readMeta(memory, module.instance.exports.meta_state_input());
-  // metadata.state_output = readMeta(memory, module.instance.exports.meta_state_output());
-  // metadata.title = readMeta(memory, module.instance.exports.meta_title());
+    let module = await WebAssembly.instantiate(wasmBytes, importObj);
 
-  return metadata;
-};
+    metadata.init_input = readMeta(memory, module.instance.exports.meta_init_input());
+    metadata.init_output = readMeta(memory, module.instance.exports.meta_init_output());
+    metadata.async_init_input = readMeta(memory, module.instance.exports.meta_async_init_input());
+    metadata.async_init_output = readMeta(memory, module.instance.exports.meta_async_init_output());
+    metadata.handle_input = readMeta(memory, module.instance.exports.meta_handle_input());
+    metadata.handle_output = readMeta(memory, module.instance.exports.meta_handle_output());
+    metadata.async_handle_input = readMeta(memory, module.instance.exports.meta_async_handle_input());
+    metadata.async_handle_output = readMeta(memory, module.instance.exports.meta_async_handle_output());
+    metadata.state_input = readMeta(memory, module.instance.exports.meta_state_input());
+    metadata.state_output = readMeta(memory, module.instance.exports.meta_state_output());
+    metadata.registry = `0x${readMeta(memory, module.instance.exports.meta_registry())}`;
+    metadata.title = readMeta(memory, module.instance.exports.meta_title());
+
+    return metadata;
+}
 
 function readMeta(memory, ptr) {
-  let length = memory.buffer.slice(ptr + 4, ptr + 8);
-  length = new Uint32Array(length)[0];
-  console.log('length:', length);
-  let pointer = memory.buffer.slice(ptr, ptr + 4);
-  pointer = new Uint32Array(pointer)[0];
-  console.log('pointer:', pointer);
-  let buf = memory.buffer.slice(pointer, pointer + length);
-  return ab2str(buf);
+    let length = memory.buffer.slice(ptr + 4, ptr + 8);
+    length = new Uint32Array(length)[0];
+
+    let pointer = memory.buffer.slice(ptr, ptr + 4);
+    pointer = new Uint32Array(pointer)[0];
+
+    let buf = memory.buffer.slice(pointer, pointer + length);
+    return ab2str(buf);
 }
 
 function ab2str(buf) {
-  return String.fromCharCode.apply(null, new Uint8Array(buf));
+    return String.fromCharCode.apply(null, new Uint8Array(buf));
 }
