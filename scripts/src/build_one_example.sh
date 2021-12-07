ad9c4eb17f6721a6ae1f5fb8ac48136fa25a205c
Gregory Sobol <grishasobol@mail.ru>
2021-12-07 16:32:21 +0300

    fix comment

--- scripts/src/build_one_example.sh
+++ scripts/src/build_one_example.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 # Go into gear source root dir and run:
-# $ script/src/build_one_example.sh /examples/your_test_name
+# $ script/src/build_one_example.sh ./examples/your_test_name
 
 set -e
 
