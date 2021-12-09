986971ad20811c62b799df91383a492b112468e6
Sabaun Taraki <taraki.sabaun.rl@gmail.com>
2021-12-09 18:19:19 +0300

    Avoid redundant code set

--- common/src/native.rs
+++ common/src/native.rs
@@ -80,7 +80,10 @@ pub fn get_program(id: ProgramId) -> Option<Program> {
 
 pub fn set_program(program: Program) {
     let code_hash = sp_io::hashing::blake2_256(program.code()).into();
-    crate::set_code(code_hash, program.code());
+    // don't do multiple sets of the same code
+    if !crate::code_exists(code_hash) {
+        crate::set_code(code_hash, program.code());
+    }
     crate::set_program(
         H256::from_slice(program.id().as_slice()),
         crate::Program {
