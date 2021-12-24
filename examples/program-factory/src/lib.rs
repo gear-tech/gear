cd16bcf1702830e5b4213b151770b46ca64a8561
Sabaun Taraki <taraki.sabaun.rl@gmail.com>
2021-12-24 16:41:13 +0300

    Remove log dep from demo

--- examples/program-factory/src/lib.rs
+++ examples/program-factory/src/lib.rs
@@ -1,17 +1,10 @@
 #![no_std]
 
 use gcore::{msg, H256};
-
-fn init_logger() {
-    let _ = env_logger::Builder::from_default_env()
-        .format_module_path(false)
-        .format_level(true)
-        .try_init();
-}
+use gstd::debug;
 
 #[no_mangle]
 pub unsafe extern "C" fn handle() {
-    init_logger();
     // r#"
     // (module
     //   (import "env" "memory" (memory 1))
@@ -22,7 +15,7 @@ pub unsafe extern "C" fn handle() {
     // )"#
     let submitted_code: H256 = hex_literal::hex!("abf3746e72a6e8740bd9e12b879fbdd59e052cb390f116454e9116c22021ae4a").into();
     let new_program_id = msg::create_program(submitted_code, b"default", b"", 10_000, 0);
-    log::debug!("new program {:?}", new_program_id);
+    debug!("New program created {:?}", new_program_id)
 }
 
 #[no_mangle]
