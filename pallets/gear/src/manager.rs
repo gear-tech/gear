be3282b204393509622b6918ed52fc4dae7dbcf4
Shamil <shamil@shamil.ru>
2022-01-18 23:14:56 +0300

    Add trap reason to runtime log

--- pallets/gear/src/manager.rs
+++ pallets/gear/src/manager.rs
@@ -1,3 +1,21 @@
+// This file is part of Gear.
+
+// Copyright (C) 2021-2022 Gear Technologies Inc.
+// SPDX-License-Identifier: GPL-3.0-or-later WITH Classpath-exception-2.0
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
 use crate::{
     pallet::Reason, Authorship, Config, DispatchOutcome, Event, ExecutionResult, MessageInfo,
     Pallet, ProgramsLimbo,
@@ -200,7 +218,16 @@ where
                 outcome: ExecutionResult::Success,
             }),
             CoreDispatchOutcome::MessageTrap { message_id, trap } => {
-                let reason = trap.map(|v| v.as_bytes().to_vec()).unwrap_or_default();
+                let reason = if let Some(reason) = trap {
+                    log::warn!(
+                        target: "runtime::gear",
+                        "Program terminated with a trap: {}",
+                        reason
+                    );
+                    reason.as_bytes().to_vec()
+                } else {
+                    Vec::new()
+                };
 
                 Event::MessageDispatched(DispatchOutcome {
                     message_id: message_id.into_origin(),
