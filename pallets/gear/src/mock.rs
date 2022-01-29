276e8366618c4fcb8c81ece053e3dbc9708ff68a
Vadim Smirnov <vs@gear-tech.io>
2022-01-29 15:22:03 +0300

    Update mock

--- pallets/gear/src/mock.rs
+++ pallets/gear/src/mock.rs
@@ -93,6 +93,7 @@ impl system::Config for Test {
     type SystemWeightInfo = ();
     type SS58Prefix = SS58Prefix;
     type OnSetCode = ();
+    type MaxConsumers = frame_support::traits::ConstU32<16>;
 }
 
 pub struct GasConverter;
