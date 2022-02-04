fa5bdd98f7c23041917543bc2e629c250b35ed1a
Dmitry Novikov <novikov.dm.al@gmail.com>
2022-02-04 16:14:46 +0300

    Fix demo according RPC-testing requirements

--- examples/incomplete_async_payloads/src/lib.rs
+++ examples/incomplete_async_payloads/src/lib.rs
@@ -1,8 +1,11 @@
 #![no_std]
 
-use gstd::{debug, msg, prelude::*};
+use core::num::ParseIntError;
+use gstd::{debug, msg, prelude::*, ActorId};
 
 const GAS_LIMIT: u64 = 1_000_000_000;
+
+static mut DEMO_PING: ActorId = ActorId::new([0u8; 32]);
 
 #[gstd::async_main]
 async fn main() {
@@ -15,7 +18,7 @@ async fn main() {
             let handle = msg::send_init();
             handle.push(b"STORED ");
 
-            let _ = msg::send_bytes_and_wait_for_reply(2.into(), b"PING", GAS_LIMIT, 0)
+            let _ = msg::send_bytes_and_wait_for_reply(DEMO_PING, b"PING", GAS_LIMIT, 0)
                 .await
                 .expect("Error in async message processing");
 
@@ -33,7 +36,7 @@ async fn main() {
 
             msg::reply_push(b"STORED ");
 
-            let _ = msg::send_bytes_and_wait_for_reply(2.into(), b"PING", GAS_LIMIT, 0)
+            let _ = msg::send_bytes_and_wait_for_reply(DEMO_PING, b"PING", GAS_LIMIT, 0)
                 .await
                 .expect("Error in async message processing");
 
@@ -60,5 +63,17 @@ async fn main() {
     }
 }
 
+fn decode_hex(s: &str) -> Result<Vec<u8>, ParseIntError> {
+    (0..s.len())
+        .step_by(2)
+        .map(|i| u8::from_str_radix(&s[i..i + 2], 16))
+        .collect()
+}
+
 #[no_mangle]
-pub unsafe extern "C" fn init() {}
+pub unsafe extern "C" fn init() {
+    let input = String::from_utf8(msg::load_bytes()).expect("Invalid message: should be utf-8");
+    DEMO_PING =
+        ActorId::from_slice(&decode_hex(&input).expect("INTIALIZATION FAILED: INVALID PROGRAM ID"))
+            .expect("Unable to create ActorId");
+}
