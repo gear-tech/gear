28b78aa4a978ee4c3e01e494d26b5cf1df5c8c34
Dmitry Novikov <novikov.dm.al@gmail.com>
2022-02-07 16:15:22 +0300

    Add basic yaml test

---
+++ examples/state_rollback/src/lib.rs
@@ -0,0 +1,29 @@
+#![no_std]
+
+use gstd::{msg, prelude::*};
+
+static mut MESSAGE: Option<String> = None;
+
+fn value() -> String {
+    unsafe { MESSAGE.clone().unwrap_or_else(|| String::from("EMPTY")) }
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn handle() {
+    if let Ok(message) = String::from_utf8(msg::load_bytes()) {
+        // prev value
+        msg::send_bytes(msg::source(), value(), 0, 0);
+
+        MESSAGE = Some(message.clone());
+
+        // new value
+        msg::reply_bytes(value(), 0, 0);
+
+        if message == "panic" {
+            panic!();
+        }
+    }
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn init() {}
