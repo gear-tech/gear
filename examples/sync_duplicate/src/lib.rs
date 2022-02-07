4d93c91b86ba29748fe832cad2c3c6c19606ea3d
Vadim Smirnov <vs@gear-tech.io>
2022-02-07 14:24:00 +0300

    Update demo-sync-duplicate

--- examples/sync_duplicate/src/lib.rs
+++ examples/sync_duplicate/src/lib.rs
@@ -1,7 +1,8 @@
 #![no_std]
 
-use gstd::{msg, prelude::*};
+use gstd::{msg, prelude::*, ActorId};
 
+static mut DEST: ActorId = ActorId::new([0u8; 32]);
 static mut COUNTER: usize = 0;
 
 fn increase() {
@@ -21,14 +22,18 @@ fn clear() {
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn init() {}
+pub unsafe extern "C" fn init() {
+    let dest = String::from_utf8(msg::load_bytes()).expect("Invalid message: should be utf-8");
+    DEST = ActorId::from_slice(&hex::decode(dest).expect("Invalid hex"))
+        .expect("Unable to create ActorId");
+}
 
 #[gstd::async_main]
 async fn main() {
     let msg = String::from_utf8(msg::load_bytes()).expect("Invalid message: should be utf-8");
     if &msg == "async" {
         increase();
-        let _ = msg::send_bytes_and_wait_for_reply(2.into(), b"PING", 100_000_000, 0)
+        let _ = msg::send_bytes_and_wait_for_reply(DEST, b"PING", 100_000_000, 0)
             .await
             .expect("Error in async message processing");
         msg::reply(get(), 100_000_000, 0);
