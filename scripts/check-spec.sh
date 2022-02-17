eb34dd88b1e28eea38f847c62036b068198e24ec
Dmitry Novikov <novikov.dm.al@gmail.com>
2022-02-17 15:44:48 +0300

    Provide script and CI checking for spec version

---
+++ scripts/check-spec.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env sh
+
+PACKAGES_REQUIRE_BUMP_SPEC=(
+    "common"
+    "core"
+    "core-backend"
+    "node"
+    "pallets"
+    "runtime-interface"
+)
+
+SPEC_ON_MASTER="$(git diff origin/master | sed -n -r "s/^\-[[:space:]]+spec_version: +([0-9]+),$/\1/p")"
+ACTUAL_SPEC="$(git diff origin/master | sed -n -r "s/^\+[[:space:]]+spec_version: +([0-9]+),$/\1/p")"
+
+for package in $(git diff --name-only origin/master | grep -v "README.md$" | cut -d "/" -f1 | uniq); do
+    if [[ " ${PACKAGES_REQUIRE_BUMP_SPEC[@]} " =~ " ${package} " ]]; then
+        UPDATED_SPEC="true"
+        if [ "$SPEC_ON_MASTER" = "$ACTUAL_SPEC" ]; then
+            echo "    These files were changed:\n"
+            echo git diff --name-only origin/master | grep "^$package"
+            echo "\n    Spec version should be bumped!"
+            exit 1
+        fi
+    fi
+done
+
+if [ "$UPDATED_SPEC" != "true" ]; then
+    if [ "$SPEC_ON_MASTER" != "$ACTUAL_SPEC" ]; then
+        echo "Spec versions are different, but they shouldn't!"
+        exit 1
+    fi
+fi
+
+echo "Spec version is correct"
+exit 0
