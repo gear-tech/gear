1a8828d0df728536c586b4501e624556d45756d0
Dmitry Novikov <novikov.dm.al@gmail.com>
2022-02-23 14:30:59 +0300

    Check for only `.rs` files

--- scripts/check-spec.sh
+++ scripts/check-spec.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-PACKAGES_REQUIRE_BUMP_SPEC="common core core-backend node pallets runtime-interface lazy-pages"
+PACKAGES_REQUIRE_BUMP_SPEC="common core core-backend core-processor node pallets runtime-interface lazy-pages"
 
 SPEC_ON_MASTER="$(git diff origin/master | sed -n -r "s/^\-[[:space:]]+spec_version: +([0-9]+),$/\1/p")"
 ACTUAL_SPEC="$(git diff origin/master | sed -n -r "s/^\+[[:space:]]+spec_version: +([0-9]+),$/\1/p")"
@@ -15,12 +15,12 @@ if [ -z "$ACTUAL_SPEC" ]; then
     ACTUAL_SPEC="0"
 fi
 
-for package in $(git diff --name-only $CURRENT_BRANCH origin/master | grep -v "README.md$" | cut -d "/" -f1 | uniq); do
+for package in $(git diff --name-only origin/master | grep ".rs$" | cut -d "/" -f1 | uniq); do
     if [[ " ${PACKAGES_REQUIRE_BUMP_SPEC[@]} " =~ " ${package} " ]]; then
         UPDATED_SPEC="true"
         if [ "$SPEC_ON_MASTER" = "$ACTUAL_SPEC" ]; then
             printf "\n   These files were changed:\n\n"
-            echo "$(git diff --name-only origin/master | grep "^$package")"
+            echo "$(git diff --name-only origin/master | grep "^$package" | grep ".rs$" | sed 's/^/> /')"
             printf "\n   Spec version should be bumped!\n\n"
             exit 1
         fi
