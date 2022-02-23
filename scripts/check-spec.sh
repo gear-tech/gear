5c0f9dfe2fa7ce9c0fca9206b125d76f0e711071
Dmitry Novikov <novikov.dm.al@gmail.com>
2022-02-23 18:01:33 +0300

    Smart spec version checking (#716)
    * Check for only `.rs` files
    
    * Improve spec checking
    
    * Remove related path

--- scripts/check-spec.sh
+++ scripts/check-spec.sh
@@ -2,37 +2,50 @@
 
 set -e
 
-PACKAGES_REQUIRE_BUMP_SPEC="common core core-backend node pallets runtime-interface lazy-pages"
+SELF="$0"
+ROOT_DIR="$(cd "$(dirname "$SELF")"/.. && pwd)"
+
+check_spec() {
+    # $1 is master version, $2 is actual one and $3 changes requirement
+    version="$(echo $1 $2 $3 | awk '{
+        if ($3 == "true")
+            if ($1 > $2)
+                if ($1 - $2 >= 50 && $2 == 100)
+                    print "" # Ok case. We have reseted the network
+                else
+                    print "be bumped from",$1 # Should be bumped and be greater than master one
+            else if ($1 == $2)
+                print "be bumped from", $2 # We should bump for next version
+            else
+                print "" # Ok case. We had our spec bumped
+        else if ($1 - $2 >= 50 && $2 == 100)
+            print "" # Ok case. We have reseted the network
+        else if ($1 != $2)
+            print "equal",$1
+    }')"
+
+    if [ -z "$version" ]
+    then
+        printf "\n   Spec version is correct.\n\n"
+    else
+        printf "\n   Spec version should $version.\n\n"
+        exit 1
+    fi
+}
+
+PACKAGES_REQUIRE_BUMP_SPEC="common core core-backend core-processor node pallets runtime-interface lazy-pages"
 
 SPEC_ON_MASTER="$(git diff origin/master | sed -n -r "s/^\-[[:space:]]+spec_version: +([0-9]+),$/\1/p")"
-ACTUAL_SPEC="$(git diff origin/master | sed -n -r "s/^\+[[:space:]]+spec_version: +([0-9]+),$/\1/p")"
+ACTUAL_SPEC="$(cat $ROOT_DIR/runtime/src/lib.rs | grep "spec_version: " | awk -F " " '{print substr($2, 1, length($2)-1)}')"
 
 if [ -z "$SPEC_ON_MASTER" ]; then
-    SPEC_ON_MASTER="0"
+    SPEC_ON_MASTER=$ACTUAL_SPEC
 fi
 
-if [ -z "$ACTUAL_SPEC" ]; then
-    ACTUAL_SPEC="0"
-fi
-
-for package in $(git diff --name-only $CURRENT_BRANCH origin/master | grep -v "README.md$" | cut -d "/" -f1 | uniq); do
+for package in $(git diff --name-only origin/master | grep ".rs$" | cut -d "/" -f1 | uniq); do
     if [[ " ${PACKAGES_REQUIRE_BUMP_SPEC[@]} " =~ " ${package} " ]]; then
-        UPDATED_SPEC="true"
-        if [ "$SPEC_ON_MASTER" = "$ACTUAL_SPEC" ]; then
-            printf "\n   These files were changed:\n\n"
-            echo "$(git diff --name-only origin/master | grep "^$package")"
-            printf "\n   Spec version should be bumped!\n\n"
-            exit 1
-        fi
+        CHANGES="true"
     fi
 done
 
-if [ "$UPDATED_SPEC" != "true" ]; then
-    if [ "$SPEC_ON_MASTER" != "$ACTUAL_SPEC" ]; then
-        printf "\n   Spec versions are different, but they shouldn't!\n\n"
-        exit 1
-    fi
-fi
-
-printf "\n   Spec version is correct!\n\n"
-exit 0
+check_spec "$SPEC_ON_MASTER" "$ACTUAL_SPEC" "$CHANGES"
