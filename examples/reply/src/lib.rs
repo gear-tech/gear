f2f64cd2155761630b82751b563158cb881dfc0c
Sudorgin Dmitry <dimasikme@yandex.ru>
2022-04-13 18:04:47 +0300

    sd task #825 reply test syntax fix

--- examples/reply/src/lib.rs
+++ examples/reply/src/lib.rs
@@ -7,7 +7,7 @@ pub unsafe extern "C" fn handle() {
     debug!("Starting reply with gas test");
     msg::reply_with_gas(b"reply with gas message", 42, 0).unwrap();
 
-    debud!("Starting reply commit with gas test");
+    debug!("Starting reply commit with gas test");
     msg::reply_push(b"Part 1").unwrap();
     msg::reply_push(b"Part 2").unwrap();
     msg::reply_commit_with_gas(42, 0).unwrap();
