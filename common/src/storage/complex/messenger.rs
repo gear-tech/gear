55a1aa0a310a2ce6641a50255c23da1bb8865b7a
Dmitry Novikov <novikov.dm.al@gmail.com>
2022-05-15 18:55:45 +0300

    Add licence and expand macros

--- common/src/storage/complex/messenger.rs
+++ common/src/storage/complex/messenger.rs
@@ -1,7 +1,26 @@
-use crate::storage::complex::MailboxError;
-use crate::storage::complex::Queue;
-use crate::storage::complicated::{Counter, LinkedListError, Toggler};
-use crate::storage::primitives::{Counted, IterableMap};
+// This file is part of Gear.
+
+// Copyright (C) 2022 Gear Technologies Inc.
+// SPDX-License-Identifier: GPL-3.0-or-later WITH Classpath-exception-2.0
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+use crate::storage::{
+    complex::{MailboxError, Queue},
+    complicated::{Counter, LinkedListError, Toggler},
+    primitives::{Counted, IterableMap},
+};
 use core::fmt::Debug;
 
 /// Message processing centralized behaviour.
--- common/src/storage/primitives/map.rs
+++ common/src/storage/primitives/map.rs
@@ -1,3 +1,21 @@
+// This file is part of Gear.
+
+// Copyright (C) 2022 Gear Technologies Inc.
+// SPDX-License-Identifier: GPL-3.0-or-later WITH Classpath-exception-2.0
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
 pub trait MapStorage {
     type Key;
     type Value;
@@ -27,6 +45,9 @@ pub trait MapStorage {
 #[macro_export]
 macro_rules! wrap_storage_map {
     (storage: $storage: ident, name: $name: ident, key: $key: ty, value: $val: ty) => {
+        wrap_storage_map!(storage: $storage, name: $name, key: $key, value: $val, counted None);
+    };
+    (storage: $storage: ident, name: $name: ident, key: $key: ty, value: $val: ty, counted $($rm_arg:expr)?) => {
         pub struct $name<T>(PhantomData<T>);
 
         impl<T: crate::Config> MapStorage for $name<T> {
@@ -59,7 +80,7 @@ macro_rules! wrap_storage_map {
             }
 
             fn remove_all() {
-                $storage::<T>::remove_all(None);
+                $storage::<T>::remove_all($($rm_arg)?);
             }
 
             fn take(key: Self::Key) -> Option<Self::Value> {
@@ -73,45 +94,13 @@ macro_rules! wrap_storage_map {
 #[macro_export]
 macro_rules! wrap_counted_storage_map {
     (storage: $storage: ident, name: $name: ident, key: $key: ty, value: $val: ty, length: $len: ty) => {
-        pub struct $name<T>(PhantomData<T>);
-
-        impl<T: crate::Config> MapStorage for $name<T> {
-            type Key = $key;
-            type Value = $val;
-
-            fn contains_key(key: &Self::Key) -> bool {
-                $storage::<T>::contains_key(key)
-            }
-
-            fn get(key: &Self::Key) -> Option<Self::Value> {
-                $storage::<T>::get(key)
-            }
-
-            fn insert(key: Self::Key, value: Self::Value) {
-                $storage::<T>::insert(key, value)
-            }
-
-            fn mutate<R, F: FnOnce(&mut Option<Self::Value>) -> R>(key: Self::Key, f: F) -> R {
-                $storage::<T>::mutate(key, f)
-            }
-
-            fn mutate_values<F: FnMut(Self::Value) -> Self::Value>(mut f: F) {
-                let f = |v| Some(f(v));
-                $storage::<T>::translate_values(f)
-            }
-
-            fn remove(key: Self::Key) {
-                $storage::<T>::remove(key)
-            }
-
-            fn remove_all() {
-                $storage::<T>::remove_all()
-            }
-
-            fn take(key: Self::Key) -> Option<Self::Value> {
-                $storage::<T>::take(key)
-            }
-        }
+        $crate::wrap_storage_map!(
+            storage: $storage,
+            name: $name,
+            key: $key,
+            value: $val,
+            counted
+        );
 
         impl<T: crate::Config> Counted for $name<T> {
             type Length = $len;
