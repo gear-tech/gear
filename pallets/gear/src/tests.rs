9803c767d0a668048ab746516bfa2ebdc066c442
Gregory Sobol <grishasobol@mail.ru>
2022-05-17 12:57:02 +0300

    clippy

--- pallets/gear/src/tests.rs
+++ pallets/gear/src/tests.rs
@@ -24,7 +24,7 @@ use demo_mul_by_const::WASM_BINARY as MUL_CONST_WASM_BINARY;
 use demo_program_factory::{CreateProgram, WASM_BINARY as PROGRAM_FACTORY_WASM_BINARY};
 use frame_support::{assert_noop, assert_ok};
 use frame_system::Pallet as SystemPallet;
-use gear_core::{code::Code, ids::CodeId, memory::PageBuf};
+use gear_core::{code::Code, ids::CodeId};
 use pallet_balances::{self, Pallet as BalancesPallet};
 
 use super::{
