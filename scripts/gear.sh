3981f45830a519b68fce27be8767823f2e3497d5
Gregory Sobol <grishasobol@mail.ru>
2022-05-28 19:02:56 +0300

    fix problems in Makefile and scripts (#980)

--- scripts/gear.sh
+++ scripts/gear.sh
@@ -304,7 +304,7 @@ case "$COMMAND" in
 
       rtest)
         header "Running node runtime testsuite"
-        rtest "$ROOT_DIR"; ;;
+        rtest "$ROOT_DIR" "$@"; ;;
 
       pallet)
         header "Running pallet-gear tests"
