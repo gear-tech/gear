5857745a8a5c0b4aebd8c75871bf84d43e46d44b
Arsenii Lyashenko <arsenylyashenko.3@gmail.com>
2022-05-31 21:17:52 +0300

    Clean up errors (#962)
    Co-authored-by: Dmitry Novikov <novikov.dm.al@gmail.com>

--- core/src/message/init.rs
+++ core/src/message/init.rs
@@ -19,8 +19,8 @@
 use crate::{
     ids::{CodeId, MessageId, ProgramId},
     message::{
-        Dispatch, DispatchKind, GasLimit, Message, Payload, Salt, StoredDispatch, StoredMessage,
-        Value,
+        Dispatch, DispatchKind, GasLimit, Message, Packet, Payload, Salt, StoredDispatch,
+        StoredMessage, Value,
     },
 };
 use codec::{Decode, Encode};
@@ -171,19 +171,18 @@ impl InitPacket {
     pub fn salt(&self) -> &[u8] {
         self.salt.as_ref()
     }
+}
 
-    /// Packet payload reference.
-    pub fn payload(&self) -> &[u8] {
+impl Packet for InitPacket {
+    fn payload(&self) -> &[u8] {
         self.payload.as_ref()
     }
 
-    /// Packet optional gas limit.
-    pub fn gas_limit(&self) -> Option<GasLimit> {
+    fn gas_limit(&self) -> Option<GasLimit> {
         self.gas_limit
     }
 
-    /// Packet value.
-    pub fn value(&self) -> Value {
+    fn value(&self) -> Value {
         self.value
     }
 }
