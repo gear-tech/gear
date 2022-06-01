5c121e9aab4a23562af9c93e0c748f598e03a839
clearloop <26088946+clearloop@users.noreply.github.com>
2022-06-02 00:37:41 +0800

    chore(examples): correct error messages in init-wait-reply-exit

--- examples/binaries/init-wait-reply-exit/src/code.rs
+++ examples/binaries/init-wait-reply-exit/src/code.rs
@@ -6,7 +6,8 @@ use gstd::{
 #[gstd::async_init]
 async fn init() {
     msg::send_and_wait_for_reply::<Vec<u8>, Vec<u8>>(msg::source(), vec![], 0)
-        .expect("get reply failed")
-        .await;
+        .expect("send message failed")
+        .await
+        .expect("get reply failed");
     exec::exit(msg::source());
 }
