2c3fba6c72fd1c7c3b40a5273b835cc93ede345f
Dmitry Novikov <novikov.dm.al@gmail.com>
2022-06-03 01:12:56 +0300

    License headers

--- common/codegen/src/lib.rs
+++ common/codegen/src/lib.rs
@@ -1,14 +1,33 @@
+// This file is part of Gear.
+
+// Copyright (C) 2021-2022 Gear Technologies Inc.
+// SPDX-License-Identifier: GPL-3.0-or-later WITH Classpath-exception-2.0
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
 #![no_std]
 
+#[macro_use]
+extern crate quote;
 extern crate alloc;
 extern crate proc_macro;
 extern crate syn;
-#[macro_use]
-extern crate quote;
 
 use alloc::string::ToString;
 use proc_macro::TokenStream;
 
+/// Derive macro for default implementation of RuntimeReason.
 #[proc_macro_derive(RuntimeReason)]
 pub fn derive_runtime_reason(input: TokenStream) -> TokenStream {
     let s = input.to_string();
@@ -22,6 +41,7 @@ fn impl_runtime_reason(ast: &syn::DeriveInput) -> quote::Tokens {
     quote! { impl RuntimeReason for #name {} }
 }
 
+/// Derive macro for default implementation of SystemReason.
 #[proc_macro_derive(SystemReason)]
 pub fn derive_system_reason(input: TokenStream) -> TokenStream {
     let s = input.to_string();
