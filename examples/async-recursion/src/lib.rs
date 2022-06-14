b7171bb3cda01d73821d5b8ff0ccb688ba33f566
Gregory Sobol <grishasobol@mail.ru>
2022-06-14 13:06:01 +0300

    Update examples/async-recursion/src/lib.rs
    Co-authored-by: Shamil <66209982+shamilsan@users.noreply.github.com>

--- examples/async-recursion/src/lib.rs
+++ examples/async-recursion/src/lib.rs
@@ -17,7 +17,7 @@ fn decode_hex(s: &str) -> Result<Vec<u8>, ParseIntError> {
 pub unsafe extern "C" fn init() {
     let input = String::from_utf8(msg::load_bytes()).expect("Invalid message: should be utf-8");
     DEST = ActorId::from_slice(
-        &decode_hex(&input).expect("INTIALIZATION FAILED: INVALID PROGRAM ID"),
+        &decode_hex(&input).expect("Initialization failed: invalid program ID"),
     )
     .expect("Unable to create ActorId");
 }
