ec43a455eceeb73b007e5965335bd94568703a14
Vadim Smirnov <vs@gear-tech.io>
2022-06-20 17:19:05 +0300

    add handle() to demo-exit-code

--- examples/exit-code/src/lib.rs
+++ examples/exit-code/src/lib.rs
@@ -5,6 +5,9 @@ use gstd::{msg, prelude::ToString, ActorId};
 static mut HOST: ActorId = ActorId::new([0u8; 32]);
 
 #[no_mangle]
+pub unsafe extern "C" fn handle() {}
+
+#[no_mangle]
 pub unsafe extern "C" fn handle_reply() {
     msg::send_bytes(HOST, msg::exit_code().to_string(), 0).unwrap();
 }
