b30895f5dd852cabeb3ef04b48460b26691bec5e
Vadim Smirnov <vs@gear-tech.io>
2022-06-21 15:41:41 +0300

    add demo-exit-code comment

--- examples/exit-code/src/lib.rs
+++ examples/exit-code/src/lib.rs
@@ -4,6 +4,10 @@ use gstd::{msg, prelude::ToString, ActorId};
 
 static mut HOST: ActorId = ActorId::new([0u8; 32]);
 
+// It was containing the only handle_reply previously
+// what is just for testing and isn't valid at all
+// cause we can't receive reply on message if we never send it.
+// For this demo in real conditions handle_reply is unreachable.
 #[no_mangle]
 pub unsafe extern "C" fn handle() {}
 
