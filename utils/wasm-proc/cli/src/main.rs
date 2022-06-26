d692c58c90b7a4f65c8c51598de6906597cb491e
Arsenii Lyashenko <arsenylyashenko.3@gmail.com>
2022-06-26 22:16:34 +0300

    Fix missing copyright and license ID

--- utils/wasm-proc/cli/src/main.rs
+++ utils/wasm-proc/cli/src/main.rs
@@ -1,5 +1,8 @@
 // This file is part of Gear.
 
+// Copyright (C) 2021-2022 Gear Technologies Inc.
+// SPDX-License-Identifier: GPL-3.0-or-later WITH Classpath-exception-2.0
+
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
