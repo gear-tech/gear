2e467453d701a731fcaff87aa1a959c955078bfa
Georgy Shepelev <geo.shepelev@gmail.com>
2022-07-12 10:26:08 +0400

    fix review remarks

--- scripts/performance-check.sh
+++ scripts/performance-check.sh
@@ -30,81 +30,59 @@ if [ -z "$CURRENT_BRANCH" ]; then
     CURRENT_BRANCH=`git branch --show-current`
 fi
 
-echo 'CURRENT_BRANCH = '$CURRENT_BRANCH' , COUNT = '$COUNT', ROOT_DIR = '$ROOT_DIR
+echo 'CURRENT_BRANCH = '$CURRENT_BRANCH' , COUNT = '$COUNT', ROOT_DIR = '$ROOT_DIR', BOXPLOT = '$BOXPLOT
 
 WARMUP_COUNT=3
 
-git checkout $MAIN_BRANCH
+collect_data() {
+    git checkout $1
 
-make gear-release
-make examples
+    make gear-release
+    make examples
 
-mkdir -p $ROOT_DIR/target/tests/
-mkdir -p $ROOT_DIR/target/tests-output/
-for i in `seq 1 $WARMUP_COUNT`; do
-    $ROOT_DIR/scripts/gear.sh test gear --release > $ROOT_DIR/target/tests-output/$i 2>&1
-done
+    rm -rf "$ROOT_DIR/target/tests/"
+    mkdir -p "$ROOT_DIR/target/tests/"
+    mkdir -p "$ROOT_DIR/target/tests-output/"
+    for i in `seq 1 $WARMUP_COUNT`; do
+        "$ROOT_DIR/scripts/gear.sh" test gear --release > "$ROOT_DIR/target/tests-output/$i" 2>&1
+    done
 
-for i in `seq 1 $COUNT`; do
-    echo $i
-    $ROOT_DIR/scripts/gear.sh test gear --release > $ROOT_DIR/target/tests-output/$i 2>&1
-    mv $ROOT_DIR/target/nextest/ci/junit.xml $ROOT_DIR/target/tests/$i
-done
+    for i in `seq 1 $COUNT`; do
+        echo $i
+        "$ROOT_DIR/scripts/gear.sh" test gear --release > "$ROOT_DIR/target/tests-output/$i" 2>&1
+        mv "$ROOT_DIR/target/nextest/ci/junit.xml" "$ROOT_DIR/target/tests/$i"
+    done
 
-mkdir -p $ROOT_DIR/target/runtime-tests/
-mkdir -p $ROOT_DIR/target/runtime-tests-output/
-for i in `seq 1 $WARMUP_COUNT`; do
-    $ROOT_DIR/scripts/gear.sh test rtest > $ROOT_DIR/target/runtime-tests-output/$i 2>&1
-done
+    rm -rf "$ROOT_DIR/target/runtime-tests/"
+    mkdir -p "$ROOT_DIR/target/runtime-tests/"
+    mkdir -p "$ROOT_DIR/target/runtime-tests-output/"
+    for i in `seq 1 $WARMUP_COUNT`; do
+        "$ROOT_DIR/scripts/gear.sh" test rtest > "$ROOT_DIR/target/runtime-tests-output/$i" 2>&1
+    done
 
-for i in `seq 1 $COUNT`; do
-    echo $i
-    $ROOT_DIR/scripts/gear.sh test rtest > $ROOT_DIR/target/runtime-tests-output/$i 2>&1
-    mv $ROOT_DIR/target/runtime-test-junit.xml $ROOT_DIR/target/runtime-tests/$i
-done
+    for i in `seq 1 $COUNT`; do
+        echo $i
+        "$ROOT_DIR/scripts/gear.sh" test rtest > "$ROOT_DIR/target/runtime-tests-output/$i" 2>&1
+        mv "$ROOT_DIR/target/runtime-test-junit.xml" "$ROOT_DIR/target/runtime-tests/$i"
+    done
 
-rm -rf $ROOT_DIR/target/main_branch/
-mkdir -p $ROOT_DIR/target/main_branch/
-cargo run --package regression-analysis --release -- collect-data --data-folder-path $ROOT_DIR/target/tests/ --output-path $ROOT_DIR/target/main_branch/pallet-tests.json
-cargo run --package regression-analysis --release -- collect-data --disable-filter --data-folder-path $ROOT_DIR/target/runtime-tests/ --output-path $ROOT_DIR/target/main_branch/runtime-tests.json
+    rm -rf "$2"
+    mkdir -p "$2"
+    cargo run --package regression-analysis --release -- collect-data --data-folder-path "$ROOT_DIR/target/tests/" --output-path "$2/pallet-tests.json"
+    cargo run --package regression-analysis --release -- collect-data --disable-filter --data-folder-path "$ROOT_DIR/target/runtime-tests/" --output-path "$2/runtime-tests.json"
+}
 
-git checkout $CURRENT_BRANCH
 
-make gear-release
-make examples
+collect_data $MAIN_BRANCH "$ROOT_DIR/target/main_branch"
 
-rm -rf $ROOT_DIR/target/tests/*
-for i in `seq 1 $WARMUP_COUNT`; do
-    $ROOT_DIR/scripts/gear.sh test gear --release > $ROOT_DIR/target/tests-output/$i 2>&1
-done
-
-for i in `seq 1 $COUNT`; do
-    echo $i
-    $ROOT_DIR/scripts/gear.sh test gear --release > $ROOT_DIR/target/tests-output/$i 2>&1
-    mv $ROOT_DIR/target/nextest/ci/junit.xml $ROOT_DIR/target/tests/$i
-done
-
-rm -rf $ROOT_DIR/target/runtime-tests/*
-for i in `seq 1 $WARMUP_COUNT`; do
-    $ROOT_DIR/scripts/gear.sh test rtest > $ROOT_DIR/target/runtime-tests-output/$i 2>&1
-done
-
-for i in `seq 1 $COUNT`; do
-    echo $i
-    $ROOT_DIR/scripts/gear.sh test rtest > $ROOT_DIR/target/runtime-tests-output/$i 2>&1
-    mv $ROOT_DIR/target/runtime-test-junit.xml $ROOT_DIR/target/runtime-tests/$i
-done
-
-rm -rf $ROOT_DIR/target/current_branch/
-mkdir -p $ROOT_DIR/target/current_branch/
-cargo run --package regression-analysis --release -- collect-data --data-folder-path $ROOT_DIR/target/tests/ --output-path $ROOT_DIR/target/current_branch/pallet-tests.json
-cargo run --package regression-analysis --release -- collect-data --disable-filter --data-folder-path $ROOT_DIR/target/runtime-tests/ --output-path $ROOT_DIR/target/current_branch/runtime-tests.json
+collect_data $CURRENT_BRANCH "$ROOT_DIR/target/current_branch"
 
 if [ "$BOXPLOT" != "0" ]; then
-    python3 $ROOT_DIR/scripts/performance-boxplot.py $ROOT_DIR/target/main_branch/pallet-tests.json $ROOT_DIR/target/current_branch/pallet-tests.json
-    mv ./results $ROOT_DIR/target/performance-pallet-tests
+    python3 "$ROOT_DIR/scripts/performance-boxplot.py" "$ROOT_DIR/target/main_branch/pallet-tests.json" "$ROOT_DIR/target/current_branch/pallet-tests.json"
+    rm -rf "$ROOT_DIR/target/performance-pallet-tests"
+    mv ./results "$ROOT_DIR/target/performance-pallet-tests"
 
-    python3 $ROOT_DIR/scripts/performance-boxplot.py $ROOT_DIR/target/main_branch/runtime-tests.json $ROOT_DIR/target/current_branch/runtime-tests.json
-    mv ./results $ROOT_DIR/target/performance-runtime-tests
+    python3 "$ROOT_DIR/scripts/performance-boxplot.py" "$ROOT_DIR/target/main_branch/runtime-tests.json" "$ROOT_DIR/target/current_branch/runtime-tests.json"
+    rm -rf "$ROOT_DIR/target/performance-runtime-tests"
+    mv ./results "$ROOT_DIR/target/performance-runtime-tests"
 fi
-
