d683bd3d4011e7f012725b8dbe12c5d4cbc23ee4
Sabaun Taraki <taraki.sabaun.rl@gmail.com>
2022-07-13 21:23:10 +0300

    Bump spec version

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -123,7 +123,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     // The version of the runtime specification. A full node will not attempt to use its native
     //   runtime in substitute for the on-chain Wasm runtime unless all of `spec_name`,
     //   `spec_version`, and `authoring_version` are the same between Wasm and native.
-    spec_version: 1290,
+    spec_version: 1300,
     impl_version: 1,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 1,
