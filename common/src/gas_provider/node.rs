64afdb68d5f0e878e199d15f19e4965ec8270ebe
Sabaun Taraki <taraki.sabaun.rl@gmail.com>
2022-07-24 09:11:22 +0300

    Move `consumed` field to specific node types, consequent changes

--- common/src/gas_provider/node.rs
+++ common/src/gas_provider/node.rs
@@ -19,93 +19,99 @@
 use super::*;
 use codec::MaxEncodedLen;
 
+/// Node of the ['Tree'] gas tree
 #[derive(Clone, Decode, Debug, Encode, MaxEncodedLen, TypeInfo, PartialEq, Eq)]
-pub enum GasNodeType<ExternalId, Id, Balance> {
+pub enum GasNode<ExternalId: Clone, Id: Clone, Balance: Zero + Clone> {
+    /// A root node for each gas tree. Usually created when a new gas-ful logic is started (i.e., user's message is sent).
     External {
         id: ExternalId,
         value: Balance,
         refs: ChildrenRefs,
+        consumed: bool,
     },
-    ReservedLocal {
-        id: ExternalId,
-        value: Balance,
-    },
+    /// A node created by "cutting" value from some other tree node. Such node types are independent and aren't
+    /// part of the tree structure (not node's parent, not node's child).
+    ReservedLocal { id: ExternalId, value: Balance },
+    /// A node, which is a part of the tree structure (can be a parent and/or a child). As well as `External` node,
+    /// it has an internal balance and can exist while being consumed (for more info read [`Tree::consume`] implementor docs).
+    /// However, it has a `parent` field pointing to the node from which that one was created.
     SpecifiedLocal {
         parent: Id,
         value: Balance,
         refs: ChildrenRefs,
+        consumed: bool,
     },
-    UnspecifiedLocal {
-        parent: Id,
-    },
+    /// Pretty same as `SpecifiedLocal`, but doesn't have internal balance, so relies on it `parent`. Also such
+    /// nodes don't have children references.
+    UnspecifiedLocal { parent: Id },
 }
 
+/// Children references convenience struct
 #[derive(Clone, Copy, Default, Decode, Debug, Encode, MaxEncodedLen, TypeInfo, PartialEq, Eq)]
 pub struct ChildrenRefs {
     spec_refs: u32,
     unspec_refs: u32,
 }
 
-impl<ExternalId, Id, Balance> GasNodeType<ExternalId, Id, Balance> {
-    pub(crate) fn is_external(&self) -> bool {
-        matches!(self, GasNodeType::External { .. })
-    }
-
-    pub(crate) fn is_specified_local(&self) -> bool {
-        matches!(self, GasNodeType::SpecifiedLocal { .. })
-    }
-
-    pub(crate) fn is_unspecified_local(&self) -> bool {
-        matches!(self, GasNodeType::UnspecifiedLocal { .. })
-    }
-
-    pub(crate) fn is_reserved_local(&self) -> bool {
-        matches!(self, GasNodeType::ReservedLocal { .. })
-    }
-}
-
-#[derive(Clone, Decode, Debug, Encode, MaxEncodedLen, TypeInfo, PartialEq, Eq)]
-pub struct GasNode<ExternalId: Clone, Id: Clone, Balance: Zero + Clone> {
-    pub inner: GasNodeType<ExternalId, Id, Balance>,
-    pub consumed: bool,
-}
-
 impl<ExternalId: Clone, Id: Clone + Copy, Balance: Zero + Clone + Copy>
     GasNode<ExternalId, Id, Balance>
 {
+    /// Creates a new `GasNode::External` root node for a new tree.
     pub fn new(origin: ExternalId, value: Balance) -> Self {
-        Self {
-            inner: GasNodeType::External {
-                id: origin,
-                value,
-                refs: Default::default(),
-            },
+        Self::External {
+            id: origin,
+            value,
+            refs: Default::default(),
             consumed: false,
         }
     }
 
+    /// Increases node's spec refs, if it can have any
     pub fn increase_spec_refs(&mut self) {
         self.adjust_refs(true, true);
     }
 
+    /// Decreases node's spec refs, if it can have any
     pub fn decrease_spec_refs(&mut self) {
         self.adjust_refs(false, true);
     }
 
+    /// Increases node's unspec refs, if it can have any
     pub fn increase_unspec_refs(&mut self) {
         self.adjust_refs(true, false);
     }
 
+    /// Decreases node's unspec refs, if it can have any
     pub fn decrease_unspec_refs(&mut self) {
         self.adjust_refs(false, false);
     }
 
+    /// Get's a mutable access to node's inner gas balance, if it can have any
     pub fn inner_value_mut(&mut self) -> Option<&mut Balance> {
-        match self.inner {
-            GasNodeType::External { ref mut value, .. } => Some(value),
-            GasNodeType::ReservedLocal { ref mut value, .. } => Some(value),
-            GasNodeType::SpecifiedLocal { ref mut value, .. } => Some(value),
-            GasNodeType::UnspecifiedLocal { .. } => None,
+        match self {
+            Self::External { ref mut value, .. }
+            | Self::ReservedLocal { ref mut value, .. }
+            | Self::SpecifiedLocal { ref mut value, .. } => Some(value),
+            Self::UnspecifiedLocal { .. } => None,
+        }
+    }
+
+    /// Marks the node as consumed, if it has the flag
+    pub fn mark_consumed(&mut self) {
+        if let Self::External { consumed, .. } | Self::SpecifiedLocal { consumed, .. } = self {
+            *consumed = true;
+        }
+    }
+
+    /// Returns whether the node is marked consumed or not
+    ///
+    /// Only `GasNode::External` and `GasNode::SpecifiedLocal` can be marked consumed and not deleted.
+    /// For more info read [`Tree::consume`] implementor docs.
+    pub fn is_consumed(&self) -> bool {
+        if let Self::External { consumed, .. } | Self::SpecifiedLocal { consumed, .. } = self {
+            *consumed
+        } else {
+            false
         }
     }
 
@@ -117,65 +123,88 @@ impl<ExternalId: Clone, Id: Clone + Copy, Balance: Zero + Clone + Copy>
     ///
     /// Patron nodes are those on which other nodes of the tree rely (including the self node).
     pub fn is_patron(&self) -> bool {
-        if let GasNodeType::External { refs, .. } | GasNodeType::SpecifiedLocal { refs, .. } =
-            self.inner
+        if let Self::External { refs, consumed, .. } | Self::SpecifiedLocal { refs, consumed, .. } =
+            self
         {
-            !self.consumed || refs.unspec_refs != 0
+            !consumed || refs.unspec_refs != 0
         } else {
             false
         }
     }
 
+    /// Returns node's inner gas balance, if it can have any
     pub fn inner_value(&self) -> Option<Balance> {
-        match self.inner {
-            GasNodeType::External { value, .. } => Some(value),
-            GasNodeType::ReservedLocal { value, .. } => Some(value),
-            GasNodeType::SpecifiedLocal { value, .. } => Some(value),
-            GasNodeType::UnspecifiedLocal { .. } => None,
+        match self {
+            Self::External { value, .. }
+            | Self::ReservedLocal { value, .. }
+            | Self::SpecifiedLocal { value, .. } => Some(*value),
+            Self::UnspecifiedLocal { .. } => None,
         }
     }
 
+    /// Returns node's parent, if it can have any.
+    ///
+    /// That is, `GasNode::External` and `GasNode::ReservedLocal` nodes
+    /// don't have a parent, so a `None` is returned if the function is
+    /// called on them.
     pub fn parent(&self) -> Option<Id> {
-        match self.inner {
-            GasNodeType::External { .. } | GasNodeType::ReservedLocal { .. } => None,
-            GasNodeType::SpecifiedLocal { parent, .. }
-            | GasNodeType::UnspecifiedLocal { parent } => Some(parent),
+        match self {
+            Self::External { .. } | Self::ReservedLocal { .. } => None,
+            Self::SpecifiedLocal { parent, .. } | Self::UnspecifiedLocal { parent } => {
+                Some(*parent)
+            }
         }
     }
 
+    /// Returns node's total refs
     pub fn refs(&self) -> u32 {
         self.spec_refs().saturating_add(self.unspec_refs())
     }
 
+    /// Returns node's spec refs
     pub fn spec_refs(&self) -> u32 {
-        match self.inner {
-            GasNodeType::External { refs, .. } | GasNodeType::SpecifiedLocal { refs, .. } => {
-                refs.spec_refs
-            }
+        match self {
+            Self::External { refs, .. } | Self::SpecifiedLocal { refs, .. } => refs.spec_refs,
             _ => 0,
         }
     }
 
+    /// Returns node's unspec refs
     pub fn unspec_refs(&self) -> u32 {
-        match self.inner {
-            GasNodeType::External { refs, .. } | GasNodeType::SpecifiedLocal { refs, .. } => {
-                refs.unspec_refs
-            }
+        match self {
+            Self::External { refs, .. } | Self::SpecifiedLocal { refs, .. } => refs.unspec_refs,
             _ => 0,
         }
+    }
+
+    /// Returns whether the node is of `External` type
+    pub(crate) fn is_external(&self) -> bool {
+        matches!(self, Self::External { .. })
+    }
+
+    /// Returns whether the node is of `SpecifiedLocal` type
+    pub(crate) fn is_specified_local(&self) -> bool {
+        matches!(self, Self::SpecifiedLocal { .. })
+    }
+
+    /// Returns whether the node is of `UnspecifiedLocal` type
+    pub(crate) fn is_unspecified_local(&self) -> bool {
+        matches!(self, Self::UnspecifiedLocal { .. })
+    }
+
+    /// Returns whether the node is of `ReservedLocal` type
+    pub(crate) fn is_reserved_local(&self) -> bool {
+        matches!(self, Self::ReservedLocal { .. })
     }
 
     fn adjust_refs(&mut self, increase: bool, spec: bool) {
-        let refs = match &mut self.inner {
-            GasNodeType::External { refs, .. } | GasNodeType::SpecifiedLocal { refs, .. } => refs,
-            _ => return,
-        };
-
-        match (increase, spec) {
-            (true, true) => refs.spec_refs = refs.spec_refs.saturating_add(1),
-            (true, false) => refs.unspec_refs = refs.unspec_refs.saturating_add(1),
-            (false, true) => refs.spec_refs = refs.spec_refs.saturating_sub(1),
-            (false, false) => refs.unspec_refs = refs.unspec_refs.saturating_sub(1),
+        if let Self::External { refs, .. } | Self::SpecifiedLocal { refs, .. } = self {
+            match (increase, spec) {
+                (true, true) => refs.spec_refs = refs.spec_refs.saturating_add(1),
+                (true, false) => refs.unspec_refs = refs.unspec_refs.saturating_add(1),
+                (false, true) => refs.spec_refs = refs.spec_refs.saturating_sub(1),
+                (false, false) => refs.unspec_refs = refs.unspec_refs.saturating_sub(1),
+            }
         }
     }
 }
