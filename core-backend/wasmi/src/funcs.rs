e5746244856d26ef5cf7356016cef6cdfee42cce
Vadim Smirnov <vs@gear-tech.io>
2022-07-27 01:26:34 +0300

    typo

--- core-backend/wasmi/src/funcs.rs
+++ core-backend/wasmi/src/funcs.rs
@@ -1,6 +1,6 @@
 // This file is part of Gear.
 
-// Copyright (C) 2022 Gear Techn&&ologies Inc.
+// Copyright (C) 2022 Gear Technologies Inc.
 // SPDX-License-Identifier: GPL-3.0-or-later WITH Classpath-exception-2.0
 
 // This program is free software: you can redistribute it and/or modify
