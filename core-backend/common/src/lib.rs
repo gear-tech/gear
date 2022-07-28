f4fd702fa4ad3cd55b54582ef6a240343fe0e432
Vadim Smirnov <vs@gear-tech.io>
2022-07-28 13:07:03 +0400

    runtime trait

--- core-backend/common/src/lib.rs
+++ core-backend/common/src/lib.rs
@@ -23,6 +23,7 @@
 extern crate alloc;
 
 pub mod error_processor;
+pub mod funcs;
 
 mod utils;
 
@@ -31,8 +32,10 @@ use alloc::{
     string::String,
     vec::Vec,
 };
-use codec::{Decode, Encode};
+use codec::{Decode, Encode, MaxEncodedLen};
+use funcs::FuncError;
 use core::{fmt, ops::Deref};
+use error_processor::IntoExtError;
 use gear_core::{
     env::Ext,
     gas::GasAmount,
@@ -120,6 +123,40 @@ pub trait IntoExtInfo {
     fn last_error(&self) -> Option<&ExtError>;
 
     fn trap_explanation(&self) -> Option<TrapExplanation>;
+}
+
+pub trait RuntimeCtx<E>
+where
+    E: Ext + IntoExtInfo + 'static,
+    E::Error: AsTerminationReason
+        + IntoExtError,
+{
+    fn alloc(&mut self, pages: u32)
+        -> Result<gear_core::memory::WasmPageNumber, E::Error>;
+
+    fn ext(&mut self) -> &mut E;
+
+    /// Read designated chunk from the sandbox memory.
+    ///
+    /// Returns `Err` if one of the following conditions occurs:
+    ///
+    /// - requested buffer is not within the bounds of the sandbox memory.
+    fn read_memory(&self, ptr: u32, len: u32) -> Result<Vec<u8>, MemoryError>;
+
+    /// Read designated chunk from the sandbox memory into the supplied buffer.
+    ///
+    /// Returns `Err` if one of the following conditions occurs:
+    ///
+    /// - requested buffer is not within the bounds of the sandbox memory.
+    fn read_memory_into_buf(&self, ptr: u32, buf: &mut [u8]) -> Result<(), MemoryError>;
+
+    /// Reads and decodes a type with a size fixed at compile time from contract memory.
+    fn read_memory_as<D: Decode + MaxEncodedLen>(&self, ptr: u32) -> Result<D, MemoryError>;
+
+    /// Write the given buffer and its length to the designated locations in sandbox memory.
+    //
+    /// `out_ptr` is the location in sandbox memory where `buf` should be written to.
+    fn write_output(&mut self, out_ptr: u32, buf: &[u8]) -> Result<(), MemoryError>;
 }
 
 pub struct BackendReport<T> {
--- core-backend/sandbox/src/runtime.rs
+++ core-backend/sandbox/src/runtime.rs
@@ -1,11 +1,14 @@
 use alloc::{vec, vec::Vec};
 use codec::{Decode, DecodeAll, MaxEncodedLen};
+use gear_backend_common::{
+    error_processor::IntoExtError, funcs::FuncError, AsTerminationReason, IntoExtInfo, RuntimeCtx,
+};
 use gear_core::env::Ext;
 
 use gear_core_errors::MemoryError;
 use sp_sandbox::{default_executor::Memory as DefaultExecutorMemory, SandboxMemory};
 
-use crate::{funcs::FuncError, MemoryWrap};
+use crate::MemoryWrap;
 
 pub(crate) struct Runtime<'a, E: Ext> {
     pub ext: &'a mut E,
@@ -14,16 +17,21 @@ pub(crate) struct Runtime<'a, E: Ext> {
     pub err: FuncError<E::Error>,
 }
 
-impl<'a, E> Runtime<'a, E>
+impl<'a, E> RuntimeCtx<E> for Runtime<'a, E>
 where
-    E: Ext + 'a,
+    E: Ext + IntoExtInfo + 'static,
+    E::Error: AsTerminationReason + IntoExtError,
 {
     /// Allocate new pages in module memory.
-    pub fn alloc(
+    fn alloc(
         &mut self,
         pages: u32,
     ) -> Result<gear_core::memory::WasmPageNumber, <E as Ext>::Error> {
         self.ext.alloc(pages.into(), self.memory_wrap)
+    }
+
+    fn ext(&mut self) -> &mut E {
+        self.ext
     }
 
     /// Read designated chunk from the sandbox memory.
@@ -31,7 +39,7 @@ where
     /// Returns `Err` if one of the following conditions occurs:
     ///
     /// - requested buffer is not within the bounds of the sandbox memory.
-    pub fn read_memory(&self, ptr: u32, len: u32) -> Result<Vec<u8>, MemoryError> {
+    fn read_memory(&self, ptr: u32, len: u32) -> Result<Vec<u8>, MemoryError> {
         let mut buf = vec![0u8; len as usize];
         self.memory
             .get(ptr, buf.as_mut_slice())
@@ -44,14 +52,14 @@ where
     /// Returns `Err` if one of the following conditions occurs:
     ///
     /// - requested buffer is not within the bounds of the sandbox memory.
-    pub fn read_memory_into_buf(&self, ptr: u32, buf: &mut [u8]) -> Result<(), MemoryError> {
+    fn read_memory_into_buf(&self, ptr: u32, buf: &mut [u8]) -> Result<(), MemoryError> {
         self.memory
             .get(ptr, buf)
             .map_err(|_| MemoryError::OutOfBounds)
     }
 
     /// Reads and decodes a type with a size fixed at compile time from contract memory.
-    pub fn read_memory_as<D: Decode + MaxEncodedLen>(&self, ptr: u32) -> Result<D, MemoryError> {
+    fn read_memory_as<D: Decode + MaxEncodedLen>(&self, ptr: u32) -> Result<D, MemoryError> {
         let buf = self.read_memory(ptr, D::max_encoded_len() as u32)?;
         let decoded = D::decode_all(&mut &buf[..]).map_err(|_| MemoryError::MemoryAccessError)?;
         Ok(decoded)
@@ -60,7 +68,7 @@ where
     /// Write the given buffer and its length to the designated locations in sandbox memory.
     //
     /// `out_ptr` is the location in sandbox memory where `buf` should be written to.
-    pub fn write_output(&mut self, out_ptr: u32, buf: &[u8]) -> Result<(), MemoryError> {
+    fn write_output(&mut self, out_ptr: u32, buf: &[u8]) -> Result<(), MemoryError> {
         self.memory
             .set(out_ptr, buf)
             .map_err(|_| MemoryError::OutOfBounds)?;
