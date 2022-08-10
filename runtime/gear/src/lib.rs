3d1f9c3b50bf4767f2261075aacd3ca1e04a3fe6
Eugene Kovalev <eugene.a.kovalev@gmail.com>
2022-08-10 21:51:34 +0800

    Ditch aux sub-directory to flatten the pallets structure

--- runtime/gear/src/lib.rs
+++ runtime/gear/src/lib.rs
@@ -89,7 +89,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     impl_name: create_runtime_str!("gear-node"),
     apis: RUNTIME_API_VERSIONS,
     authoring_version: 1,
-    spec_version: 1510,
+    spec_version: 1520,
     impl_version: 1,
     transaction_version: 1,
     state_version: 1,
