0ff42a84a4a83ef03b01d8ad2ae6c151cb6f1d3b
Gregory Sobol <grishasobol@mail.ru>
2022-08-16 20:16:35 +0300

    fix wrong block panic in node (#1318)

--- runtime/gear/src/lib.rs
+++ runtime/gear/src/lib.rs
@@ -89,7 +89,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     impl_name: create_runtime_str!("gear-node"),
     apis: RUNTIME_API_VERSIONS,
     authoring_version: 1,
-    spec_version: 1540,
+    spec_version: 1550,
     impl_version: 1,
     transaction_version: 1,
     state_version: 1,
