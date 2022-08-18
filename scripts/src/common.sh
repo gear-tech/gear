0c84a1b37bb7fa998d7c282a1eeea66271433732
Dmitry Novikov <novikov.dm.al@gmail.com>
2022-08-18 18:33:40 +0300

    Cmt

--- scripts/src/common.sh
+++ scripts/src/common.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env sh
 
+bold() {
+  tput bold
+}
+
+normal() {
+  tput sgr0
+}
+
+header() {
+  bold && printf "\n  >> $1\n" && normal
+}
+
 parse_yamls_list() {
   has_yamls=$(echo "$1" | grep "yamls=" || true)
 
