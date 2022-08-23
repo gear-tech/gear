37354cd2c2b11837b3609038861c9137e76df434
Shamil <shamil@shamil.ru>
2022-08-23 18:25:07 +0300

    Minor fixes in spec

--- node/src/chain_spec.rs
+++ node/src/chain_spec.rs
@@ -111,7 +111,7 @@ pub fn development_config() -> ChainSpec {
         None,
         None,
         Extensions {
-            relay_chain: "rococo_local_testnet".into(), // You MUST set this to the correct network!
+            relay_chain: "rococo_gear_testnet".into(), // You MUST set this to the correct network!
             para_id: 2000,
         },
     )
@@ -157,7 +157,7 @@ pub fn local_testnet_config() -> ChainSpec {
                     get_account_id_from_seed::<sr25519::Public>("Eve//stash"),
                     get_account_id_from_seed::<sr25519::Public>("Ferdie//stash"),
                 ],
-                1000.into(),
+                2000.into(),
             )
         },
         // Bootnodes
@@ -172,7 +172,7 @@ pub fn local_testnet_config() -> ChainSpec {
         Some(properties),
         // Extensions
         Extensions {
-            relay_chain: "rococo_local_testnet".into(), // You MUST set this to the correct network!
+            relay_chain: "rococo_gear_testnet".into(), // You MUST set this to the correct network!
             para_id: 2000,
         },
     )
