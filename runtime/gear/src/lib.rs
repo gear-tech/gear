ee4f41ddc1c87d271df3671f8a6a306e1f6c0150
Vadim Smirnov <vs@gear-tech.io>
2022-08-31 19:14:16 +0300

    bump spec

--- runtime/gear/src/lib.rs
+++ runtime/gear/src/lib.rs
@@ -88,7 +88,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     impl_name: create_runtime_str!("gear"),
     apis: RUNTIME_API_VERSIONS,
     authoring_version: 1,
-    spec_version: 1670,
+    spec_version: 1680,
     impl_version: 1,
     transaction_version: 1,
     state_version: 1,
