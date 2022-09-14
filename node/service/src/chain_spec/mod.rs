f977fbcb0776bb3322a2ea6686092112902aaad5
Vadim Smirnov <vs@gear-tech.io>
2022-09-14 11:46:03 +0300

    Add ChainSpec extensions

--- node/service/src/chain_spec/mod.rs
+++ node/service/src/chain_spec/mod.rs
@@ -18,17 +18,35 @@
 
 //! Gear chain configurations
 
-pub use runtime_primitives::{AccountId, AccountPublic};
+pub use runtime_primitives::{AccountId, AccountPublic, Block};
+use sc_chain_spec::ChainSpecExtension;
 use sp_core::{Pair, Public};
 use sp_runtime::traits::IdentifyAccount;
+
+use serde::{Deserialize, Serialize};
 
 #[cfg(feature = "gear-native")]
 pub mod gear;
 #[cfg(feature = "vara-native")]
 pub mod vara;
 
+/// Node `ChainSpec` extensions.
+///
+/// Additional parameters for some Substrate core modules,
+/// customizable from the chain spec.
+#[derive(Default, Clone, Serialize, Deserialize, ChainSpecExtension)]
+#[serde(rename_all = "camelCase")]
+pub struct Extensions {
+    /// Block numbers with known hashes.
+    pub fork_blocks: sc_client_api::ForkBlocks<Block>,
+    /// Known bad block hashes.
+    pub bad_blocks: sc_client_api::BadBlocks<Block>,
+    /// The light sync state extension used by the sync-state rpc.
+    pub light_sync_state: sc_sync_state_rpc::LightSyncStateExtension,
+}
+
 /// General `ChainSpec` used as a basis for a specialized config.
-pub type RawChainSpec = sc_service::GenericChainSpec<()>;
+pub type RawChainSpec = sc_service::GenericChainSpec<(), Extensions>;
 
 /// Generate a crypto pair from seed.
 pub fn get_from_seed<TPublic: Public>(seed: &str) -> <TPublic::Pair as Pair>::Public {
