6904e872fe60f887dc88da9ecec28997edc56b2f
Vadim Smirnov <vs@gear-tech.io>
2022-09-17 13:46:22 +0300

    fix benchmarks

--- node/service/src/lib.rs
+++ node/service/src/lib.rs
@@ -16,7 +16,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-use sc_client_api::{BlockBackend};
+use sc_client_api::BlockBackend;
 use sc_executor::{NativeElseWasmExecutor, NativeExecutionDispatch};
 use sc_finality_grandpa::SharedVoterState;
 use sc_keystore::LocalKeystore;
