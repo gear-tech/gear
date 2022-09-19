857e00cab78f74fa71f8bc797ac67f8b048e7cde
Vadim Smirnov <vs@gear-tech.io>
2022-09-19 15:50:42 +0300

    Fix try-runtime

--- runtime/common/src/apis.rs
+++ runtime/common/src/apis.rs
@@ -214,11 +214,24 @@ macro_rules! impl_runtime_apis_plus_common {
 					// have a backtrace here. If any of the pre/post migration checks fail, we shall stop
 					// right here and right now.
 					let weight = Executive::try_runtime_upgrade().unwrap();
-					(weight, BlockWeights::get().max_block)
+					(weight, RuntimeBlockWeights::get().max_block)
 				}
 
-				fn execute_block_no_check(block: Block) -> Weight {
-					Executive::execute_block_no_check(block)
+				fn execute_block(
+					block: Block,
+					state_root_check: bool,
+					select: frame_try_runtime::TryStateSelect
+				) -> Weight {
+					log::info!(
+						target: "node-runtime",
+						"try-runtime: executing block {:?} / root checks: {:?} / try-state-select: {:?}",
+						block.header.hash(),
+						state_root_check,
+						select,
+					);
+					// NOTE: intentional unwrap: we don't want to propagate the error backwards, and want to
+					// have a backtrace here.
+					Executive::try_execute_block(block, state_root_check, select).unwrap()
 				}
 			}
 		}
