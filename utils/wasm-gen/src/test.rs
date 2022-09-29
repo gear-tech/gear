d86d2cecc584688b849c1e9b1cc140cb5f354844
Gregory Sobol <grishasobol@mail.ru>
2022-09-29 18:58:33 +0300

    fix cargo lock

--- utils/wasm-gen/src/test.rs
+++ utils/wasm-gen/src/test.rs
@@ -18,7 +18,7 @@ fn gen_wasm() {
 #[test]
 fn gen_wasm_rare() {
     let mut rng = SmallRng::seed_from_u64(12345);
-    for _ in 0..100 {
+    for _ in (0..100).progress() {
         let mut buf = vec![0; 1000000];
         rng.fill_bytes(&mut buf);
         let mut u = Unstructured::new(&buf);
@@ -31,15 +31,12 @@ fn gen_wasm_rare() {
 #[test]
 fn gen_wasm_valid() {
     let mut rng = SmallRng::seed_from_u64(33333);
-    for _ in (0..1000).progress() {
+    for _ in (0..100).progress() {
         let mut buf = vec![0; 1000000];
         rng.fill_bytes(&mut buf);
         let mut u = Unstructured::new(&buf);
         let code = gen_gear_program_code(&mut u, GearConfig::new_valid());
-
-        let wat = wasmprinter::print_bytes(&code).unwrap();
-        println!("\n\n\n\n{}\n\n\n\n", wat);
-
+        let _wat = wasmprinter::print_bytes(&code).unwrap();
         wasmparser::validate(&code).unwrap();
     }
 }
