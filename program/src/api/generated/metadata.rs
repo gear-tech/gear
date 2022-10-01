cc55409b182866265e90eeff4ab0a16b0f7792e8
Sabaun Taraki <taraki.sabaun.rl@gmail.com>
2022-10-01 19:20:45 +0300

    Implement upload_code test

--- program/src/api/generated/metadata.rs
+++ program/src/api/generated/metadata.rs
@@ -1544,9 +1544,9 @@ pub mod api {
                     let metadata = locked_metadata.read();
                     if metadata.constant_hash("System", "Version")?
                         == [
-                            11u8, 117u8, 206u8, 47u8, 240u8, 216u8, 123u8, 154u8, 105u8, 65u8,
-                            67u8, 66u8, 143u8, 50u8, 36u8, 81u8, 20u8, 55u8, 23u8, 77u8, 143u8,
-                            77u8, 8u8, 150u8, 238u8, 166u8, 101u8, 183u8, 240u8, 85u8, 40u8, 178u8,
+                            177u8, 162u8, 217u8, 244u8, 71u8, 27u8, 3u8, 212u8, 27u8, 99u8, 228u8,
+                            178u8, 51u8, 181u8, 107u8, 27u8, 217u8, 94u8, 72u8, 147u8, 30u8, 137u8,
+                            225u8, 2u8, 196u8, 249u8, 246u8, 42u8, 108u8, 120u8, 227u8, 75u8,
                         ]
                     {
                         let pallet = metadata.pallet("System")?;
@@ -8321,9 +8321,9 @@ pub mod api {
                     let metadata = locked_metadata.read();
                     if metadata.constant_hash("Gear", "Schedule")?
                         == [
-                            190u8, 249u8, 239u8, 138u8, 76u8, 42u8, 7u8, 80u8, 198u8, 241u8, 123u8,
-                            55u8, 30u8, 245u8, 2u8, 11u8, 199u8, 93u8, 105u8, 198u8, 118u8, 254u8,
-                            198u8, 41u8, 202u8, 168u8, 165u8, 146u8, 28u8, 56u8, 198u8, 124u8,
+                            2u8, 92u8, 241u8, 208u8, 134u8, 26u8, 93u8, 84u8, 242u8, 94u8, 155u8,
+                            49u8, 158u8, 231u8, 192u8, 14u8, 161u8, 127u8, 139u8, 140u8, 3u8, 71u8,
+                            209u8, 35u8, 75u8, 28u8, 153u8, 32u8, 33u8, 151u8, 91u8, 214u8,
                         ]
                     {
                         let pallet = metadata.pallet("Gear")?;
@@ -11585,9 +11585,9 @@ pub mod api {
             };
             if runtime_metadata_hash
                 != [
-                    104u8, 116u8, 103u8, 35u8, 166u8, 108u8, 99u8, 170u8, 227u8, 100u8, 234u8,
-                    149u8, 235u8, 170u8, 204u8, 49u8, 11u8, 220u8, 107u8, 177u8, 68u8, 75u8, 129u8,
-                    93u8, 119u8, 174u8, 245u8, 25u8, 4u8, 77u8, 76u8, 12u8,
+                    191u8, 19u8, 55u8, 153u8, 194u8, 30u8, 58u8, 184u8, 69u8, 251u8, 120u8, 186u8,
+                    93u8, 49u8, 24u8, 3u8, 101u8, 176u8, 253u8, 161u8, 158u8, 57u8, 120u8, 34u8,
+                    182u8, 13u8, 114u8, 12u8, 40u8, 71u8, 124u8, 80u8,
                 ]
             {
                 Err(::subxt::MetadataError::IncompatibleMetadata)
