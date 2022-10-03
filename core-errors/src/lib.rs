de6382b297feb5ebb418dfd6ca2b24218467852c
Dmitry Novikov <novikov.dm.al@gmail.com>
2022-10-03 18:49:09 +0300

    Refactor errors a bit

--- core-errors/src/lib.rs
+++ core-errors/src/lib.rs
@@ -181,25 +181,41 @@ pub enum ExecutionError {
 #[cfg_attr(feature = "codec", derive(Encode, Decode, TypeInfo))]
 pub enum ExtError {
     /// We got some error but don't know which exactly because of disabled gcore's `codec` feature
+    #[cfg(not(feature = "codec"))]
     #[display(fmt = "Some error")]
     Some,
+
+    /// Decode error.
+    ///
+    /// Supposed to be unreachable.
+    #[cfg(feature = "codec")]
+    #[display(fmt = "`ExtError` decoding error")]
+    Decode,
+
     /// Memory error.
+    #[cfg(feature = "codec")]
     #[display(fmt = "Memory error: {}", _0)]
     Memory(MemoryError),
+
     /// Message error.
+    #[cfg(feature = "codec")]
     #[display(fmt = "Message error: {}", _0)]
     Message(MessageError),
+
     /// Waiting error.
+    #[cfg(feature = "codec")]
     #[display(fmt = "Waiting error: {}", _0)]
     Wait(WaitError),
+
     /// Execution error.
+    #[cfg(feature = "codec")]
     #[display(fmt = "Execution error: {}", _0)]
     Execution(ExecutionError),
 }
 
+#[cfg(feature = "codec")]
 impl ExtError {
     /// Size of error encoded in SCALE codec
-    #[cfg(feature = "codec")]
     pub fn encoded_size(&self) -> usize {
         Encode::encoded_size(self)
     }
