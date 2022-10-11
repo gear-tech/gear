c366911e7db3d5e263f7ebbe84e87eced711629d
Dmitry Novikov <novikov.dm.al@gmail.com>
2022-10-11 20:26:28 +0300

    Fix feature-dependent node build (#1635)

--- node/cli/src/command.rs
+++ node/cli/src/command.rs
@@ -16,15 +16,10 @@
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-use crate::{
-    cli::{Cli, Subcommand},
-    inherent_benchmark_data, RemarkBuilder, TransferKeepAliveBuilder,
-};
-use frame_benchmarking_cli::{BenchmarkCmd, ExtrinsicFactory, SUBSTRATE_REFERENCE_HARDWARE};
+use crate::cli::{Cli, Subcommand};
 use runtime_primitives::Block;
 use sc_cli::{ChainSpec, ExecutionStrategy, RuntimeVersion, SubstrateCli};
 use service::{chain_spec, IdentifyVariant};
-use sp_keyring::Sr25519Keyring;
 
 impl SubstrateCli for Cli {
     fn impl_name() -> String {
@@ -109,6 +104,7 @@ impl SubstrateCli for Cli {
 }
 
 /// Unwraps a [`service::Client`] into the concrete runtime client.
+#[allow(unused)]
 macro_rules! unwrap_client {
     (
         $client:ident,
@@ -186,7 +182,14 @@ pub fn run() -> sc_cli::Result<()> {
                 Ok((cmd.run(client, backend, Some(aux_revert)), task_manager))
             })
         }
+        #[cfg(feature = "runtime-benchmarks")]
         Some(Subcommand::Benchmark(cmd)) => {
+            use crate::{inherent_benchmark_data, RemarkBuilder, TransferKeepAliveBuilder};
+            use frame_benchmarking_cli::{
+                BenchmarkCmd, ExtrinsicFactory, SUBSTRATE_REFERENCE_HARDWARE,
+            };
+            use sp_keyring::Sr25519Keyring;
+
             let runner = cli.create_runner(cmd)?;
 
             runner.sync_run(|config| {
@@ -239,7 +242,6 @@ pub fn run() -> sc_cli::Result<()> {
                         })?;
 
                         let (client, _, _, _) = service::new_chain_ops(&config)?;
-                        // let ext_builder = client.clone();
                         let ext_builder = RemarkBuilder::new(client.clone());
 
                         unwrap_client!(
@@ -278,6 +280,7 @@ pub fn run() -> sc_cli::Result<()> {
                 }
             })
         }
+        #[cfg(feature = "runtime-test")]
         Some(Subcommand::GearRuntimeTest(cmd)) => {
             let runner = cli.create_runner(cmd)?;
 
