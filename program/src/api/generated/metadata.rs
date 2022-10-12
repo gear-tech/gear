37113aa20e95c6566d75c77f378b09e560f23a5c
Arsenii Lyashenko <arsenylyashenko.3@gmail.com>
2022-10-12 22:46:15 +0300

    Update metadata

--- program/src/api/generated/metadata.rs
+++ program/src/api/generated/metadata.rs
@@ -1544,9 +1544,9 @@ pub mod api {
                     let metadata = locked_metadata.read();
                     if metadata.constant_hash("System", "Version")?
                         == [
-                            11u8, 117u8, 206u8, 47u8, 240u8, 216u8, 123u8, 154u8, 105u8, 65u8,
-                            67u8, 66u8, 143u8, 50u8, 36u8, 81u8, 20u8, 55u8, 23u8, 77u8, 143u8,
-                            77u8, 8u8, 150u8, 238u8, 166u8, 101u8, 183u8, 240u8, 85u8, 40u8, 178u8,
+                            177u8, 162u8, 217u8, 244u8, 71u8, 27u8, 3u8, 212u8, 27u8, 99u8, 228u8,
+                            178u8, 51u8, 181u8, 107u8, 27u8, 217u8, 94u8, 72u8, 147u8, 30u8, 137u8,
+                            225u8, 2u8, 196u8, 249u8, 246u8, 42u8, 108u8, 120u8, 227u8, 75u8,
                         ]
                     {
                         let pallet = metadata.pallet("System")?;
@@ -8321,9 +8321,10 @@ pub mod api {
                     let metadata = locked_metadata.read();
                     if metadata.constant_hash("Gear", "Schedule")?
                         == [
-                            190u8, 249u8, 239u8, 138u8, 76u8, 42u8, 7u8, 80u8, 198u8, 241u8, 123u8,
-                            55u8, 30u8, 245u8, 2u8, 11u8, 199u8, 93u8, 105u8, 198u8, 118u8, 254u8,
-                            198u8, 41u8, 202u8, 168u8, 165u8, 146u8, 28u8, 56u8, 198u8, 124u8,
+                            255u8, 184u8, 45u8, 49u8, 74u8, 255u8, 160u8, 208u8, 41u8, 199u8,
+                            204u8, 8u8, 57u8, 204u8, 203u8, 165u8, 127u8, 32u8, 22u8, 182u8, 26u8,
+                            150u8, 222u8, 194u8, 223u8, 111u8, 191u8, 147u8, 154u8, 255u8, 68u8,
+                            249u8,
                         ]
                     {
                         let pallet = metadata.pallet("Gear")?;
@@ -9924,7 +9925,18 @@ pub mod api {
                     pub instruction_weights:
                         runtime_types::pallet_gear::schedule::InstructionWeights,
                     pub host_fn_weights: runtime_types::pallet_gear::schedule::HostFnWeights,
+                    pub static_host_fn_weights:
+                        runtime_types::pallet_gear::schedule::StaticHostFnWeights,
                     pub memory_weights: runtime_types::pallet_gear::schedule::MemoryWeights,
+                }
+                #[derive(
+                    :: subxt :: codec :: CompactAs,
+                    :: subxt :: codec :: Decode,
+                    :: subxt :: codec :: Encode,
+                    Debug,
+                )]
+                pub struct StaticHostFnWeights {
+                    pub instantiate_module_per_byte: ::core::primitive::u64,
                 }
             }
             #[derive(:: subxt :: codec :: Decode, :: subxt :: codec :: Encode, Debug)]
@@ -11585,9 +11597,9 @@ pub mod api {
             };
             if runtime_metadata_hash
                 != [
-                    104u8, 116u8, 103u8, 35u8, 166u8, 108u8, 99u8, 170u8, 227u8, 100u8, 234u8,
-                    149u8, 235u8, 170u8, 204u8, 49u8, 11u8, 220u8, 107u8, 177u8, 68u8, 75u8, 129u8,
-                    93u8, 119u8, 174u8, 245u8, 25u8, 4u8, 77u8, 76u8, 12u8,
+                    139u8, 151u8, 148u8, 49u8, 12u8, 128u8, 119u8, 106u8, 42u8, 119u8, 187u8, 32u8,
+                    145u8, 139u8, 134u8, 147u8, 169u8, 162u8, 188u8, 68u8, 211u8, 66u8, 238u8,
+                    230u8, 109u8, 185u8, 153u8, 153u8, 148u8, 5u8, 197u8, 39u8,
                 ]
             {
                 Err(::subxt::MetadataError::IncompatibleMetadata)
