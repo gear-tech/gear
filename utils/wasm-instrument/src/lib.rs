df63c9f878ba35916a4659ae94499259d3d3068d
Georgy Shepelev <geo.shepelev@gmail.com>
2022-10-17 15:45:28 +0400

    gear-wasm-instrument

---
+++ utils/wasm-instrument/src/lib.rs
@@ -0,0 +1,209 @@
+// This file is part of Gear.
+
+// Copyright (C) 2021-2022 Gear Technologies Inc.
+// SPDX-License-Identifier: GPL-3.0-or-later WITH Classpath-exception-2.0
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+#![cfg_attr(not(feature = "std"), no_std)]
+
+use wasm_instrument::{
+    gas_metering::{self, Rules},
+    parity_wasm::{
+        builder,
+        elements::{self, Instruction, ValueType},
+    },
+};
+
+#[cfg(test)]
+mod tests;
+
+pub fn inject<R: Rules>(
+    module: elements::Module,
+    rules: &R,
+    gas_module_name: &str,
+) -> Result<elements::Module, elements::Module> {
+    // Injecting gas counting external
+    let mut mbuilder = builder::from_module(module);
+    // fn out_of_gas(gas: i32, gas_counter: i64)
+    let import_sig = mbuilder.push_signature(
+        builder::signature()
+            .with_param(ValueType::I32)
+            .with_param(ValueType::I64)
+            .build_sig(),
+    );
+
+    mbuilder.push_import(
+        builder::import()
+            .module(gas_module_name)
+            .field("out_of_gas")
+            .external()
+            .func(import_sig)
+            .build(),
+    );
+
+    mbuilder.push_import(
+        builder::import()
+            .module(gas_module_name)
+            .field("out_of_allowance")
+            .external()
+            .func(import_sig)
+            .build(),
+    );
+
+    // back to plain module
+    let module = mbuilder.build();
+
+    let import_count = module.import_count(elements::ImportCountType::Function);
+    let out_of_gas_index = import_count as u32 - 2;
+    let out_of_allowance_index = import_count as u32 - 1;
+
+    let gas_charge_index = module.functions_space();
+    let gas_index = module.globals_space() as u32;
+    let allowance_index = gas_index + 1;
+
+    let mut mbuilder = builder::from_module(module);
+
+    mbuilder.push_global(
+        builder::global()
+            .value_type()
+            .i64()
+            .init_expr(Instruction::I64Const(0))
+            .mutable()
+            .build(),
+    );
+
+    mbuilder.push_export(
+        builder::export()
+            .field("gear_gas")
+            .internal()
+            .global(gas_index)
+            .build(),
+    );
+
+    mbuilder.push_global(
+        builder::global()
+            .value_type()
+            .i64()
+            .init_expr(Instruction::I64Const(0))
+            .mutable()
+            .build(),
+    );
+
+    mbuilder.push_export(
+        builder::export()
+            .field("gear_allowance")
+            .internal()
+            .global(allowance_index)
+            .build(),
+    );
+
+    let mut elements = vec![
+        // check if there is enough gas allowance
+        Instruction::GetGlobal(allowance_index),
+        Instruction::GetLocal(0),
+        Instruction::I64ExtendUI32,
+        Instruction::I64Const(i64::MAX),
+        Instruction::I64Add,
+        Instruction::I64LtU,
+        Instruction::If(elements::BlockType::NoResult),
+        Instruction::GetLocal(0),
+        Instruction::GetGlobal(allowance_index),
+        Instruction::Call(out_of_allowance_index),
+        Instruction::Unreachable,
+        Instruction::End,
+        // check if there is enough gas
+        Instruction::GetGlobal(gas_index),
+        Instruction::GetLocal(0),
+        Instruction::I64ExtendUI32,
+        Instruction::I64Const(i64::MAX),
+        Instruction::I64Add,
+        Instruction::I64LtU,
+        Instruction::If(elements::BlockType::NoResult),
+        Instruction::GetLocal(0),
+        Instruction::GetGlobal(gas_index),
+        Instruction::Call(out_of_gas_index),
+        Instruction::Unreachable,
+        Instruction::End,
+        // update gas allowance
+        Instruction::GetGlobal(allowance_index),
+        Instruction::GetLocal(0),
+        Instruction::I64ExtendUI32,
+        Instruction::I64Const(i64::MAX),
+        Instruction::I64Add,
+        Instruction::I64Sub,
+        Instruction::SetGlobal(allowance_index),
+        // update gas
+        Instruction::GetGlobal(gas_index),
+        Instruction::GetLocal(0),
+        Instruction::I64ExtendUI32,
+        Instruction::I64Const(i64::MAX),
+        Instruction::I64Add,
+        Instruction::I64Sub,
+        Instruction::SetGlobal(gas_index),
+        //
+        Instruction::End,
+    ];
+
+    // determine cost for successful execution
+    let cost_check_blocks = match elements
+        .iter()
+        .take(7)
+        .try_fold(0u64, |cost, instruction| {
+            rules.instruction_cost(instruction).map(|c| cost + c as u64)
+        })
+    {
+        Some(c) => 2 * c,
+        None => return Err(mbuilder.build()),
+    };
+
+    let cost_set_blocks = match elements
+        .iter()
+        .skip(24)
+        .take(7)
+        .try_fold(0u64, |cost, instruction| {
+            rules.instruction_cost(instruction).map(|c| cost + c as u64)
+        })
+    {
+        Some(c) => 2 * c,
+        None => return Err(mbuilder.build()),
+    };
+
+    let cost = (cost_check_blocks + cost_set_blocks) as i64;
+
+    // update cost for 'gas_charge' function itself
+    for instruction in elements
+        .iter_mut()
+        .filter(|i| matches!(i, Instruction::I64Const(_)))
+    {
+        *instruction = Instruction::I64Const(cost);
+    }
+
+    // gas_charge function
+    mbuilder.push_function(
+        builder::function()
+            .signature()
+            .with_param(ValueType::I32)
+            .build()
+            .body()
+            .with_instructions(elements::Instructions::new(elements))
+            .build()
+            .build(),
+    );
+
+    // back to plain module
+    let module = mbuilder.build();
+
+    gas_metering::post_injection_handler(module, rules, gas_charge_index, out_of_gas_index, 2)
+}
