36fea35b1707389353693637427ef370fbaa3821
Arsenii Lyashenko <arsenylyashenko.3@gmail.com>
2022-10-20 15:28:23 +0300

    Move benches a little lower in the file

--- pallets/gear/src/benchmarking/mod.rs
+++ pallets/gear/src/benchmarking/mod.rs
@@ -767,6 +767,95 @@ benchmarks! {
         >(&block_config, context, memory_pages);
     }
 
+    gr_reserve_gas {
+        let r in 0 .. API_BENCHMARK_BATCHES;
+        let id_bytes = 0_u128.encode();
+        let id_len = id_bytes.len();
+        let code = WasmModule::<T>::from(ModuleDefinition {
+            memory: Some(ImportedMemory::max::<T>()),
+            imported_functions: vec![ImportedFunction {
+                module: "env",
+                name: "gr_reserve_gas",
+                params: vec![ValueType::I64, ValueType::I32, ValueType::I32],
+                return_type: Some(ValueType::I32),
+            }],
+            data_segments: vec![
+                DataSegment {
+                    offset: 0,
+                    value: id_bytes,
+                },
+            ],
+            handle_body: Some(body::repeated(r * API_BENCHMARK_BATCHES, &[
+                Instruction::I64Const(50_000_000), // gas amount
+                Instruction::I32Const(10), // duration
+                Instruction::I32Const(0), // id ptr
+                Instruction::Call(0),
+                Instruction::Drop,
+            ])),
+            ..Default::default()
+        });
+        let instance = Program::<T>::new(code, vec![])?;
+        let Exec {
+            ext_manager,
+            block_config,
+            context,
+            memory_pages,
+        } = prepare::<T>(instance.caller.into_origin(), HandleKind::Handle(ProgramId::from_origin(instance.addr)), vec![], 0u32.into())?;
+    }: {
+        core_processor::process::<
+            Externalities,
+            ExecutionEnvironment,
+        >(&block_config, context, memory_pages);
+    }
+
+    // We cannot call `gr_unreserve_gas` multiple times. Therefore our weight determination is not
+    // as precise as with other APIs.
+    gr_unreserve_gas {
+        let r in 0 .. 1;
+        let id_bytes = 0_u128.encode();
+        let id_len = id_bytes.len();
+        let amount_bytes = 0_u64.encode();
+        let amount_len = amount_bytes.len();
+        let code = WasmModule::<T>::from(ModuleDefinition {
+            memory: Some(ImportedMemory::max::<T>()),
+            imported_functions: vec![ImportedFunction {
+                module: "env",
+                name: "gr_unreserve_gas",
+                params: vec![ValueType::I32, ValueType::I32],
+                return_type: Some(ValueType::I32),
+            }],
+            data_segments: vec![
+                DataSegment {
+                    offset: 0,
+                    value: id_bytes,
+                },
+                DataSegment {
+                    offset: id_len as u32,
+                    value: amount_bytes,
+                }
+            ],
+            handle_body: Some(body::repeated(r, &[
+                Instruction::I32Const(0), // id ptr
+                Instruction::I32Const(id_len as i32), // unreserved amount ptr
+                Instruction::Call(0),
+                Instruction::Drop,
+            ])),
+            ..Default::default()
+        });
+        let instance = Program::<T>::new(code, vec![])?;
+        let Exec {
+            ext_manager,
+            block_config,
+            context,
+            memory_pages,
+        } = prepare::<T>(instance.caller.into_origin(), HandleKind::Handle(ProgramId::from_origin(instance.addr)), vec![], 0u32.into())?;
+    }: {
+        core_processor::process::<
+            Externalities,
+            ExecutionEnvironment,
+        >(&block_config, context, memory_pages);
+    }
+
     gr_message_id {
         let r in 0 .. API_BENCHMARK_BATCHES;
         let instance = Program::<T>::new(WasmModule::getter(
@@ -1862,95 +1951,6 @@ benchmarks! {
                 Regular(Instruction::Call(0)),
             ])),
             .. Default::default()
-        });
-        let instance = Program::<T>::new(code, vec![])?;
-        let Exec {
-            ext_manager,
-            block_config,
-            context,
-            memory_pages,
-        } = prepare::<T>(instance.caller.into_origin(), HandleKind::Handle(ProgramId::from_origin(instance.addr)), vec![], 0u32.into())?;
-    }: {
-        core_processor::process::<
-            Externalities,
-            ExecutionEnvironment,
-        >(&block_config, context, memory_pages);
-    }
-
-    gr_reserve_gas {
-        let r in 0 .. API_BENCHMARK_BATCHES;
-        let id_bytes = 0_u128.encode();
-        let id_len = id_bytes.len();
-        let code = WasmModule::<T>::from(ModuleDefinition {
-            memory: Some(ImportedMemory::max::<T>()),
-            imported_functions: vec![ImportedFunction {
-                module: "env",
-                name: "gr_reserve_gas",
-                params: vec![ValueType::I64, ValueType::I32, ValueType::I32],
-                return_type: Some(ValueType::I32),
-            }],
-            data_segments: vec![
-                DataSegment {
-                    offset: 0,
-                    value: id_bytes,
-                },
-            ],
-            handle_body: Some(body::repeated(r * API_BENCHMARK_BATCHES, &[
-                Instruction::I64Const(50_000_000), // gas amount
-                Instruction::I32Const(10), // duration
-                Instruction::I32Const(0), // id ptr
-                Instruction::Call(0),
-                Instruction::Drop,
-            ])),
-            ..Default::default()
-        });
-        let instance = Program::<T>::new(code, vec![])?;
-        let Exec {
-            ext_manager,
-            block_config,
-            context,
-            memory_pages,
-        } = prepare::<T>(instance.caller.into_origin(), HandleKind::Handle(ProgramId::from_origin(instance.addr)), vec![], 0u32.into())?;
-    }: {
-        core_processor::process::<
-            Externalities,
-            ExecutionEnvironment,
-        >(&block_config, context, memory_pages);
-    }
-
-    // We cannot call `gr_unreserve_gas` multiple times. Therefore our weight determination is not
-    // as precise as with other APIs.
-    gr_unreserve_gas {
-        let r in 0 .. 1;
-        let id_bytes = 0_u128.encode();
-        let id_len = id_bytes.len();
-        let amount_bytes = 0_u64.encode();
-        let amount_len = amount_bytes.len();
-        let code = WasmModule::<T>::from(ModuleDefinition {
-            memory: Some(ImportedMemory::max::<T>()),
-            imported_functions: vec![ImportedFunction {
-                module: "env",
-                name: "gr_unreserve_gas",
-                params: vec![ValueType::I32, ValueType::I32],
-                return_type: Some(ValueType::I32),
-            }],
-            data_segments: vec![
-                DataSegment {
-                    offset: 0,
-                    value: id_bytes,
-                },
-                DataSegment {
-                    offset: id_len as u32,
-                    value: amount_bytes,
-                }
-            ],
-            handle_body: Some(body::repeated(r, &[
-                Instruction::I32Const(0), // id ptr
-                Instruction::I32Const(id_len as i32), // unreserved amount ptr
-                Instruction::Call(0),
-                Instruction::Drop,
-            ])),
-            ..Default::default()
         });
         let instance = Program::<T>::new(code, vec![])?;
         let Exec {
