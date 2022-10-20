c0cde07b1ed17217359326dc84e676295f0f2b62
ekovalev <ekovalev@users.noreply.github.com>
2022-10-20 19:48:37 +0800

    Correct ideal stake in staking reward curve (#1699)

--- runtime/vara/src/lib.rs
+++ runtime/vara/src/lib.rs
@@ -579,7 +579,7 @@ pallet_staking_reward_curve::build! {
     const REWARD_CURVE: PiecewiseLinear<'static> = curve!(
         min_inflation: 0_025_000,
         max_inflation: 0_075_000,
-        ideal_stake: 0_500_000,
+        ideal_stake: 0_750_000,
         falloff: 0_050_000,
         max_piece_count: 40,
         test_precision: 0_005_000,
