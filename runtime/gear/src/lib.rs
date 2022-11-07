755b8b54c6ff0a8328610f1c5271f5f280ed2821
Arsenii Lyashenko <arsenylyashenko.3@gmail.com>
2022-11-07 23:37:30 +0300

    Bump spec version

--- runtime/gear/src/lib.rs
+++ runtime/gear/src/lib.rs
@@ -97,7 +97,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     impl_name: create_runtime_str!("gear"),
     apis: RUNTIME_API_VERSIONS,
     authoring_version: 1,
-    spec_version: 100,
+    spec_version: 110,
     impl_version: 1,
     transaction_version: 1,
     state_version: 1,
