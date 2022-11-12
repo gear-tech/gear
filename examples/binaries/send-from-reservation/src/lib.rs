5bdef145c4ffbd19df96bac9ea94d46076001ea8
Arsenii Lyashenko <arsenylyashenko.3@gmail.com>
2022-11-12 12:44:52 +0300

    expect

--- examples/binaries/send-from-reservation/src/lib.rs
+++ examples/binaries/send-from-reservation/src/lib.rs
@@ -53,14 +53,15 @@ pub enum ReplyAction {
 
 #[no_mangle]
 unsafe extern "C" fn handle() {
-    let action: HandleAction = msg::load().unwrap();
+    let action: HandleAction = msg::load().expect("Failed to load handle payload");
     match action {
         HandleAction::SendToUser => {
-            let id = ReservationId::reserve(3_000_000_000, 50).unwrap();
-            msg::send_bytes_from_reservation(id, msg::source(), b"send_to_user", 500).unwrap();
+            let id = ReservationId::reserve(3_000_000_000, 50).expect("Failed to reserve gas");
+            msg::send_bytes_from_reservation(id, msg::source(), b"send_to_user", 500)
+                .expect("Failed to send message");
         }
         HandleAction::SendToUserDelayed => {
-            let id = ReservationId::reserve(4_000_000_000, 60).unwrap();
+            let id = ReservationId::reserve(4_000_000_000, 60).expect("Failed to reserve gas");
             msg::send_bytes_delayed_from_reservation(
                 id,
                 msg::source(),
@@ -68,15 +69,15 @@ unsafe extern "C" fn handle() {
                 600,
                 1,
             )
-            .unwrap();
+            .expect("Failed to send message");
         }
         HandleAction::SendToProgram { pid, user } => {
-            let id = ReservationId::reserve(5_000_000_000, 70).unwrap();
+            let id = ReservationId::reserve(5_000_000_000, 70).expect("Failed to reserve gas");
             msg::send_from_reservation(id, pid.into(), HandleAction::ReceiveFromProgram(user), 700)
-                .unwrap();
+                .expect("Failed to send message");
         }
         HandleAction::SendToProgramDelayed { pid, user } => {
-            let id = ReservationId::reserve(6_000_000_000, 80).unwrap();
+            let id = ReservationId::reserve(6_000_000_000, 80).expect("Failed to reserve gas");
             msg::send_delayed_from_reservation(
                 id,
                 pid.into(),
@@ -84,23 +85,26 @@ unsafe extern "C" fn handle() {
                 800,
                 1,
             )
-            .unwrap();
+            .expect("Failed to send message");
         }
         HandleAction::ReplyToUser => {
-            let id = ReservationId::reserve(7_000_000_000, 90).unwrap();
-            msg::reply_bytes_from_reservation(id, b"reply_to_user", 900).unwrap();
+            let id = ReservationId::reserve(7_000_000_000, 90).expect("Failed to reserve gas");
+            msg::reply_bytes_from_reservation(id, b"reply_to_user", 900)
+                .expect("Failed to send message");
         }
         HandleAction::ReplyToUserDelayed => {
-            let id = ReservationId::reserve(8_000_000_000, 100).unwrap();
+            let id = ReservationId::reserve(8_000_000_000, 100).expect("Failed to reserve gas");
             msg::reply_bytes_delayed_from_reservation(id, b"reply_to_user_delayed", 1000, 1)
-                .unwrap();
+                .expect("Failed to send message");
         }
         HandleAction::ReplyToProgram { pid, user } => {
-            msg::send(pid.into(), HandleAction::ReplyToProgramStep2(user), 900).unwrap();
+            msg::send(pid.into(), HandleAction::ReplyToProgramStep2(user), 900)
+                .expect("Failed to reserve gas");
         }
         HandleAction::ReplyToProgramStep2(user) => {
-            let id = ReservationId::reserve(7_000_000_000, 90).unwrap();
-            msg::reply_from_reservation(id, ReplyAction::Receive(user), 900).unwrap();
+            let id = ReservationId::reserve(7_000_000_000, 90).expect("Failed to reserve gas");
+            msg::reply_from_reservation(id, ReplyAction::Receive(user), 900)
+                .expect("Failed to reply");
         }
         HandleAction::ReplyToProgramDelayed { pid, user } => {
             msg::send(
@@ -108,35 +112,37 @@ unsafe extern "C" fn handle() {
                 HandleAction::ReplyToProgramDelayedStep2(user),
                 1000,
             )
-            .unwrap();
+            .expect("Failed to send message");
         }
         HandleAction::ReplyToProgramDelayedStep2(user) => {
-            let id = ReservationId::reserve(8_000_000_000, 100).unwrap();
+            let id = ReservationId::reserve(8_000_000_000, 100).expect("Failed to reserve gas");
             msg::reply_delayed_from_reservation(id, ReplyAction::ReceiveDelayed(user), 1000, 1)
-                .unwrap();
+                .expect("Failed to reply");
         }
         HandleAction::ReceiveFromProgram(user) => {
             assert_eq!(msg::value(), 700);
-            msg::send_bytes(user.into(), b"receive_from_program", 700).unwrap();
+            msg::send_bytes(user.into(), b"receive_from_program", 700)
+                .expect("Failed to send message");
         }
         HandleAction::ReceiveFromProgramDelayed(user) => {
             assert_eq!(msg::value(), 800);
-            msg::send_bytes(user.into(), b"receive_from_program_delayed", 800).unwrap();
+            msg::send_bytes(user.into(), b"receive_from_program_delayed", 800)
+                .expect("Failed to send message");
         }
     }
 }
 
 #[no_mangle]
 unsafe extern "C" fn handle_reply() {
-    let action: ReplyAction = msg::load().unwrap();
+    let action: ReplyAction = msg::load().expect("Failed to load handle payload");
     match action {
         ReplyAction::Receive(user) => {
             assert_eq!(msg::value(), 900);
-            msg::send_bytes(user.into(), b"reply", 900).unwrap();
+            msg::send_bytes(user.into(), b"reply", 900).expect("Failed to send message");
         }
         ReplyAction::ReceiveDelayed(user) => {
             assert_eq!(msg::value(), 1000);
-            msg::send_bytes(user.into(), b"reply_delayed", 1000).unwrap();
+            msg::send_bytes(user.into(), b"reply_delayed", 1000).expect("Failed to send message");
         }
     }
 }
