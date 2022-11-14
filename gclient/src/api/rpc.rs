24d104da1ebfe29bf63061fc11f5fca9fb00ef2e
clearloop <26088946+clearloop@users.noreply.github.com>
2022-11-14 17:32:09 +0800

    chore(gclient): make clippy happy

--- gclient/src/api/rpc.rs
+++ gclient/src/api/rpc.rs
@@ -15,6 +15,7 @@
 
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
+#![allow(clippy::too_many_arguments)]
 
 use crate::{api::Result, GearApi};
 use gear_core::ids::{CodeId, MessageId, ProgramId};
