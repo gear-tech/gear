6c81280636c7a1ecfdff9b2faf5b8fb1638650a8
clearloop <26088946+clearloop@users.noreply.github.com>
2022-12-01 03:09:44 +0800

    feat(cli): override log level on flag validator (#1860)

--- node/cli/src/command.rs
+++ node/cli/src/command.rs
@@ -346,7 +346,15 @@ pub fn run() -> sc_cli::Result<()> {
             Ok(())
         }
         None => {
-            let runner = cli.create_runner(&cli.run.base)?;
+            let runner = if cli.run.base.validator {
+                cli.create_runner_with_logger_hook(&cli.run.base, |logger, _| {
+                    logger.with_detailed_output(false);
+                    logger.with_max_level(log::LevelFilter::Info);
+                })?
+            } else {
+                cli.create_runner(&cli.run.base)?
+            };
+
             runner.run_node_until_exit(|config| async move {
                 service::new_full(config, cli.no_hardware_benchmarks)
                     .map_err(sc_cli::Error::Service)
