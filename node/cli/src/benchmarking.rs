67dbd702c9ed1d1fd6533feec2c8d308f30b6973
Vadim Smirnov <vs@gear-tech.io>
2022-12-26 16:58:33 +0300

    Bump substrate to v0.9.36 (#2020)

--- node/cli/src/benchmarking.rs
+++ node/cli/src/benchmarking.rs
@@ -279,15 +279,12 @@ impl frame_benchmarking_cli::ExtrinsicBuilder for TransferKeepAliveBuilder {
 }
 
 /// Generates inherent data for the `benchmark overhead` command.
-///
-/// Note: Should only be used for benchmarking.
 pub fn inherent_benchmark_data() -> Result<InherentData> {
     let mut inherent_data = InherentData::new();
     let d = Duration::from_millis(0);
     let timestamp = sp_timestamp::InherentDataProvider::new(d.into());
 
-    timestamp
-        .provide_inherent_data(&mut inherent_data)
+    futures::executor::block_on(timestamp.provide_inherent_data(&mut inherent_data))
         .map_err(|e| format!("creating inherent data: {e:?}"))?;
     Ok(inherent_data)
 }
