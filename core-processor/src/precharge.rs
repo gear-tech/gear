5525d79cbad14ac5d2d0d680a2209b585b7aa758
Arsenii Lyashenko <arsenylyashenko.3@gmail.com>
2023-01-24 13:05:47 +0300

    Remove whitespace in copyright
    Co-authored-by: Shamil <66209982+shamilsan@users.noreply.github.com>

--- core-processor/src/precharge.rs
+++ core-processor/src/precharge.rs
@@ -1,4 +1,4 @@
-// Copyright (C)  2023 Gear Technologies Inc.
+// Copyright (C) 2023 Gear Technologies Inc.
 // SPDX-License-Identifier: GPL-3.0-or-later WITH Classpath-exception-2.0
 //
 // This program is free software: you can redistribute it and/or modify
