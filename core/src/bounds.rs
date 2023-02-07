f4d818b051ec4809a79992fb475954a7ede7b538
Mikita Stryzhak <mikita.stryzhak@gmail.com>
2023-02-07 10:46:48 +0000

    backup
    Signed-off-by: Mikita Stryzhak <mikita.stryzhak@gmail.com>

--- core/src/bounds.rs
+++ core/src/bounds.rs
@@ -17,7 +17,9 @@
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 use codec::{Decode, Encode};
-use sp_runtime::traits::{Get, One, SaturatedConversion, Saturating, UniqueSaturatedInto, Zero, Bounded};
+use sp_runtime::traits::{
+    Bounded, Get, One, SaturatedConversion, Saturating, UniqueSaturatedInto, Zero,
+};
 
 /// Hold bound, specifying cost of storing, expected block number for task to
 /// create on it, deadlines and durations of holding.
@@ -32,7 +34,11 @@ pub struct HoldBound<BlockNumber: Saturating + Zero, Cost: Saturating + Zero> {
 // `unused` allowed because some fns may be used in future, but clippy
 // doesn't allow this due to `pub(crate)` visibility.
 #[allow(unused)]
-impl<BlockNumber: Saturating + Zero + Bounded, Cost: Saturating + Zero + Bounded + From<BlockNumber>> HoldBound<BlockNumber, Cost> {
+impl<
+        BlockNumber: Saturating + Zero + Bounded,
+        Cost: Saturating + Zero + Bounded + From<BlockNumber>,
+    > HoldBound<BlockNumber, Cost>
+{
     /// Creates cost builder for hold bound.
     pub fn by(cost: Cost) -> HoldBoundCost<T> {
         assert!(!cost.is_zero());
@@ -73,5 +79,67 @@ impl<BlockNumber: Saturating + Zero + Bounded, Cost: Saturating + Zero + Bounded
         self.deadline_duration(current_bn)
             .saturated_into::<Cost>()
             .saturating_mul(self.cost())
+    }
+}
+
+/// Cost builder for `HoldBound<T>`.
+#[derive(Clone, Debug, Decode, Encode, PartialEq, Eq, PartialOrd, Ord)]
+pub(crate) struct HoldBoundCost<
+    BlockNumber: Saturating + Zero + Bounded,
+    Cost: Saturating + Zero + Bounded + From<BlockNumber>,
+>(Cost);
+
+#[allow(unused)]
+impl<
+        BlockNumber: Saturating + Zero + Bounded,
+        Cost: Saturating + Zero + Bounded + From<BlockNumber>,
+    > HoldBoundCost<BlockNumber, Cost>
+{
+    /// Creates bound to specific given block number.
+    pub fn at(self, expected: BlockNumber) -> HoldBound<T> {
+        HoldBound {
+            cost: self.0,
+            expected,
+        }
+    }
+
+    /// Creates bound to specific given deadline block number.
+    pub fn deadline(self, deadline: BlockNumber) -> HoldBound<T> {
+        let expected = deadline.saturating_sub(CostsPerBlockOf::<T>::reserve_for());
+
+        self.at(expected)
+    }
+
+    /// Creates bound for given duration since current block.
+    pub fn duration(self, duration: BlockNumber>) -> HoldBound<T> {
+        let expected = Pallet::<T>::block_number().saturating_add(duration);
+
+        self.at(expected)
+    }
+
+    /// Creates maximal available bound for given gas limit.
+    pub fn maximum_for(self, gas: GasBalanceOf<T>) -> HoldBound<T> {
+        let deadline_duration = gas
+            .saturating_div(self.0.max(One::one()))
+            .saturated_into::<BlockNumberFor<T>>();
+
+        let deadline = Pallet::<T>::block_number().saturating_add(deadline_duration);
+
+        self.deadline(deadline)
+    }
+
+    /// Creates maximal available bound for given message id,
+    /// by querying it's gas limit.
+    pub fn maximum_for_message(self, message_id: MessageId) -> HoldBound<T> {
+        // Querying gas limit. Fails in cases of `GasTree` invalidations.
+        let gas_limit = GasHandlerOf::<T>::get_limit(message_id)
+            .unwrap_or_else(|e| unreachable!("GasTree corrupted! {:?}", e));
+
+        self.maximum_for(gas_limit)
+    }
+
+    // Zero-duration hold bound.
+    pub fn zero(self) -> HoldBound<T> {
+        self.at(Pallet::<T>::block_number())
     }
 }
