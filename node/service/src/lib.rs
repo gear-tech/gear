c1c7578bec3bc91105684f513207bc201dd81e54
Shamil <66209982+shamilsan@users.noreply.github.com>
2023-02-11 10:14:02 +0300

    Update deps

--- node/service/src/lib.rs
+++ node/service/src/lib.rs
@@ -353,7 +353,7 @@ where
 }
 
 /// Creates a full service from the configuration.
-pub fn new_full_base<RuntimeApi, ExecutorDispatch, Runtime, Extrinsic>(
+pub fn new_full_base<RuntimeApi, ExecutorDispatch>(
     mut config: Configuration,
     disable_hardware_benchmarks: bool,
     with_startup_data: impl FnOnce(
