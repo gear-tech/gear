18cc2cfc5af3706270b7ce92db8f9a031b206362
Arseniy Lyashenko <arsenylyashenko.3@gmail.com>
2023-02-21 19:40:05 +0300

    Add missing import

--- utils/wasm-builder/src/cargo_command.rs
+++ utils/wasm-builder/src/cargo_command.rs
@@ -16,7 +16,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-use anyhow::{ensure, Result};
+use anyhow::{ensure, Context, Result};
 use std::{path::PathBuf, process::Command};
 
 use crate::builder_error::BuilderError;
