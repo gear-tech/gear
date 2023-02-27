b481d0ea7c9a83b1b963bce1d8bbf5fc678df9dd
clearloop <26088946+clearloop@users.noreply.github.com>
2023-02-28 02:53:30 +0800

    ci(gcli): remove feature bin as well here

--- gcli/tests/gear.rs
+++ gcli/tests/gear.rs
@@ -16,7 +16,6 @@
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-#![cfg(feature = "bin")]
 use common::env;
 use gsdk::{result::Error, Api};
 use std::path::PathBuf;
