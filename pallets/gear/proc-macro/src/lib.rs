11c3b315c1a947ff112cb9d24e8a1f76624d6cdf
Vadim Smirnov <vs@gear-tech.io>
2023-03-01 11:02:50 +0100

    Update gear-proc-macro for Weights

--- pallets/gear/proc-macro/src/lib.rs
+++ pallets/gear/proc-macro/src/lib.rs
@@ -23,29 +23,26 @@
 extern crate alloc;
 
 use alloc::string::ToString;
-use proc_macro2::TokenStream;
+use proc_macro::TokenStream;
+use proc_macro2::TokenStream as TokenStream2;
 use quote::{quote, quote_spanned};
-use syn::{parse_macro_input, spanned::Spanned, Data, DataStruct, DeriveInput, Fields, Ident};
-
+use syn::{parse_macro_input, spanned::Spanned, Data, DeriveInput, Ident};
 /// This derives `Debug` for a struct where each field must be of some numeric type.
 /// It interprets each field as its represents some weight and formats it as times so that
 /// it is readable by humans.
 #[proc_macro_derive(WeightDebug)]
-pub fn derive_weight_debug(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+pub fn derive_weight_debug(input: TokenStream) -> TokenStream {
     derive_debug(input, format_weight)
 }
 
 /// This is basically identical to the std libs Debug derive but without adding any
 /// bounds to existing generics.
 #[proc_macro_derive(ScheduleDebug)]
-pub fn derive_schedule_debug(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+pub fn derive_schedule_debug(input: TokenStream) -> TokenStream {
     derive_debug(input, format_default)
 }
 
-fn derive_debug(
-    input: proc_macro::TokenStream,
-    fmt: impl Fn(&Ident) -> TokenStream,
-) -> proc_macro::TokenStream {
+fn derive_debug(input: TokenStream, fmt: impl Fn(&Ident) -> TokenStream2) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
     let name = &input.ident;
     let (impl_generics, ty_generics, where_clause) = input.generics.split_for_impl();
@@ -66,7 +63,7 @@ fn derive_debug(
     let fields = {
         drop(fmt);
         drop(data);
-        TokenStream::new()
+        TokenStream2::new()
     };
 
     let tokens = quote! {
@@ -83,9 +80,11 @@ fn derive_debug(
     tokens.into()
 }
 
-/// This is only used when the `full` feature is activated.
+/// This is only used then the `full` feature is activated.
 #[cfg(feature = "full")]
-fn iterate_fields(data: &DataStruct, fmt: impl Fn(&Ident) -> TokenStream) -> TokenStream {
+fn iterate_fields(data: &syn::DataStruct, fmt: impl Fn(&Ident) -> TokenStream2) -> TokenStream2 {
+    use syn::Fields;
+
     match &data.fields {
         Fields::Named(fields) => {
             let recurse = fields.named.iter().filter_map(|f| {
@@ -111,30 +110,33 @@ fn iterate_fields(data: &DataStruct, fmt: impl Fn(&Ident) -> TokenStream) -> Tok
     }
 }
 
-fn format_weight(field: &Ident) -> TokenStream {
+fn format_weight(field: &Ident) -> TokenStream2 {
     quote_spanned! { field.span() =>
-        &if self.#field > 1_000_000_000 {
+        &if self.#field.ref_time() > 1_000_000_000 {
             format!(
-                "{:.1?} ms",
-                Fixed::saturating_from_rational(self.#field, 1_000_000_000).to_float()
+                "{:.1?} ms, {} bytes",
+                Fixed::saturating_from_rational(self.#field.ref_time(), 1_000_000_000).to_float(),
+                self.#field.proof_size()
             )
-        } else if self.#field > 1_000_000 {
+        } else if self.#field.ref_time() > 1_000_000 {
             format!(
-                "{:.1?} µs",
-                Fixed::saturating_from_rational(self.#field, 1_000_000).to_float()
+                "{:.1?} µs, {} bytes",
+                Fixed::saturating_from_rational(self.#field.ref_time(), 1_000_000).to_float(),
+                self.#field.proof_size()
             )
-        } else if self.#field > 1_000 {
+        } else if self.#field.ref_time() > 1_000 {
             format!(
-                "{:.1?} ns",
-                Fixed::saturating_from_rational(self.#field, 1_000).to_float()
+                "{:.1?} ns, {} bytes",
+                Fixed::saturating_from_rational(self.#field.ref_time(), 1_000).to_float(),
+                self.#field.proof_size()
             )
         } else {
-            format!("{} ps", self.#field)
+            format!("{} ps, {} bytes", self.#field.ref_time(), self.#field.proof_size())
         }
     }
 }
 
-fn format_default(field: &Ident) -> TokenStream {
+fn format_default(field: &Ident) -> TokenStream2 {
     quote_spanned! { field.span() =>
         &self.#field
     }
