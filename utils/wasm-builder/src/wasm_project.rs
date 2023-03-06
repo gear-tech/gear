bfcd1e0e4e1d533145912ecd8cb84217d12ab4ec
Dmitry Novikov <novikov.dm.al@gmail.com>
2023-03-06 20:10:56 +0300

    Use crate name in `meta.txt` as prefix

--- utils/wasm-builder/src/wasm_project.rs
+++ utils/wasm-builder/src/wasm_project.rs
@@ -199,11 +199,18 @@ impl WasmProject {
 
         // Write metadata
         if let Some(metadata) = &self.project_type.metadata() {
-            let wasm_meta_path = self.original_dir.join("meta.txt");
+            let file_base_name = self
+                .file_base_name
+                .as_ref()
+                .expect("Run `WasmProject::create_project()` first");
+
+            let wasm_meta_path = self
+                .original_dir
+                .join([file_base_name, ".meta.txt"].concat());
             let wasm_meta_hash_path = self.original_dir.join(".metahash");
 
             smart_fs::write_metadata(wasm_meta_path, metadata)
-                .context("unable to write `meta.txt`")?;
+                .context("unable to write `*.meta.txt`")?;
 
             smart_fs::write(wasm_meta_hash_path, format!("{:?}", metadata.hash()))
                 .context("unable to write `.metahash`")?;
