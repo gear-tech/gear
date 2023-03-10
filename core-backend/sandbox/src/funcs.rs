a8e1698a6a0dfc333e69ed9c47e74074a6a4f5c3
Arsenii Lyashenko <arsenylyashenko.3@gmail.com>
2023-03-10 16:55:53 +0300

    Remove redundant `Ext` methods (#2365)

--- core-backend/sandbox/src/funcs.rs
+++ core-backend/sandbox/src/funcs.rs
@@ -314,8 +314,6 @@ where
         let inheritor_id_ptr = args.iter().read()?;
 
         ctx.run(RuntimeCosts::Exit, |ctx| -> Result<(), _> {
-            ctx.ext.exit()?;
-
             let read_inheritor_id = ctx.register_read_decoded(inheritor_id_ptr);
             let inheritor_id = ctx.read_decoded(read_inheritor_id)?;
             Err(ActorTerminationReason::Exit(inheritor_id).into())
@@ -905,8 +903,7 @@ where
     pub fn leave(ctx: &mut Runtime<E>, _args: &[Value]) -> SyscallOutput {
         sys_trace!(target: "syscall::gear", "leave");
 
-        ctx.run(RuntimeCosts::Leave, |ctx| {
-            ctx.ext.leave()?;
+        ctx.run(RuntimeCosts::Leave, |_ctx| {
             Err(ActorTerminationReason::Leave.into())
         })
     }
@@ -1066,8 +1063,9 @@ where
     pub fn out_of_gas(ctx: &mut Runtime<E>, _args: &[Value]) -> SyscallOutput {
         sys_trace!(target: "syscall::gear", "out_of_gas");
 
-        let reason = ctx.ext.out_of_gas().into_termination_reason();
-        ctx.set_termination_reason(reason);
+        ctx.set_termination_reason(
+            ActorTerminationReason::Trap(TrapExplanation::GasLimitExceeded).into(),
+        );
 
         Err(HostError)
     }
@@ -1076,8 +1074,7 @@ where
     pub fn out_of_allowance(ctx: &mut Runtime<E>, _args: &[Value]) -> SyscallOutput {
         sys_trace!(target: "syscall::gear", "out_of_allowance");
 
-        let reason = ctx.ext.out_of_allowance().into_termination_reason();
-        ctx.set_termination_reason(reason);
+        ctx.set_termination_reason(ActorTerminationReason::GasAllowanceExceeded.into());
 
         Err(HostError)
     }
