d54cc87b0561f7263c1c56fbe8d611dd74cb7f2d
Arseniy Lyashenko <arsenylyashenko.3@gmail.com>
2023-03-14 21:07:03 +0300

    Update setup_machine.sh

--- scripts/benchmarking/setup_machine.sh
+++ scripts/benchmarking/setup_machine.sh
@@ -5,14 +5,14 @@ echo 0 > /proc/sys/kernel/randomize_va_space
 echo
 
 echo "Disabling frequency scaling"
-for i in /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor
+for i in /sys/devices/system/cpu/cpu[0-9]*
 do
-  echo performance > "/sys/devices/system/cpu/cpu{i}/cpufreq/scaling_governor"
+  echo performance > "$i/cpufreq/scaling_governor"
 done
 echo
 
 echo "Disabling SMT"
-echo off | sudo tee /sys/devices/system/cpu/smt/control
+echo off > /sys/devices/system/cpu/smt/control
 echo "Warning: To enable SMT back it's advised to reboot system"
 echo
 
@@ -22,6 +22,6 @@ if [ -f /sys/devices/system/cpu/intel_pstate ]; then
   echo 1 > /sys/devices/system/cpu/intel_pstate/no_turbo
 else
   # AMD
-  echo 0 | sudo tee /sys/devices/system/cpu/cpufreq/boost
+  echo 0 > /sys/devices/system/cpu/cpufreq/boost
 fi
 echo
