fa58fa6f454b117ff3dfce1810a7fac2bccc324f
Vadim Smirnov <vs@gear-tech.io>
2023-03-15 21:05:25 +0300

    Reduce payload size

--- gclient/tests/upload.rs
+++ gclient/tests/upload.rs
@@ -144,14 +144,6 @@ async fn get_mailbox() -> anyhow::Result<()> {
             (call $reply_push (i32.const 0) (i32.const 0xfa00) (i32.const 100))
             (call $reply_push (i32.const 0) (i32.const 0xfa00) (i32.const 100))
             (call $reply_push (i32.const 0) (i32.const 0xfa00) (i32.const 100))
-            (call $reply_push (i32.const 0) (i32.const 0xfa00) (i32.const 100))
-            (call $reply_push (i32.const 0) (i32.const 0xfa00) (i32.const 100))
-            (call $reply_push (i32.const 0) (i32.const 0xfa00) (i32.const 100))
-            (call $reply_push (i32.const 0) (i32.const 0xfa00) (i32.const 100))
-            (call $reply_push (i32.const 0) (i32.const 0xfa00) (i32.const 100))
-            (call $reply_push (i32.const 0) (i32.const 0xfa00) (i32.const 100))
-            (call $reply_push (i32.const 0) (i32.const 0xfa00) (i32.const 100))
-            (call $reply_push (i32.const 0) (i32.const 0xfa00) (i32.const 100))
 
             ;; sending commit
             (call $reply_commit (i32.const 10) (i32.const 0) (i32.const 200))
@@ -198,7 +190,7 @@ async fn get_mailbox() -> anyhow::Result<()> {
     assert_eq!(mailbox.len(), 5);
 
     for msg in mailbox {
-        assert_eq!(msg.0.payload().len(), 1000 * 1024); // 1MB payload
+        assert_eq!(msg.0.payload().len(), 500 * 1024); // 500KB payload
         assert!(msg.0.payload().starts_with(b"PONG"));
     }
 
