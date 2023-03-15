14fa293af527f8a9c691dd17c0ce7984844015a4
Gregory Sobol <grishasobol@mail.ru>
2023-03-15 18:35:18 +0300

    fix bench

--- pallets/gear/src/benchmarking/mod.rs
+++ pallets/gear/src/benchmarking/mod.rs
@@ -989,7 +989,7 @@ benchmarks! {
     }
 
     gr_reply_push_input_per_kb {
-        let n in 0 .. T::Schedule::get().limits.payload_len / 1024;
+        let n in 0 .. MAX_PAYLOAD_LEN_KB / 1024;
         let mut res = None;
         let exec = Benches::<T>::gr_reply_push_input_per_kb(n)?;
     }: {
