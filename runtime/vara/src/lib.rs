ea26723f2531147eb516d96271e9f3542ac57312
Vadim Smirnov <vs@gear-tech.io>
2023-03-23 12:19:19 +0300

    [vara] Add ImOnline event handler to gear authorship (#2461)

--- runtime/vara/src/lib.rs
+++ runtime/vara/src/lib.rs
@@ -119,7 +119,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     // The version of the runtime specification. A full node will not attempt to use its native
     //   runtime in substitute for the on-chain Wasm runtime unless all of `spec_name`,
     //   `spec_version`, and `authoring_version` are the same between Wasm and native.
-    spec_version: 100,
+    spec_version: 120,
     impl_version: 1,
     apis: RUNTIME_API_VERSIONS,
     transaction_version: 1,
@@ -266,7 +266,7 @@ impl pallet_grandpa::Config for Runtime {
 
 impl pallet_authorship::Config for Runtime {
     type FindAuthor = pallet_session::FindAccountFromAuthorIndex<Self, Babe>;
-    type EventHandler = ();
+    type EventHandler = ImOnline;
 }
 
 parameter_types! {
