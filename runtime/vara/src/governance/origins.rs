3d05732a5507409c598d78d7349a83b950f283c0
Vadim Smirnov <vs@gear-tech.io>
2023-03-25 17:49:21 +0700

    [vara] Make the fellowship self-governed collective (#2471)

--- runtime/vara/src/governance/origins.rs
+++ runtime/vara/src/governance/origins.rs
@@ -47,46 +47,64 @@ pub mod pallet_custom_origins {
         ReferendumKiller,
         /// Origin able to dispatch a whitelisted call.
         WhitelistedCaller,
-        /// Origin commanded by any members of the Gear Fellowship (no grade needed).
+        /// Origin commanded by any members of the Vara Fellowship (no grade needed).
         FellowshipInitiates,
-        /// Origin commanded by Gear Fellows (1st grade or greater).
+        /// Origin commanded by Vara Fellows (1st grade or greater).
         Fellows,
-        /// Origin commanded by Gear Experts (2nd grade or greater).
+        /// Origin commanded by Vara Experts (2nd grade or greater).
         FellowshipExperts,
-        /// Origin commanded by Gear Masters (3rd grade of greater).
+        /// Origin commanded by Vara Masters (3rd grade of greater).
         FellowshipMasters,
+        /// Origin commanded by rank 1 of the Vara Fellowship and with a success of 1.
+        Fellowship1Dan,
+        /// Origin commanded by rank 2 of the Vara Fellowship and with a success of 2.
+        Fellowship2Dan,
+        /// Origin commanded by rank 3 of the Vara Fellowship and with a success of 3.
+        Fellowship3Dan,
+        /// Origin commanded by rank 4 of the Vara Fellowship and with a success of 4.
+        Fellowship4Dan,
+        /// Origin commanded by rank 5 of the Vara Fellowship and with a success of 5.
+        Fellowship5Dan,
+        /// Origin commanded by rank 6 of the Vara Fellowship and with a success of 6.
+        Fellowship6Dan,
+        /// Origin commanded by rank 7 of the Vara Fellowship and with a success of 7.
+        Fellowship7Dan,
+        /// Origin commanded by rank 8 of the Vara Fellowship and with a success of 8.
+        Fellowship8Dan,
+        /// Origin commanded by rank 9 of the Vara Fellowship and with a success of 9.
+        Fellowship9Dan,
     }
 
     macro_rules! decl_unit_ensures {
-		( $name:ident: $success_type:ty = $success:expr ) => {
-			pub struct $name;
-			impl<O: Into<Result<Origin, O>> + From<Origin>>
-				EnsureOrigin<O> for $name
-			{
-				type Success = $success_type;
-				fn try_origin(o: O) -> Result<Self::Success, O> {
-					o.into().and_then(|o| match o {
-						Origin::$name => Ok($success),
-						r => Err(O::from(r)),
-					})
-				}
-				#[cfg(feature = "runtime-benchmarks")]
-				fn try_successful_origin() -> Result<O, ()> {
-					Ok(O::from(Origin::$name))
-				}
-			}
-		};
-		( $name:ident ) => { decl_unit_ensures! { $name : () = () } };
-		( $name:ident: $success_type:ty = $success:expr, $( $rest:tt )* ) => {
-			decl_unit_ensures! { $name: $success_type = $success }
-			decl_unit_ensures! { $( $rest )* }
-		};
-		( $name:ident, $( $rest:tt )* ) => {
-			decl_unit_ensures! { $name }
-			decl_unit_ensures! { $( $rest )* }
-		};
-		() => {}
-	}
+        ( $name:ident: $success_type:ty = $success:expr ) => {
+            pub struct $name;
+            impl<O: Into<Result<Origin, O>> + From<Origin>>
+                EnsureOrigin<O> for $name
+            {
+                type Success = $success_type;
+                fn try_origin(o: O) -> Result<Self::Success, O> {
+                    o.into().and_then(|o| match o {
+                        Origin::$name => Ok($success),
+                        r => Err(O::from(r)),
+                    })
+                }
+                #[cfg(feature = "runtime-benchmarks")]
+                fn try_successful_origin() -> Result<O, ()> {
+                    Ok(O::from(Origin::$name))
+                }
+            }
+        };
+        ( $name:ident ) => { decl_unit_ensures! { $name : () = () } };
+        ( $name:ident: $success_type:ty = $success:expr, $( $rest:tt )* ) => {
+            decl_unit_ensures! { $name: $success_type = $success }
+            decl_unit_ensures! { $( $rest )* }
+        };
+        ( $name:ident, $( $rest:tt )* ) => {
+            decl_unit_ensures! { $name }
+            decl_unit_ensures! { $( $rest )* }
+        };
+        () => {}
+    }
     decl_unit_ensures!(
         StakingAdmin,
         Treasurer,
@@ -96,8 +114,55 @@ pub mod pallet_custom_origins {
         ReferendumKiller,
         WhitelistedCaller,
         FellowshipInitiates: u16 = 0,
-        Fellows: u16 = 1,
-        FellowshipExperts: u16 = 2,
-        FellowshipMasters: u16 = 3,
+        Fellows: u16 = 3,
+        FellowshipExperts: u16 = 5,
+        FellowshipMasters: u16 = 7,
     );
+
+    macro_rules! decl_ensure {
+        (
+            $vis:vis type $name:ident: EnsureOrigin<Success = $success_type:ty> {
+                $( $item:ident = $success:expr, )*
+            }
+        ) => {
+            $vis struct $name;
+            impl<O: Into<Result<Origin, O>> + From<Origin>>
+                EnsureOrigin<O> for $name
+            {
+                type Success = $success_type;
+                fn try_origin(o: O) -> Result<Self::Success, O> {
+                    o.into().and_then(|o| match o {
+                        $(
+                            Origin::$item => Ok($success),
+                        )*
+                        r => Err(O::from(r)),
+                    })
+                }
+                #[cfg(feature = "runtime-benchmarks")]
+                fn try_successful_origin() -> Result<O, ()> {
+                    // By convention the more privileged origins go later, so for greatest chance
+                    // of success, we want the last one.
+                    let _result: Result<O, ()> = Err(());
+                    $(
+                        let _result: Result<O, ()> = Ok(O::from(Origin::$item));
+                    )*
+                    _result
+                }
+            }
+        }
+    }
+
+    decl_ensure! {
+        pub type EnsureFellowship: EnsureOrigin<Success = u16> {
+            Fellowship1Dan = 1,
+            Fellowship2Dan = 2,
+            Fellowship3Dan = 3,
+            Fellowship4Dan = 4,
+            Fellowship5Dan = 5,
+            Fellowship6Dan = 6,
+            Fellowship7Dan = 7,
+            Fellowship8Dan = 8,
+            Fellowship9Dan = 9,
+        }
+    }
 }
