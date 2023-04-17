dd49c84921091571258e2274c22b85cfcaf004ca
Sabaun Taraki <taraki.sabaun.rl@gmail.com>
2023-04-17 15:22:50 +0300

    Remove commented mutations

--- core-processor/src/processing.rs
+++ core-processor/src/processing.rs
@@ -38,11 +38,7 @@ use gear_core::{
 };
 use gear_core_errors::{SimpleReplyError, SimpleSignalError};
 
-// #[cfg(feature = "fuzz")]
-// use mutagen::mutate;
-
 /// Process program & dispatch for it and return journal for updates.
-// #[cfg_attr(feature = "fuzz", mutate)]
 pub fn process<E>(
     block_config: &BlockConfig,
     execution_context: ProcessExecutionContext,
@@ -55,11 +51,6 @@ where
     <E::Ext as Ext>::Error: BackendExtError,
 {
     use crate::precharge::SuccessfulDispatchResultKind::*;
-
-    // // run fuzzer with `cargo +nightly fuzz run --release --sanitizer=none  main -- -rss_limit_mb=8192 -seed=377328566`
-    // if false {
-    //     panic!("GOT YOU!");
-    // }
 
     let BlockConfig {
         block_info,
