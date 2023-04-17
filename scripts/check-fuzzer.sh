6588e103ff898f201c240861d86b076310866d4d
Sabaun Taraki <taraki.sabaun.rl@gmail.com>
2023-04-17 15:20:17 +0300

    Implement CI mutation test for the fuzzer

---
+++ scripts/check-fuzzer.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+function main() {
+    cargo +nightly build -p gear-common
+    CONSUME_WITH_LOCK_MUTATION=$(jq 'select(.mutation.fn_name=="consume") | select(.mutation.mutator=="unop_not") | select(.mutation.location_in_file=="599:12-599:13") | .id' target/mutagen/mutations)
+    echo " >> Running fuzzer check with mutation id $CONSUME_WITH_LOCK_MUTATION"
+    MUTATION_ID=$CONSUME_WITH_LOCK_MUTATION ./scripts/gear.sh test fuzz &> fuzz_run
+
+    if cat fuzz_run | grep -q -P '^(?=.*GasTree corrupted)(?=.*ConsumedWithLock)' ; then
+        echo "Success"
+        exit 0
+    else
+        echo "Failed"
+        exit 1
+    fi
+}
+
+main
