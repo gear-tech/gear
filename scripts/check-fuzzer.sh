0fda16721025ae3c98d3fa827305ddef8fd4609e
Sabaun Taraki <taraki.sabaun.rl@gmail.com>
2023-04-18 19:53:39 +0300

    Fix

--- scripts/check-fuzzer.sh
+++ scripts/check-fuzzer.sh
@@ -4,7 +4,8 @@ main() {
     cargo +nightly build -p gear-common
     CONSUME_WITH_LOCK_MUTATION=$(jq 'select(.mutation.fn_name=="consume") | select(.mutation.mutator=="unop_not") | select(.mutation.location_in_file=="605:12-605:13") | .id' target/mutagen/mutations)
     echo " >> Running fuzzer check with mutation id $CONSUME_WITH_LOCK_MUTATION"
-    MUTATION_ID=$CONSUME_WITH_LOCK_MUTATION ./scripts/gear.sh test fuzz &> fuzz_run
+    MUTATION_ID=$CONSUME_WITH_LOCK_MUTATION ./scripts/gear.sh test fuzz > fuzz_run 2>&1
+
 
     if cat fuzz_run | grep -q -P '^(?=.*GasTree corrupted)(?=.*ConsumedWithLock)' ; then
         echo "Success"
