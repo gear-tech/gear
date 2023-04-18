2415c734077b4e9cd4d22c083d53005c32df5e62
Sabaun Taraki <taraki.sabaun.rl@gmail.com>
2023-04-18 18:36:31 +0300

    Change dir for fuzzing seeds

--- scripts/check-fuzzer.sh
+++ scripts/check-fuzzer.sh
@@ -2,19 +2,17 @@
 
 main() {
     cargo +nightly build -p gear-common
-    jq 'select(.mutation.fn_name=="consume") | select(.mutation.mutator=="unop_not")' target/mutagen/mutations
-    cat -n common/src/gas_provider/internal.rs
-    CONSUME_WITH_LOCK_MUTATION=$(jq 'select(.mutation.fn_name=="consume") | select(.mutation.mutator=="unop_not") | select(.mutation.location_in_file=="600:12-600:13") | .id' target/mutagen/mutations)
+    CONSUME_WITH_LOCK_MUTATION=$(jq 'select(.mutation.fn_name=="consume") | select(.mutation.mutator=="unop_not") | select(.mutation.location_in_file=="605:12-605:13") | .id' target/mutagen/mutations)
     echo " >> Running fuzzer check with mutation id $CONSUME_WITH_LOCK_MUTATION"
-    # MUTATION_ID=$CONSUME_WITH_LOCK_MUTATION ./scripts/gear.sh test fuzz &> fuzz_run
+    MUTATION_ID=$CONSUME_WITH_LOCK_MUTATION ./scripts/gear.sh test fuzz &> fuzz_run
 
-    # if cat fuzz_run | grep -q -P '^(?=.*GasTree corrupted)(?=.*ConsumedWithLock)' ; then
-    #     echo "Success"
-    #     exit 0
-    # else
-    #     echo "Failed"
-    #     exit 1
-    # fi
+    if cat fuzz_run | grep -q -P '^(?=.*GasTree corrupted)(?=.*ConsumedWithLock)' ; then
+        echo "Success"
+        exit 0
+    else
+        echo "Failed"
+        exit 1
+    fi
 }
 
 main
