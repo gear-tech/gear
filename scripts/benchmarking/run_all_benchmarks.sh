8e07e21f11508f3ea6992bff369c06d9ed5a92e3
StackOverflowExcept1on <109800286+StackOverflowExcept1on@users.noreply.github.com>
2023-04-25 12:40:13 +0300

    added test workflow for benchmarks

--- scripts/benchmarking/run_all_benchmarks.sh
+++ scripts/benchmarking/run_all_benchmarks.sh
@@ -129,6 +129,9 @@ for PALLET in "${PALLETS[@]}"; do
   fi
 done
 
+# TODO: delete/add flag to skip
+exit
+
 echo "[+] Benchmarking the machine..."
 OUTPUT=$(
   $GEAR benchmark machine --chain=$chain_spec --allow-fail 2>&1
