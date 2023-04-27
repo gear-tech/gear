e49add1f0d3e37122cca57d6b157da7a64a2efd4
gsobol <grishasobol@mail.ru>
2023-04-27 06:22:33 +0000

    fix script

--- scripts/pin-rust-nightly.sh
+++ scripts/pin-rust-nightly.sh
@@ -13,13 +13,13 @@ os_name="$(uname)"
 if [ "$os_name" = "Darwin" ]; then
     rustup toolchain install nightly-$pin_date --component llvm-tools-preview
     rustup target add wasm32-unknown-unknown --toolchain nightly-$pin_date
-    rm -rf ~/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu
-    mv ~/.rustup/toolchains/nightly-$pin_date-x86_64-unknown-linux-gnu ~/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu
+    rm -rf ~/.rustup/toolchains/nightly-aarch64-apple-darwin
+    mv ~/.rustup/toolchains/nightly-$pin_date-aarch64-apple-darwin ~/.rustup/toolchains/nightly-aarch64-apple-darwin
 elif [ "$os_name" = "Linux" ]; then
     rustup toolchain install nightly-$pin_date --component llvm-tools-preview
     rustup target add wasm32-unknown-unknown --toolchain nightly-$pin_date
-    rm -rf ~/.rustup/toolchains/nightly-aarch64-apple-darwin
-    mv ~/.rustup/toolchains/nightly-$pin_date-aarch64-apple-darwin ~/.rustup/toolchains/nightly-aarch64-apple-darwin
+    rm -rf ~/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu
+    mv ~/.rustup/toolchains/nightly-$pin_date-x86_64-unknown-linux-gnu ~/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu
 else
     echo "Unknown operating system"
 fi
