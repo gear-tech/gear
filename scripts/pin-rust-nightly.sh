098023d5e7b353ac91670e02e54049fb26bfd621
Gregory Sobol <grishasobol@mail.ru>
2023-04-29 17:38:34 +0300

    fix PR

--- scripts/pin-rust-nightly.sh
+++ scripts/pin-rust-nightly.sh
@@ -11,18 +11,8 @@ fi
 
 pin_date=$1
 os_name="$(uname)"
-
-if [ "$os_name" = "Darwin" ]; then
-    suffix=$(rustc -Vv | grep "host: " | sed "s/^host: \(.*\)$/\1/")
-    rustup toolchain install nightly-$pin_date --component llvm-tools-preview
-    rustup target add wasm32-unknown-unknown --toolchain nightly-$pin_date
-    rm -rf ~/.rustup/toolchains/nightly-$suffix
-    mv ~/.rustup/toolchains/nightly-$pin_date-$suffix ~/.rustup/toolchains/nightly-$suffix
-elif [ "$os_name" = "Linux" ]; then
-    rustup toolchain install nightly-$pin_date --component llvm-tools-preview
-    rustup target add wasm32-unknown-unknown --toolchain nightly-$pin_date
-    rm -rf ~/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu
-    mv ~/.rustup/toolchains/nightly-$pin_date-x86_64-unknown-linux-gnu ~/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu
-else
-    echo "Unknown operating system"
-fi
+suffix=$(rustc -Vv | grep "host: " | sed "s/^host: \(.*\)$/\1/")
+rustup toolchain install nightly-$pin_date --component llvm-tools-preview
+rustup target add wasm32-unknown-unknown --toolchain nightly-$pin_date
+rm -rf ~/.rustup/toolchains/nightly-$suffix
+ln -s ~/.rustup/toolchains/nightly-$pin_date-$suffix ~/.rustup/toolchains/nightly-$suffix
