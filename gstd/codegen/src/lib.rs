5e37e028a69409a5d803fbe27826b7ef704cbf26
StackOverflowExcept1on <109800286+StackOverflowExcept1on@users.noreply.github.com>
2023-05-08 17:36:26 +0300

    also update comments

--- gstd/codegen/src/lib.rs
+++ gstd/codegen/src/lib.rs
@@ -328,12 +328,12 @@ pub fn async_init(attr: TokenStream, item: TokenStream) -> TokenStream {
 /// outputs:
 ///
 /// ```ignore
-/// /// Same as [`send_bytes`](crate::msg::send_bytes), but the program
+/// /// Same as [`send_bytes`](self::send_bytes), but the program
 /// /// will interrupt until the reply is received.
 /// ///
 /// /// # See also
 /// ///
-/// /// - [`send_bytes_for_reply_as`](crate::msg::send_bytes_for_reply_as)
+/// /// - [`send_bytes_for_reply_as`](self::send_bytes_for_reply_as)
 /// pub fn send_bytes_for_reply<T: AsRef<[u8]>>(
 ///     program: ActorId,
 ///     payload: T,
@@ -345,15 +345,15 @@ pub fn async_init(attr: TokenStream, item: TokenStream) -> TokenStream {
 ///     Ok(MessageFuture { waiting_reply_to })
 /// }
 ///
-/// /// Same as [`send_bytes`](crate::msg::send_bytes), but the program
+/// /// Same as [`send_bytes`](self::send_bytes), but the program
 /// /// will interrupt until the reply is received.
 /// ///
 /// /// The output should be decodable via SCALE codec.
 /// ///
 /// /// # See also
 /// ///
-/// /// - [`send_bytes_for_reply`](crate::msg::send_bytes_for_reply)
-/// /// - <https://docs.substrate.io/v3/advanced/scale-codec>
+/// /// - [`send_bytes_for_reply`](self::send_bytes_for_reply)
+/// /// - <https://docs.substrate.io/reference/scale-codec>
 /// pub fn send_bytes_for_reply_as<T: AsRef<[u8]>, D: Decode>(
 ///     program: ActorId,
 ///     payload: T,
