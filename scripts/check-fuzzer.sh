bbb1183d60c44a7ccbf15a5b210a70af7f4bedba
clearloop <26088946+clearloop@users.noreply.github.com>
2023-05-20 06:04:13 +0800

    Revert "chore(scripts): update mutation id in fuzzer"
    This reverts commit 743f8f47382530e04a0bdcbcce41e39d923d3fae.

--- scripts/check-fuzzer.sh
+++ scripts/check-fuzzer.sh
@@ -2,7 +2,7 @@
 
 main() {
     cargo +nightly build -p gear-common -F fuzz
-    CONSUME_WITH_LOCK_MUTATION=$(jq 'select(.mutation.fn_name=="consume") | select(.mutation.mutator=="unop_not") | select(.mutation.location_in_file=="537:12-537:13") | .id' target/mutagen/mutations)
+    CONSUME_WITH_LOCK_MUTATION=$(jq 'select(.mutation.fn_name=="consume") | select(.mutation.mutator=="unop_not") | select(.mutation.location_in_file=="542:12-542:13") | .id' target/mutagen/mutations)
     echo " >> Running fuzzer check with mutation id $CONSUME_WITH_LOCK_MUTATION"
     MUTATION_ID=$CONSUME_WITH_LOCK_MUTATION ./scripts/gear.sh test fuzz > fuzz_run 2>&1
 
