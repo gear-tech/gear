07797071944456cb0bcc324d4f8a57d50544ff83
Sabaun Taraki <taraki.sabaun.rl@gmail.com>
2023-05-29 17:10:08 +0300

    refactor(fuzzer): Migrate mutation tests from `mutagen` to a more stable `fail` crate (#2679)

--- scripts/check-fuzzer.sh
+++ scripts/check-fuzzer.sh
@@ -1,19 +1,17 @@
 #!/usr/bin/env sh
 
 main() {
-    cargo +nightly build -p gear-common -F fuzz
-    CONSUME_WITH_LOCK_MUTATION=$(jq 'select(.mutation.fn_name=="consume") | select(.mutation.mutator=="unop_not") | select(.mutation.location_in_file=="542:12-542:13") | .id' target/mutagen/mutations)
-    echo " >> Running fuzzer check with mutation id $CONSUME_WITH_LOCK_MUTATION"
-    MUTATION_ID=$CONSUME_WITH_LOCK_MUTATION ./scripts/gear.sh test fuzz > fuzz_run 2>&1
+    echo " >> Running fuzzer with failpoint"
+    RUST_BACKTRACE=1 FAILPOINTS=fail_fuzzer=return ./scripts/gear.sh test fuzz > fuzz_run 2>&1
 
-
-    if cat fuzz_run | grep -q -P '^(?=.*GasTree corrupted)(?=.*ConsumedWithLock)' ; then
+    if cat fuzz_run | grep -qzP '(?s)(?=.*GasTree corrupted)(?=.*NodeAlreadyExists)(?=.*\Qpallet_gear::pallet::Pallet<T>>::consume_and_retrieve\E)' ; then
         echo "Success"
         exit 0
     else
         echo "Failed"
         exit 1
     fi
+
 }
 
 main
