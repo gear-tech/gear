5a1b4be19904f6fa6eed3bba9f3e006a20bd58f0
Sabaun Taraki <taraki.sabaun.rl@gmail.com>
2023-05-31 15:03:28 +0300

    Set gas before returning error from memory manager for `gr_read`

--- core-backend/wasmi/src/funcs/mod.rs
+++ core-backend/wasmi/src/funcs/mod.rs
@@ -179,12 +179,11 @@ where
         let write_buffer = ctx.register_write(buffer_ptr, len);
 
         let mut memory = CallerWrap::memory(&mut ctx.caller, ctx.memory);
-        ctx.manager
-            .write(&mut memory, write_buffer, buffer, &mut gas_left)?;
-
+        let res = ctx.manager
+            .write(&mut memory, write_buffer, buffer, &mut gas_left);
         state.ext.set_gas_left(gas_left);
 
-        Result::<(), TerminationReason>::Ok(())
+        res.map_err(Into::<TerminationReason>::into)
     }
 
     #[host(cost = RuntimeCosts::Size)]
