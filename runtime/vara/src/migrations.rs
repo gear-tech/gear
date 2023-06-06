6430cc73f72058a1165942096cff02e0507c6e6c
Vadim Smirnov <ukint-vs@proton.me>
2023-06-06 21:16:38 +0300

    runtime(gear,vara): Remove validator_set migration·

--- runtime/vara/src/migrations.rs
+++ runtime/vara/src/migrations.rs
@@ -1,9 +1,6 @@
 use crate::*;
 
-use runtime_common::migration::SessionValidatorSetMigration;
-
 pub type Migrations = (
-    SessionValidatorSetMigration<Runtime>,
     pallet_gear_gas::migrations::v1::MigrateToV1<Runtime>,
     pallet_gear_scheduler::migration::MigrateToV2<Runtime>,
     pallet_gear_program::migration::MigrateToV2<Runtime>,
