576e2106c0261d5b64696a9d77963691cad124d8
Shamil <66209982+shamilsan@users.noreply.github.com>
2023-06-07 17:07:04 +0400

    chore: use Rust toolchain override

--- scripts/src/clippy.sh
+++ scripts/src/clippy.sh
@@ -21,14 +21,14 @@ EOF
 
 gear_clippy() {
   # `nightly`` is used for the workspace as the clippy check is run with `--all-features`.
-  SKIP_WASM_BUILD=1 SKIP_GEAR_RUNTIME_WASM_BUILD=1 SKIP_VARA_RUNTIME_WASM_BUILD=1 cargo +nightly clippy --workspace "$@" --exclude gear-runtime --exclude runtime-fuzzer --exclude runtime-fuzzer-fuzz -- --no-deps -D warnings
-  SKIP_WASM_BUILD=1 SKIP_GEAR_RUNTIME_WASM_BUILD=1 cargo +nightly clippy -p runtime-fuzzer -p runtime-fuzzer-fuzz -p gear-runtime --all-features -- --no-deps -D warnings
+  SKIP_WASM_BUILD=1 SKIP_GEAR_RUNTIME_WASM_BUILD=1 SKIP_VARA_RUNTIME_WASM_BUILD=1 cargo clippy --workspace "$@" --exclude gear-runtime --exclude runtime-fuzzer --exclude runtime-fuzzer-fuzz -- --no-deps -D warnings
+  SKIP_WASM_BUILD=1 SKIP_GEAR_RUNTIME_WASM_BUILD=1 cargo clippy -p runtime-fuzzer -p runtime-fuzzer-fuzz -p gear-runtime --all-features -- --no-deps -D warnings
 }
 
 # $1 - ROOT DIR
 examples_clippy() {
   cd "$1"/examples
-  SKIP_WASM_BUILD=1 cargo +nightly hack clippy --workspace --release -- --no-deps \
+  SKIP_WASM_BUILD=1 cargo hack clippy --workspace --release -- --no-deps \
 	  -A clippy::stable_sort_primitive \
     -D warnings
   cd "$1"
