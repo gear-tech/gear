0a08be793811b022c9efa18c759403f945447a6e
Vadim Smirnov <ukint-vs@proton.me>
2023-06-07 14:08:48 +0300

    remove unused migrations

--- runtime/vara/src/migrations.rs
+++ runtime/vara/src/migrations.rs
@@ -3,6 +3,5 @@ use crate::*;
 pub type Migrations = (
     pallet_gear_gas::migrations::v1::MigrateToV1<Runtime>,
     pallet_gear_scheduler::migration::MigrateToV2<Runtime>,
-    pallet_gear_program::migration::MigrateToV2<Runtime>,
     pallet_gear_gas::migrations::v2::MigrateToV2<Runtime>,
 );
