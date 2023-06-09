da0a6ed691856bbb5f788523c143dd41e41b52cb
Ilya Kalitov <tltsutltsu@mail.ru>
2023-06-09 07:43:15 +0300

    remove unnecessary imports in build script

--- examples/binaries/fungible-token/build.rs
+++ examples/binaries/fungible-token/build.rs
@@ -16,8 +16,6 @@
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-use ft_io::FungibleTokenMetadata;
-
 fn main() {
     gear_wasm_builder::build();
 }
