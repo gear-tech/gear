a9862801d5b6ba1865427b4d3d01a0fead2f8a2e
Sabaun Taraki <taraki.sabaun.rl@gmail.com>
2023-06-14 16:07:10 +0300

    Initial

--- core/src/reservation.rs
+++ core/src/reservation.rs
@@ -32,14 +32,33 @@ use scale_info::{
 /// Controls gas reservations states.
 #[derive(Debug, Clone)]
 pub struct GasReserver {
+    /// Message id withing which reservations are created
+    /// by the current instance of [`GasReserver`].
     message_id: MessageId,
+    /// Nonce used to generate [`ReservationId`]s.
+    ///
+    /// It's really important that if gas reserver is created
+    /// several times with the same `message_id`, value of this
+    /// field is re-used.
+    // TODO #2773
     nonce: u64,
+    /// Gas reservations states.
     states: GasReservationStates,
+    /// Maximum allowed reservations to be stored in `states`
+    ///
+    /// This field is used not only to control `states` during
+    /// one execution, but also during several execution using
+    /// gas reserver for the actor. To reach that `states` must
+    /// be set with reservation from previous executions of the
+    /// actor.
     max_reservations: u64,
 }
 
 impl GasReserver {
     /// Creates a new gas reserver.
+    ///
+    /// `map`, which is a [`BTreeMap`] of [`GasReservationSlot`]s,
+    /// will be converted to the [`HashMap`] of [`GasReservationState`]s.
     pub fn new(
         message_id: MessageId,
         nonce: u64,
@@ -50,11 +69,7 @@ impl GasReserver {
             message_id,
             nonce,
             states: {
-                let mut states = if map.len() <= max_reservations as usize {
-                    HashMap::with_capacity(max_reservations as usize)
-                } else {
-                    HashMap::new()
-                };
+                let mut states = HashMap::with_capacity(max_reservations as usize);
                 states.extend(map.into_iter().map(|(id, slot)| (id, slot.into())));
                 states
             },
@@ -62,6 +77,10 @@ impl GasReserver {
         }
     }
 
+    /// Checks that the number of existing and newly created reservations
+    /// in the `states` is less than `max_reservations`. Removed reservations,
+    /// which are stored with the [`GasReservationState::Removed`] state in the
+    /// `states`, aren't excluded from the check.
     fn check_execution_limit(&self) -> Result<(), ReservationError> {
         // operation might very expensive in the future
         // so we will store 2 numerics to optimize it maybe
@@ -82,6 +101,8 @@ impl GasReserver {
         }
     }
 
+    /// Fetches current state of the `nonce` and
+    /// updates its state by incrementing it.
     fn fetch_inc_nonce(&mut self) -> u64 {
         let nonce = self.nonce;
         self.nonce = nonce.saturating_add(1);
@@ -89,6 +110,10 @@ impl GasReserver {
     }
 
     /// Reserves gas.
+    ///
+    /// Creates a new reservation and returns it's id.
+    ///
+    /// Returns an error if maximum limit of reservations is reached.
     pub fn reserve(
         &mut self,
         amount: u64,
@@ -98,7 +123,8 @@ impl GasReserver {
 
         let id = ReservationId::generate(self.message_id, self.fetch_inc_nonce());
 
-        self.states.insert(
+        // TODO #2773
+        let maybe_reservation = self.states.insert(
             id,
             GasReservationState::Created {
                 amount,
@@ -107,10 +133,22 @@ impl GasReserver {
             },
         );
 
+        if maybe_reservation.is_some() {
+            unreachable!(
+                "Duplicate reservation was created with message id {} and nonce {}",
+                self.message_id, self.nonce,
+            );
+        }
+
         Ok(id)
     }
 
-    /// Unreserves gas.
+    /// Unreserves gas reserved withing `id` reservation.
+    ///
+    /// Return error if:
+    /// 1. Reservation doesn't exist.
+    /// 2. Reservation was "unreserved", so in [`GasReservationState::Removed`] state.
+    /// 3. Reservation was marked used.
     pub fn unreserve(&mut self, id: ReservationId) -> Result<u64, ReservationError> {
         let state = self
             .states
@@ -132,7 +170,11 @@ impl GasReserver {
         Ok(amount)
     }
 
-    /// Marks reservation as used to avoid double usage in sys-calls like `gr_reservation_send`.
+    /// Marks reservation as used.
+    ///
+    /// This allows to avoid double usage of the reservation
+    /// for sending a new message from execution of `message_id`
+    /// of current gas reserver.
     pub fn mark_used(&mut self, id: ReservationId) -> Result<(), ReservationError> {
         if let Some(
             GasReservationState::Created { used, .. } | GasReservationState::Exists { used, .. },
@@ -149,17 +191,17 @@ impl GasReserver {
         }
     }
 
-    /// Return reservation nonce.
+    /// Returns gas reservations current nonce.
     pub fn nonce(&self) -> u64 {
         self.nonce
     }
 
-    /// Get gas reservation states.
+    /// Gets gas reservations states.
     pub fn states(&self) -> &GasReservationStates {
         &self.states
     }
 
-    /// Convert into gas reservation map.
+    /// Convert current gas reserver into gas reservation map.
     pub fn into_map<F>(
         self,
         current_block_height: u32,
@@ -203,23 +245,23 @@ impl GasReserver {
     }
 }
 
-/// Gas reservation states.
+/// Gas reservations states.
 pub type GasReservationStates = HashMap<ReservationId, GasReservationState>;
 
 /// Gas reservation state.
 ///
-/// Used to control what reservation created, removed or nothing happened.
+/// Used to control whether reservation was created, removed or nothing happened.
 #[derive(Debug, Clone, Copy, Eq, PartialEq)]
 pub enum GasReservationState {
     /// Reservation exists.
     Exists {
         /// Amount of reserved gas.
         amount: u64,
-        /// Block number when reservation is crated.
+        /// Block number when reservation is created.
         start: u32,
         /// Block number when reservation will expire.
         finish: u32,
-        /// Whether reservation used.
+        /// Flag signalizing whether reservation is used.
         used: bool,
     },
     /// Reservation will be created.
@@ -228,7 +270,7 @@ pub enum GasReservationState {
         amount: u64,
         /// How many blocks reservation will live.
         duration: u32,
-        /// Whether reservation used.
+        /// Flag signalizing whether reservation is used.
         used: bool,
     },
     /// Reservation will be removed.
@@ -249,9 +291,9 @@ impl From<GasReservationSlot> for GasReservationState {
     }
 }
 
-/// Gas reservation map.
+/// Gas reservations map.
 ///
-/// Used across execution and exists in storage.
+/// Used across execution and is stored to storage.
 pub type GasReservationMap = BTreeMap<ReservationId, GasReservationSlot>;
 
 /// Gas reservation slot.
@@ -259,7 +301,7 @@ pub type GasReservationMap = BTreeMap<ReservationId, GasReservationSlot>;
 pub struct GasReservationSlot {
     /// Amount of reserved gas.
     pub amount: u64,
-    /// Block number when reservation is crated.
+    /// Block number when reservation is created.
     pub start: u32,
     /// Block number when reservation will expire.
     pub finish: u32,
@@ -269,17 +311,35 @@ pub struct GasReservationSlot {
 mod tests {
     use super::*;
 
+    const MAX_RESERVATIONS: u64 = 256;
+
     fn new_reserver() -> GasReserver {
-        GasReserver::new(Default::default(), 0, Default::default(), 256)
+        GasReserver::new(Default::default(), 0, Default::default(), MAX_RESERVATIONS)
     }
 
     #[test]
-    #[should_panic = "LimitReached"]
     fn max_reservations_limit_works() {
         let mut reserver = new_reserver();
-        for _ in 0..usize::MAX {
-            reserver.reserve(100, 10).unwrap();
+        for n in 0..(MAX_RESERVATIONS * 10) {
+            let res = reserver.reserve(100, 10);
+            if n > MAX_RESERVATIONS {
+                assert_eq!(res, Err(ReservationError::ReservationsLimitReached));
+            } else {
+                assert!(res.is_ok());
+            }
         }
+    }
+
+    #[test]
+    fn mark_used_for_unreserved_fails() {
+        let mut reserver = new_reserver();
+        let id = reserver.reserve(1, 1).unwrap();
+        reserver.unreserve(id).unwrap();
+
+        assert_eq!(
+            reserver.mark_used(id),
+            Err(ReservationError::InvalidReservationId)
+        );
     }
 
     #[test]
