4f86b18a713b47af77d13bd5fbed85392fcb143f
Dmitry Novikov <novikov.dm.al@gmail.com>
2023-06-17 14:15:37 +0400

    Refactor `core-errors/simple`

--- core-errors/src/simple.rs
+++ core-errors/src/simple.rs
@@ -1,195 +1,214 @@
-// Copyright (C) 2023 Gear Technologies Inc.
-// SPDX-License-Identifier: GPL-3.0-or-later WITH Classpath-exception-2.0
-//
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program. If not, see <https://www.gnu.org/licenses/>.
+// // Copyright (C) 2023 Gear Technologies Inc.
+// // SPDX-License-Identifier: GPL-3.0-or-later WITH Classpath-exception-2.0
+// //
+// // This program is free software: you can redistribute it and/or modify
+// // it under the terms of the GNU General Public License as published by
+// // the Free Software Foundation, either version 3 of the License, or
+// // (at your option) any later version.
+// //
+// // This program is distributed in the hope that it will be useful,
+// // but WITHOUT ANY WARRANTY; without even the implied warranty of
+// // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// // GNU General Public License for more details.
+// //
+// // You should have received a copy of the GNU General Public License
+// // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-//! Simple errors being used for status codes
+// //! Simple errors being used for status codes
 
-use enum_iterator::Sequence;
+#![allow(missing_docs)]
+#![allow(clippy::unnecessary_cast)]
 
-/// Type that can be encoded and decoded into status code
-pub trait SimpleCodec: sealed::Sealed + Sized {
-    /// Convert type into status code
-    fn into_status_code(self) -> i32;
+#[cfg(feature = "codec")]
+use scale_info::{
+    scale::{self, Decode, Encode},
+    TypeInfo,
+};
 
-    /// Convert status code into self
-    fn from_status_code(status_code: i32) -> Option<Self>;
-}
-
-mod sealed {
-    pub trait Sealed {}
-}
-
-/// Simple execution error
-#[derive(
-    Copy, Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd, Sequence, derive_more::Display,
-)]
-#[non_exhaustive]
 #[repr(u8)]
-pub enum SimpleExecutionError {
-    /// Gas limit exceeded
-    #[display(fmt = "Gas limit exceeded")]
-    GasLimitExceeded = 0,
-    /// Memory exceeded
-    #[display(fmt = "Memory exceeded")]
-    MemoryExceeded = 1,
-    /// Ext error
-    #[display(fmt = "Business-logic error")]
-    Ext = 2,
-    /// Panic occurred
-    Panic = 3,
-    /// `unreachable` occurred
-    Unknown = 255,
+#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord)]
+#[cfg_attr(feature = "codec", derive(Encode, Decode, TypeInfo), codec(crate = scale))]
+pub enum ReplyCode {
+    Success(SuccessReason) = 0,
+    Error(ErrorReason) = 1,
+    //
+    Unsupported = 255,
 }
 
-impl SimpleExecutionError {
-    fn decode(num: u8) -> Option<Self> {
-        match num {
-            0 => Some(Self::GasLimitExceeded),
-            1 => Some(Self::MemoryExceeded),
-            2 => Some(Self::Ext),
-            3 => Some(Self::Panic),
-            255 => Some(Self::Unknown),
-            _ => None,
-        }
+impl From<SuccessReason> for ReplyCode {
+    fn from(reason: SuccessReason) -> Self {
+        Self::Success(reason)
     }
 }
 
-/// Reply error
-#[derive(
-    Copy, Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd, Sequence, derive_more::Display,
-)]
-#[non_exhaustive]
+impl From<ErrorReason> for ReplyCode {
+    fn from(reason: ErrorReason) -> Self {
+        Self::Error(reason)
+    }
+}
+
+impl ReplyCode {
+    pub fn to_bytes(self) -> [u8; 4] {
+        // SAFETY: Because `Self` is marked `repr(u8)`, its layout is a `repr(C)` `union`
+        // between `repr(C)` structs, each of which has the `u8` discriminant as its first
+        // field, so we can read the discriminant without offsetting the pointer.
+        let first = unsafe { *<*const _>::from(&self).cast::<u8>() };
+
+        let mut bytes = [first, 0, 0, 0];
+
+        match self {
+            Self::Success(reason) => bytes[1..].copy_from_slice(&reason.to_bytes()),
+            Self::Error(reason) => bytes[1..].copy_from_slice(&reason.to_bytes()),
+            Self::Unsupported => {}
+        }
+
+        bytes
+    }
+
+    pub fn from_bytes(bytes: [u8; 4]) -> Self {
+        match bytes[0] {
+            0 => {
+                let reason_bytes = bytes[1..].try_into().unwrap_or_else(|_| unreachable!());
+                Self::Success(SuccessReason::from_bytes(reason_bytes))
+            }
+            1 => {
+                let reason_bytes = bytes[1..].try_into().unwrap_or_else(|_| unreachable!());
+                Self::Error(ErrorReason::from_bytes(reason_bytes))
+            }
+            _ => Self::Unsupported,
+        }
+    }
+
+    pub fn error(reason: impl Into<ErrorReason>) -> Self {
+        Self::Error(reason.into())
+    }
+
+    pub fn is_success(&self) -> bool {
+        matches!(self, Self::Success(_))
+    }
+
+    pub fn is_error(&self) -> bool {
+        matches!(self, Self::Error(_))
+    }
+
+    pub fn is_unsupported(&self) -> bool {
+        matches!(self, Self::Unsupported)
+    }
+}
+
 #[repr(u8)]
-pub enum SimpleReplyError {
-    /// Execution error.
-    #[display(fmt = "Execution error: {_0}")]
-    Execution(SimpleExecutionError) = 1,
-    /// Not executable actor.
-    #[display(fmt = "Not executable actor")]
-    NonExecutable = 2,
-    /// Message killed from storage as out of rent.
-    #[display(fmt = "Out of rent")]
-    OutOfRent = 3,
-    /// `gr_create_program` called with in-existing code ID.
-    #[display(fmt = "Program code does not exist")]
-    CodeNotExists = 4,
+#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord)]
+#[cfg_attr(feature = "codec", derive(Encode, Decode, TypeInfo), codec(crate = scale))]
+pub enum SuccessReason {
+    Auto = 0,
+    Manual = 1,
+    //
+    Unsupported = 255,
 }
 
-impl SimpleCodec for SimpleReplyError {
-    fn into_status_code(self) -> i32 {
-        let first = match self {
-            SimpleReplyError::Execution(_) => 1,
-            SimpleReplyError::NonExecutable => 2,
-            SimpleReplyError::OutOfRent => 3,
-            SimpleReplyError::CodeNotExists => 4,
-        };
-        let mut second = 0;
-        if let Self::Execution(err) = self {
-            second = err as u32;
-        }
-
-        (first | second << 8) as i32
+impl SuccessReason {
+    fn to_bytes(self) -> [u8; 3] {
+        [self as u8, 0, 0]
     }
 
-    fn from_status_code(status_code: i32) -> Option<Self> {
-        let status_code = status_code as u32;
-        let first = (status_code & 0xff) as u8;
-        let second = ((status_code & 0xff00) >> 8) as u8;
-
-        let execution = SimpleExecutionError::decode(second)?;
-
-        match first {
-            1 => Some(SimpleReplyError::Execution(execution)),
-            2 => Some(SimpleReplyError::NonExecutable),
-            3 => Some(SimpleReplyError::OutOfRent),
-            4 => Some(SimpleReplyError::CodeNotExists),
-            _ => None,
+    fn from_bytes(bytes: [u8; 3]) -> Self {
+        match bytes[0] {
+            0 => Self::Auto,
+            1 => Self::Manual,
+            _ => Self::Unsupported,
         }
     }
 }
 
-impl sealed::Sealed for SimpleReplyError {}
-
-/// Signal error
-#[derive(
-    Copy, Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd, Sequence, derive_more::Display,
-)]
-#[non_exhaustive]
 #[repr(u8)]
-pub enum SimpleSignalError {
-    /// Execution error
-    #[display(fmt = "Execution error: {_0}")]
-    Execution(SimpleExecutionError) = 1,
-    /// Message has been removed from the waitlist
-    #[display(fmt = "Message has been removed from the waitlist")]
-    RemovedFromWaitlist = 2,
+#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord)]
+#[cfg_attr(feature = "codec", derive(Encode, Decode, TypeInfo), codec(crate = scale))]
+pub enum ErrorReason {
+    Execution(ExecutionError) = 0,
+    InactiveProgram = 1,
+    FailedToCreateProgram = 2,
+    //
+    Unsupported = 255,
 }
 
-impl SimpleCodec for SimpleSignalError {
-    fn into_status_code(self) -> i32 {
-        let first = match self {
-            SimpleSignalError::Execution(_) => 1,
-            SimpleSignalError::RemovedFromWaitlist => 2,
-        };
-        let mut second = 0;
-        if let Self::Execution(err) = self {
-            second = err as u32;
-        }
-
-        (first | second << 8) as i32
-    }
-
-    fn from_status_code(status_code: i32) -> Option<Self> {
-        let status_code = status_code as u32;
-        let first = (status_code & 0xff) as u8;
-        let second = ((status_code & 0xff00) >> 8) as u8;
-
-        let execution = SimpleExecutionError::decode(second)?;
-
-        match first {
-            1 => Some(SimpleSignalError::Execution(execution)),
-            2 => Some(SimpleSignalError::RemovedFromWaitlist),
-            _ => None,
-        }
+impl From<ExecutionError> for ErrorReason {
+    fn from(error: ExecutionError) -> Self {
+        Self::Execution(error)
     }
 }
 
-impl sealed::Sealed for SimpleSignalError {}
+impl ErrorReason {
+    fn to_bytes(self) -> [u8; 3] {
+        // SAFETY: Because `Self` is marked `repr(u8)`, its layout is a `repr(C)` `union`
+        // between `repr(C)` structs, each of which has the `u8` discriminant as its first
+        // field, so we can read the discriminant without offsetting the pointer.
+        let first = unsafe { *<*const _>::from(&self).cast::<u8>() };
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+        let mut bytes = [first, 0, 0];
 
-    #[test]
-    fn encode_decode() {
-        for variant in enum_iterator::all::<SimpleSignalError>() {
-            let status_code = variant.into_status_code();
-            assert_ne!(variant.into_status_code(), 0);
-            assert_eq!(
-                SimpleSignalError::from_status_code(status_code),
-                Some(variant)
-            );
+        match self {
+            Self::Execution(error) => {
+                bytes[1..].copy_from_slice(&error.to_bytes());
+            }
+            Self::InactiveProgram | Self::FailedToCreateProgram | Self::Unsupported => {}
         }
 
-        for variant in enum_iterator::all::<SimpleReplyError>() {
-            let status_code = variant.into_status_code();
-            assert_ne!(variant.into_status_code(), 0);
-            assert_eq!(
-                SimpleReplyError::from_status_code(status_code),
-                Some(variant)
-            );
+        bytes
+    }
+
+    fn from_bytes(bytes: [u8; 3]) -> Self {
+        match bytes[0] {
+            0 => {
+                let err_bytes = bytes[1..].try_into().unwrap_or_else(|_| unreachable!());
+                Self::Execution(ExecutionError::from_bytes(err_bytes))
+            }
+            1 => Self::InactiveProgram,
+            2 => Self::FailedToCreateProgram,
+            _ => Self::Unsupported,
         }
+    }
+}
+
+#[repr(u8)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord)]
+#[cfg_attr(feature = "codec", derive(Encode, Decode, TypeInfo), codec(crate = scale))]
+pub enum ExecutionError {
+    RanOutOfGas = 0,
+    MemoryOverflow = 1,
+    BackendError = 2,
+    UserspacePanic = 3,
+    UnreachableInstruction = 4,
+    //
+    Unsupported = 255,
+}
+
+impl ExecutionError {
+    fn to_bytes(self) -> [u8; 2] {
+        [self as u8, 0]
+    }
+
+    fn from_bytes(bytes: [u8; 2]) -> Self {
+        match bytes[0] {
+            0 => Self::RanOutOfGas,
+            1 => Self::MemoryOverflow,
+            2 => Self::BackendError,
+            3 => Self::UserspacePanic,
+            4 => Self::UnreachableInstruction,
+            _ => Self::Unsupported,
+        }
+    }
+}
+
+#[repr(u8)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord)]
+#[cfg_attr(feature = "codec", derive(Encode, Decode, TypeInfo), codec(crate = scale))]
+pub enum SignalCode {
+    Execution(ExecutionError),
+    RemovedFromWaitlist,
+}
+
+impl From<ExecutionError> for SignalCode {
+    fn from(error: ExecutionError) -> Self {
+        Self::Execution(error)
     }
 }
