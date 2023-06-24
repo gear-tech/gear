24e6e21bd49218e6630bfc9d790cd7506029bad6
StackOverflowExcept1on <109800286+StackOverflowExcept1on@users.noreply.github.com>
2023-06-24 08:30:45 +0300

    feat(gsdk): add clear instructions how to update it & add test

---
+++ scripts/update-gsdk-metadata.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# This script is used to update the generated code in the gsdk crate
+# Also see `gsdk/HOW-TO-UPDATE.md` for more details
+
+set -ex
+
+cargo build --package vara-runtime --features debug-mode --release
+cargo build --package gsdk-api-gen --release
+GSDK_API_GEN=1 cargo build --package gsdk --release
+cargo fmt
