7625bbd699e7b6ff3f3cdc516c4aee6b11909901
Dennis Diatlov <dennis@gear-tech.io>
2023-07-06 10:09:00 +0100

    fix(gstd): Fix deadlocks in mutex/rw-lock (#2906)

--- examples/binaries/waiter/src/code.rs
+++ examples/binaries/waiter/src/code.rs
@@ -1,15 +1,12 @@
-use crate::{Command, SleepForWaitType, WaitSubcommand};
+use crate::{
+    Command, MxLockContinuation, RwLockContinuation, RwLockType, SleepForWaitType, WaitSubcommand,
+};
 use futures::future;
 
-use gstd::{errors::Error, exec, format, msg, MessageId};
+use gstd::{errors::Error, exec, format, lock, msg, MessageId};
 
-fn process_wait_subcommand(subcommand: WaitSubcommand) {
-    match subcommand {
-        WaitSubcommand::Wait => exec::wait(),
-        WaitSubcommand::WaitFor(duration) => exec::wait_for(duration),
-        WaitSubcommand::WaitUpTo(duration) => exec::wait_up_to(duration),
-    }
-}
+static mut MUTEX: lock::Mutex<()> = lock::Mutex::new(());
+static mut RW_LOCK: lock::RwLock<()> = lock::RwLock::new(());
 
 #[gstd::async_main]
 async fn main() {
@@ -79,5 +76,43 @@ async fn main() {
         Command::WakeUp(msg_id) => {
             exec::wake(msg_id.into()).expect("Failed to wake up the message");
         }
+        Command::MxLock(continuation) => {
+            let _lock_guard = unsafe { MUTEX.lock().await };
+            process_mx_lock_continuation(continuation).await;
+        }
+        Command::RwLock(lock_type, continuation) => {
+            match lock_type {
+                RwLockType::Read => {
+                    let _lock_guard = unsafe { RW_LOCK.read().await };
+                    process_rw_lock_continuation(continuation).await;
+                }
+                RwLockType::Write => {
+                    let _lock_guard = unsafe { RW_LOCK.write().await };
+                    process_rw_lock_continuation(continuation).await;
+                }
+            };
+        }
+    }
+}
+
+fn process_wait_subcommand(subcommand: WaitSubcommand) {
+    match subcommand {
+        WaitSubcommand::Wait => exec::wait(),
+        WaitSubcommand::WaitFor(duration) => exec::wait_for(duration),
+        WaitSubcommand::WaitUpTo(duration) => exec::wait_up_to(duration),
+    }
+}
+
+async fn process_mx_lock_continuation(continuation: MxLockContinuation) {
+    match continuation {
+        MxLockContinuation::Nothing => {}
+        MxLockContinuation::SleepFor(duration) => exec::sleep_for(duration).await,
+    }
+}
+
+async fn process_rw_lock_continuation(continuation: RwLockContinuation) {
+    match continuation {
+        RwLockContinuation::Nothing => {}
+        RwLockContinuation::SleepFor(duration) => exec::sleep_for(duration).await,
     }
 }
