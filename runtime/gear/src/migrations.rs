bd80dae6fe0f36069fe37d06c1588660ab36912c
Dmitry Novikov <novikov.dm.al@gmail.com>
2023-07-10 13:37:48 +0200

    chore(runtime): Remove executed migrations (#2918)

--- runtime/gear/src/migrations.rs
+++ runtime/gear/src/migrations.rs
@@ -1,6 +1,3 @@
 use crate::*;
 
-pub type Migrations = (
-    pallet_gear_gas::migrations::v2::MigrateToV2<Runtime>,
-    pallet_gear_messenger::migrations::MigrateToV2<Runtime>,
-);
+pub type Migrations = ();
