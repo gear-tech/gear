54e1c3cca691755a6591b9bd5c13bf73ae30ab45
Dmitry Novikov <novikov.dm.al@gmail.com>
2023-07-11 19:33:42 +0200

    fix(rpc-fns): Read state gas limit restrictions (#2926)

--- core-processor/src/executor.rs
+++ core-processor/src/executor.rs
@@ -601,6 +601,8 @@ where
         .into_ext_info(&memory)
         .map_err(|e| format!("Backend postprocessing error: {e:?}"))?;
 
+    log::debug!("\n\n\tBURNED: {}\n\n", info.gas_amount.burned());
+
     for (dispatch, _, _) in info.generated_dispatches {
         if matches!(dispatch.kind(), DispatchKind::Reply) {
             return Ok(dispatch.payload_bytes().to_vec());
