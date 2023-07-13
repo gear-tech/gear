9d70c91863d26864f2b87b3078ae38c0155e0866
Elijah <83283675+tltsutltsu@users.noreply.github.com>
2023-07-13 22:44:40 +0600

    fix(core-backend): continue to adopt `LimitedStr` (#2758)
    Co-authored-by: StackOverflowExcept1on <109800286+StackOverflowExcept1on@users.noreply.github.com>
    Co-authored-by: Dmitry Novikov <novikov.dm.al@gmail.com>

--- pallets/gear/src/tests.rs
+++ pallets/gear/src/tests.rs
@@ -62,7 +62,8 @@ use frame_support::{
 };
 use frame_system::pallet_prelude::BlockNumberFor;
 use gear_backend_common::{
-    TrapExplanation, UnrecoverableExecutionError, UnrecoverableExtError, UnrecoverableWaitError,
+    LimitedStr, TrapExplanation, UnrecoverableExecutionError, UnrecoverableExtError,
+    UnrecoverableWaitError,
 };
 use gear_core::{
     code::{self, Code},
@@ -13191,9 +13192,10 @@ fn module_instantiation_error() {
 
         assert!(Gear::is_terminated(prog_id));
         let err = get_last_event_error(mid);
-        assert!(
-            err.starts_with(&ActorExecutionErrorReplyReason::Environment("".into()).to_string())
-        );
+        assert!(err.starts_with(
+            &ActorExecutionErrorReplyReason::Environment(LimitedStr::from_small_str(""))
+                .to_string()
+        ));
     });
 }
 
@@ -13225,9 +13227,10 @@ fn wrong_entry_type() {
 
         assert!(Gear::is_terminated(pid));
         let err = get_last_event_error(mid);
-        assert!(
-            err.starts_with(&ActorExecutionErrorReplyReason::Environment("".into()).to_string())
-        );
+        assert!(err.starts_with(
+            &ActorExecutionErrorReplyReason::Environment(LimitedStr::from_small_str(""))
+                .to_string()
+        ));
     });
 }
 
