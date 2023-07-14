a06d09f5e44c42ea1142540cfff63deb43b1d3d9
Dmitry Novikov <novikov.dm.al@gmail.com>
2023-07-14 15:27:18 +0200

    Throw correct feature for builder

--- scripts/src/check.sh
+++ scripts/src/check.sh
@@ -20,7 +20,7 @@ EOF
 
 gear_check() {
   echo "  >> Check workspace without crates that use runtime with 'fuzz' feature"
-  SKIP_WASM_BUILD=1 SKIP_GEAR_RUNTIME_WASM_BUILD=1 SKIP_VARA_RUNTIME_WASM_BUILD=1 cargo check --workspace "$@" --exclude runtime-fuzzer --exclude runtime-fuzzer-fuzz
+  __GEAR_WASM_BUILDER_NO_BUILD=1 SKIP_WASM_BUILD=1 SKIP_GEAR_RUNTIME_WASM_BUILD=1 SKIP_VARA_RUNTIME_WASM_BUILD=1 cargo check --workspace "$@" --exclude runtime-fuzzer --exclude runtime-fuzzer-fuzz
 
   echo "  >> Check crates that use runtime with 'fuzz' feature"
   cargo check "$@" -p runtime-fuzzer -p runtime-fuzzer-fuzz
