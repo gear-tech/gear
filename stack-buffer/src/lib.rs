dff86250594c294b8691bb446e5a2ea4f0de454c
StackOverflowExcept1on <109800286+StackOverflowExcept1on@users.noreply.github.com>
2023-08-07 13:15:00 +0300

    add more comments to unsafe code

--- stack-buffer/src/lib.rs
+++ stack-buffer/src/lib.rs
@@ -30,8 +30,12 @@ use core::{
     slice,
 };
 
+/// The maximum buffer size that can be allocated on the stack.
+/// This is currently limited to 64 KiB.
 pub const MAX_BUFFER_SIZE: usize = 64 * 1024;
 
+/// A closure data type that is used in the native library to pass
+/// a pointer to allocated stack memory.
 type Callback = unsafe extern "C-unwind" fn(ptr: *mut MaybeUninit<u8>, data: *mut c_void);
 
 #[cfg(any(
@@ -39,11 +43,17 @@ type Callback = unsafe extern "C-unwind" fn(ptr: *mut MaybeUninit<u8>, data: *mu
     all(not(feature = "compile-alloca"), target_arch = "wasm32")
 ))]
 extern "C-unwind" {
+    /// Function from the native library that manipulates the stack pointer directly.
+    /// Can be used to dynamically allocate stack space.
     fn c_with_alloca(size: usize, callback: Callback, data: *mut c_void);
 }
 
+/// This is a polyfill function that is used when the native library is unavailable.
+/// The maximum size that can be allocated on the stack is limited
+/// by the [`MAX_BUFFER_SIZE`] constant.
 #[cfg(all(not(feature = "compile-alloca"), not(target_arch = "wasm32")))]
 unsafe extern "C-unwind" fn c_with_alloca(_size: usize, callback: Callback, data: *mut c_void) {
+    // Same as `MaybeUninit::uninit_array()`.
     // Create an uninitialized array of `MaybeUninit`. The `assume_init` is
     // safe because the type we are claiming to have initialized here is a
     // bunch of `MaybeUninit`s, which do not require initialization.
@@ -51,19 +61,26 @@ unsafe extern "C-unwind" fn c_with_alloca(_size: usize, callback: Callback, data
     callback(buffer.as_mut_ptr(), data);
 }
 
+/// Helper function to create a trampoline between C and Rust code.
 #[inline(always)]
 fn get_trampoline<F: FnOnce(*mut MaybeUninit<u8>)>(_closure: &F) -> Callback {
     trampoline::<F>
 }
 
+/// A function that serves as a trampoline between C and Rust code.
+/// It is mainly used to switch from `fn()` to `FnOnce()`,
+/// which allows local variables to be captured.
 unsafe extern "C-unwind" fn trampoline<F: FnOnce(*mut MaybeUninit<u8>)>(
     ptr: *mut MaybeUninit<u8>,
     data: *mut c_void,
 ) {
+    // This code gets `*mut ManuallyDrop<F>`, then takes ownership of the `F` function
+    // and executes it with a pointer to the allocated stack memory.
     let f = ManuallyDrop::take(&mut *(data as *mut ManuallyDrop<F>));
     f(ptr);
 }
 
+/// This is a higher-level function for dynamically allocating space on the stack.
 fn with_alloca<T>(size: usize, f: impl FnOnce(&mut [MaybeUninit<u8>]) -> T) -> T {
     let mut ret = MaybeUninit::uninit();
 
@@ -72,6 +89,7 @@ fn with_alloca<T>(size: usize, f: impl FnOnce(&mut [MaybeUninit<u8>]) -> T) -> T
         ret.write(f(slice));
     };
 
+    // The `closure` variable is passed as `*mut ManuallyDrop<F>` to the trampoline function.
     let trampoline = get_trampoline(&closure);
     let mut closure_data = ManuallyDrop::new(closure);
 
