ac006cd76f6fbbcc292a4575af6190b88e12ac9b
StackOverflowExcept1on <109800286+StackOverflowExcept1on@users.noreply.github.com>
2023-08-10 18:16:17 +0300

    fill todos with documentation

--- gstd/src/prog/encoded.rs
+++ gstd/src/prog/encoded.rs
@@ -26,7 +26,8 @@ use crate::{
 use gstd_codegen::wait_create_program_for_reply;
 use scale_info::scale::{Decode, Encode};
 
-/// TODO.
+/// Same as [`create_program_bytes`](super::create_program_bytes), but allows
+/// initialize program with the encodable payload.
 #[wait_create_program_for_reply]
 pub fn create_program<E: Encode>(
     code_id: CodeId,
@@ -37,7 +38,8 @@ pub fn create_program<E: Encode>(
     super::create_program_bytes(code_id, salt, payload.encode(), value)
 }
 
-/// TODO.
+/// Same as [`create_program`], but creates a new program after the `delay`
+/// expressed in block count.
 pub fn create_program_delayed<E: Encode>(
     code_id: CodeId,
     salt: impl AsRef<[u8]>,
@@ -48,7 +50,7 @@ pub fn create_program_delayed<E: Encode>(
     super::create_program_bytes_delayed(code_id, salt, payload.encode(), value, delay)
 }
 
-/// TODO.
+/// Same as [`create_program`], but with an explicit gas limit.
 #[wait_create_program_for_reply]
 pub fn create_program_with_gas<E: Encode>(
     code_id: CodeId,
@@ -60,7 +62,8 @@ pub fn create_program_with_gas<E: Encode>(
     super::create_program_bytes_with_gas(code_id, salt, payload.encode(), gas_limit, value)
 }
 
-/// TODO.
+/// Same as [`create_program_with_gas`], but creates a new program after the
+/// `delay` expressed in block count.
 pub fn create_program_with_gas_delayed<E: Encode>(
     code_id: CodeId,
     salt: impl AsRef<[u8]>,
