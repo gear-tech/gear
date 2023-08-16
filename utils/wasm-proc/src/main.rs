1e8932d89bee44a9cde614975e4770c7ac99915b
Vadim Smirnov <ukint-vs@proton.me>
2023-08-16 19:39:26 +0400

    Update wasm-proc imports

--- utils/wasm-proc/src/main.rs
+++ utils/wasm-proc/src/main.rs
@@ -37,6 +37,7 @@ const RT_ALLOWED_IMPORTS: [&str; 64] = [
     "ext_crypto_start_batch_verify_version_1",
     // From `GearRI` (runtime-interface/scr/lib.rs)
     "ext_gear_ri_pre_process_memory_accesses_version_1",
+    "ext_gear_ri_pre_process_memory_accesses_version_2",
     "ext_gear_ri_lazy_pages_status_version_1",
     "ext_gear_ri_write_accessed_pages_version_1",
     "ext_gear_ri_init_lazy_pages_version_1",
