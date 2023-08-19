2638a27b2e25e74a0e5684665326a43a37a299aa
Sabaun Taraki <taraki.sabaun.rl@gmail.com>
2023-08-19 16:40:02 +0300

    feat(runtime-fuzzer): Introduce "precise" `runtime-fuzzer` (#3102)

--- utils/runtime-fuzzer/fuzz/fuzz_targets/main.rs
+++ utils/runtime-fuzzer/fuzz/fuzz_targets/main.rs
@@ -19,53 +19,13 @@
 #![no_main]
 
 use libfuzzer_sys::fuzz_target;
-use once_cell::sync::OnceCell;
-use std::{
-    fs::{self, OpenOptions},
-    io::{Error as IoError, Result as IoResult, Write},
-    path::Path,
-};
+use runtime_fuzzer::GearCalls;
 
-const SEEDS_STORE_DIR: &str = "/tmp/fuzzing-seeds-dir";
-const SEEDS_STORE_FILE: &str = "fuzzing-seeds";
-
-static RUN_INTIALIZED: OnceCell<String> = OnceCell::new();
-
-fuzz_target!(|seed: u64| {
+fuzz_target!(|gear_calls: GearCalls| {
     gear_utils::init_default_logger();
 
-    dump_seed(seed).unwrap_or_else(|e| unreachable!("internal error: failed dumping seed: {e}"));
-
-    log::info!("Running the seed {seed}");
-    runtime_fuzzer::run(seed);
+    // Newline to easily browse logs.
+    println!();
+    log::info!("Executing generated gear calls");
+    runtime_fuzzer::run(gear_calls);
 });
-
-// Dumps seed to the `SEEDS_STORE_FILE` file inside `SEEDS_STORE_DIR`
-// directory before running fuzz test.
-//
-// If directory already exists for the current run, it will be cleared.
-fn dump_seed(seed: u64) -> IoResult<()> {
-    let seeds_file = RUN_INTIALIZED.get_or_try_init(|| {
-        let seeds_dir = Path::new(SEEDS_STORE_DIR);
-        match seeds_dir.exists() {
-            true => clear_dir(seeds_dir)?,
-            false => fs::create_dir_all(seeds_dir)?,
-        }
-
-        Ok::<String, IoError>(format!("{SEEDS_STORE_DIR}/{SEEDS_STORE_FILE}"))
-    })?;
-
-    OpenOptions::new()
-        .create(true)
-        .append(true)
-        .open(seeds_file)
-        .and_then(|mut file| writeln!(file, "{seed}"))
-}
-
-fn clear_dir(path: &Path) -> IoResult<()> {
-    for dir_entry in fs::read_dir(path)? {
-        fs::remove_file(dir_entry?.path())?;
-    }
-
-    Ok(())
-}
