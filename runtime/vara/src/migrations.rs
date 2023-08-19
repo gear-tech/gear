948341790152413e59bc5a29f0e1663608db58b4
Dmitry Novikov <novikov.dm.al@gmail.com>
2023-08-19 17:27:45 +0400

    Remove migrations to gear-bank from vara runtime

--- runtime/vara/src/migrations.rs
+++ runtime/vara/src/migrations.rs
@@ -5,5 +5,4 @@ pub type Migrations = (
     pallet_gear_scheduler::migration::MigrateToV2<Runtime>,
     pallet_gear_gas::migrations::v2::MigrateToV2<Runtime>,
     pallet_gear_messenger::migrations::MigrateToV2<Runtime>,
-    runtime_common::migrations::MigrateToGearBank<Runtime, GasConverter>,
 );
