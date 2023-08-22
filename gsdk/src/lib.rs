b3beacd1cb86d4ca28907c444e867577a05dec0d
clearloop <26088946+clearloop@users.noreply.github.com>
2023-08-22 17:32:28 +0400

    feat(gsdk): use GasInfo from gear_core

--- gsdk/src/lib.rs
+++ gsdk/src/lib.rs
@@ -31,9 +31,9 @@ use crate::{
         ActiveProgram,
     },
 };
+pub use gear_core::gas::GasInfo;
 use gear_core::ids::{MessageId, ReservationId};
-use parity_scale_codec::{Decode, Encode};
-use serde::{Deserialize, Serialize};
+use parity_scale_codec::Decode;
 use sp_runtime::AccountId32;
 use std::collections::HashMap;
 pub use subxt::dynamic::Value;
@@ -70,17 +70,6 @@ pub mod gp {
 
 /// Block number type
 pub type BlockNumber = u32;
-
-/// Information of gas
-#[derive(Clone, Debug, Decode, Encode, PartialEq, Eq, Serialize, Deserialize)]
-pub struct GasInfo {
-    /// Represents minimum gas limit required for execution.
-    pub min_limit: u64,
-    /// Gas amount that we reserve for some other on-chain interactions.
-    pub reserved: u64,
-    /// Contains number of gas burned during message processing.
-    pub burned: u64,
-}
 
 /// Gear gas node id.
 pub type GearGasNodeId = GasNodeId<MessageId, ReservationId>;
