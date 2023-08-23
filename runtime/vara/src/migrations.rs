b16f7c6cf67d5eeb7424c653dffe736f67747d69
Vadim Smirnov <ukint-vs@proton.me>
2023-08-23 11:34:08 +0400

    chore(vara): Remove old migrations

--- runtime/vara/src/migrations.rs
+++ runtime/vara/src/migrations.rs
@@ -1,8 +1,3 @@
 use crate::*;
 
-pub type Migrations = (
-    pallet_gear_gas::migrations::v1::MigrateToV1<Runtime>,
-    pallet_gear_scheduler::migration::MigrateToV2<Runtime>,
-    pallet_gear_gas::migrations::v2::MigrateToV2<Runtime>,
-    pallet_gear_messenger::migrations::MigrateToV2<Runtime>,
-);
+pub type Migrations = ();
