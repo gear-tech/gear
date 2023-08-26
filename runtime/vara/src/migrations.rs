a1cea795253b6acb2df99ce4cdd2aa3de490ce3a
Vadim Smirnov <ukint-vs@proton.me>
2023-08-26 20:46:22 +0400

    Nomination pools migration v5: rewardpool fix

--- runtime/vara/src/migrations.rs
+++ runtime/vara/src/migrations.rs
@@ -5,4 +5,6 @@ pub type Migrations = (
     pallet_gear_scheduler::migration::MigrateToV2<Runtime>,
     pallet_gear_gas::migrations::v2::MigrateToV2<Runtime>,
     pallet_gear_messenger::migrations::MigrateToV2<Runtime>,
+    // unreleased
+    pallet_nomination_pools::migration::v5::MigrateToV5<Runtime>,
 );
