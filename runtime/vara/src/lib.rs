ea46e5495ff77743a1f66d219cd2471e3be2d8ad
Vadim Smirnov <ukint-vs@proton.me>
2023-09-06 13:40:25 +0200

    fix(vara): Bump MaxAuthorities to 100_000

--- runtime/vara/src/lib.rs
+++ runtime/vara/src/lib.rs
@@ -755,7 +755,7 @@ parameter_types! {
     pub const ImOnlineUnsignedPriority: TransactionPriority = TransactionPriority::max_value();
     /// We prioritize im-online heartbeats over election solution submission.
     pub const StakingUnsignedPriority: TransactionPriority = TransactionPriority::max_value() / 2;
-    pub const MaxAuthorities: u32 = 32;
+    pub const MaxAuthorities: u32 = 100_000;
     pub const MaxKeys: u32 = 10_000;
     pub const MaxPeerInHeartbeats: u32 = 10_000;
     pub const MaxPeerDataEncodingSize: u32 = 1_000;
