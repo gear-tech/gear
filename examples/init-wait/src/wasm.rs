f15ba78005a5f81be34648298c1cccff9bcc92a2
Shamil <66209982+shamilsan@users.noreply.github.com>
2023-09-19 21:51:35 +0300

    refactor(examples): make demos be more consistent (#3138)

--- examples/init-wait/src/wasm.rs
+++ examples/init-wait/src/wasm.rs
@@ -16,7 +16,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-use gstd::{exec, msg, collections::BTreeMap, MessageId};
+use gstd::{collections::BTreeMap, exec, msg, MessageId};
 
 #[derive(PartialEq, Debug)]
 enum State {
