5ddc2921932a0dd7c7d56140c2d1f685b31eaee6
Maxim Savenkov <max_savenkov@mail.ru>
2023-09-22 17:42:47 +0200

    Add requested changes

--- utils/key-finder/src/main.rs
+++ utils/key-finder/src/main.rs
@@ -1,18 +1,5 @@
-use std::env;
-use std::path::{MAIN_SEPARATOR, Path};
+use directories::ProjectDirs;
 use hex::encode;
-
-#[cfg(all(unix, not(target_os = "macos")))]
-const APPDATA: &'static str = r"/.local/share/";
-
-#[cfg(target_os = "macos")]
-const APPDATA: &'static str = r"/Library/Application Support/";
-
-#[cfg(target_os = "windows")]
-const APPDATA: &'static str = r"\AppData\Local\";
-
-#[cfg(not(any(unix, windows, target_os = "macos")))]
-compile_error!("Unsupported OS");
 
 const VERSIONS: [&'static str; 7] = [
     "staging_testnet",
@@ -21,54 +8,47 @@ const VERSIONS: [&'static str; 7] = [
     "gear_staging_testnet_v4",
     "gear_staging_testnet_v5",
     "gear_staging_testnet_v6",
-    "gear_staging_testnet_v7"
+    "gear_staging_testnet_v7",
 ];
 
-const PATHS: [&'static str; 2] = [
-    "gear",
-    "gear-node"
-];
+const PATHS: [&'static str; 2] = ["gear", "gear-node"];
 
 fn main() {
-    #[cfg(any(unix, target_os = "macos"))]
-    let home = env::var("HOME").expect("HOME not set");
-    #[cfg(target_os = "windows")]
-    let home = env::var("USERPROFILE").expect("USERPROFILE not set");
-
-    let keys = PATHS.iter().flat_map(|p| {
-        let mut path = home.clone() + APPDATA + p;
-        if Path::new(&path).is_dir() {
-            path.push(MAIN_SEPARATOR);
-            path.push_str("chains");
-            path.push(MAIN_SEPARATOR);
-            VERSIONS.iter().filter_map(|v| {
-                let mut path = path.clone() + v;
-                if Path::new(&path).is_dir() {
-                    path.push(MAIN_SEPARATOR);
-                    path.push_str("network");
-                    path.push(MAIN_SEPARATOR);
-                    path.push_str("secret_ed25519");
-                    if Path::new(&path).is_file() {
-                        return Some((path, p, v));
-                    }
+    PATHS
+        .iter()
+        .flat_map(|p| {
+            if let Some(proj_dirs) = ProjectDirs::from("", "", p) {
+                let path = proj_dirs.data_local_dir();
+                if path.is_dir() {
+                    let path = path.join("chains");
+                    return VERSIONS
+                        .iter()
+                        .filter_map(|v| {
+                            let mut path = path.clone();
+                            path.push(v);
+                            if path.is_dir() {
+                                path.push("network");
+                                path.push("secret_ed25519");
+                                if path.is_file() {
+                                    return Some((path, p, v));
+                                }
+                            }
+                            None
+                        })
+                        .collect::<Vec<_>>();
                 }
-                None
-            }).collect()
-        }
-        else {
+            }
             vec![]
-        }
-    }).filter_map(|(path, p, v)| {
-        if let Ok(key) = std::fs::read(&path) {
-            Some((p.to_string() + "/" + v,  encode(key)))
-        }
-        else {
-            eprintln!("Failed to read key from {}", path);
-            None
-        }
-    }).collect::<Vec<_>>();
-
-    for key in keys {
-        println!("Key found in \"{}\": 0x{}", key.0, key.1);
-    }
+        })
+        .filter_map(|(path, p, v)| {
+            if let Ok(key) = std::fs::read(&path) {
+                Some((p.to_string() + "/" + v, encode(key)))
+            } else {
+                eprintln!("Failed to read key from {:?}", path);
+                None
+            }
+        })
+        .for_each(|(path, key)| {
+            println!("Key found in \"{}\": 0x{}", path, key);
+        });
 }
