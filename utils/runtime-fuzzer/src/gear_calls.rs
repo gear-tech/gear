84dd258062e6eec87ed40fed7aedb0116db591f0
StackOverflowExcept1on <109800286+StackOverflowExcept1on@users.noreply.github.com>
2023-10-21 16:40:54 +0300

    fix(runtime-fuzzer): update wasm-smith fork & disable all traps in wasm (#3404)

--- utils/runtime-fuzzer/src/gear_calls.rs
+++ utils/runtime-fuzzer/src/gear_calls.rs
@@ -442,7 +442,6 @@ fn config(
         existing_addresses,
         log_info,
         params_config,
-        unreachable_enabled: false,
         initial_pages: initial_pages as u32,
         ..Default::default()
     }
