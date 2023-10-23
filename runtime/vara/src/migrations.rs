aafadf0d29d51d3883b096572ab63971e9c53357
Vadim Smirnov <ukint-vs@proton.me>
2023-10-23 17:19:26 +0800

    migrations

--- runtime/vara/src/migrations.rs
+++ runtime/vara/src/migrations.rs
@@ -16,8 +16,24 @@
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-#[allow(unused)]
 use crate::*;
 use sp_runtime::traits::Get;
 
-pub type Migrations = ();
+pub struct NominationPoolsMigrationV4OldPallet;
+impl Get<Perbill> for NominationPoolsMigrationV4OldPallet {
+    fn get() -> Perbill {
+        Perbill::from_percent(10)
+    }
+}
+
+/// All migrations that will run on the next runtime upgrade.
+///
+/// Should be cleared after every release.
+pub type Migrations = (
+    // unreleased
+    pallet_nomination_pools::migration::v4::MigrateV3ToV5<
+        Runtime,
+        NominationPoolsMigrationV4OldPallet,
+    >,
+    pallet_offences::migration::v1::MigrateToV1<Runtime>,
+);
