e123edbfc119be77b0fe986bc64fd0230cf44f93
clearloop <26088946+clearloop@users.noreply.github.com>
2023-10-24 12:57:07 +0800

    feat(gcli): introduce function iterator for metadata executor

--- gcli/src/meta/executor.rs
+++ gcli/src/meta/executor.rs
@@ -21,9 +21,7 @@
 //! is because they are for the on-chain environment data.
 
 use anyhow::{anyhow, Result};
-use wasmi::{
-    AsContextMut, Caller, Engine, Extern, Func, Linker, Memory, MemoryType, Module, Store,
-};
+use wasmi::{AsContextMut, Engine, Extern, Linker, Memory, MemoryType, Module, Store};
 
 const PAGE_STORAGE_PREFIX: [u8; 32] = *b"gcligcligcligcligcligcligcligcli";
 
@@ -32,24 +30,6 @@ const PAGE_STORAGE_PREFIX: [u8; 32] = *b"gcligcligcligcligcligcligcligcli";
 pub struct HostState {
     /// Message buffer in host state.
     pub msg: Vec<u8>,
-}
-
-macro_rules! func {
-    ($store:tt) => {
-        func!($store,)
-    };
-    ($store:tt, $($ty:tt),* ) => {
-        Func::wrap(
-            &mut $store,
-            move |_caller: Caller<'_, HostState>, $(_: $ty),*| { Ok(()) },
-        )
-    };
-    (@result $store:tt, $($ty:tt),* ) => {
-        Func::wrap(
-            &mut $store,
-            move |_caller: Caller<'_, HostState>, $(_: $ty),*| { 0 },
-        )
-    };
 }
 
 /// Call `metadata` method in the WASM code.
@@ -67,115 +47,26 @@ fn execute(wasm: &[u8], method: &str) -> Result<Vec<u8>> {
     let module = Module::new(&engine, wasm).unwrap();
 
     let mut store = Store::new(&engine, HostState::default());
-    let mut linker = <Linker<HostState>>::new();
+    let mut linker = <Linker<HostState>>::new(&engine);
+    let memory = Memory::new(
+        store.as_context_mut(),
+        MemoryType::new(256, None).map_err(|_| anyhow!("failed to create memory type"))?,
+    )
+    .map_err(|_| anyhow!("failed to create memory"))?;
 
     // Execution environment
     //
     // TODO: refactor this after #3416.
     {
-        let memory = Memory::new(store.as_context_mut(), MemoryType::new(256, None)).unwrap();
-        linker.define("env", "memory", Extern::Memory(memory))?;
-        linker.define("env", "alloc", funcs::alloc(&mut store, memory))?;
-        linker.define("env", "gr_oom_panic", funcs::gr_oom_panic(&mut store))?;
-        linker.define("env", "gr_read", funcs::gr_read(&mut store, memory))?;
-        linker.define("env", "gr_reply", funcs::gr_reply(&mut store, memory))?;
-        linker.define("env", "gr_panic", funcs::gr_panic(&mut store, memory))?;
-        linker.define("env", "gr_size", funcs::gr_size(&mut store, memory))?;
-        // methods may be used by programs but not required by metadata.
-        linker.define("env", "free", func!(@result store, i32))?;
-        linker.define("env", "gr_block_height", func!(store, u32))?;
-        linker.define("env", "gr_block_timestamp", func!(store, u32))?;
-        linker.define(
-            "env",
-            "gr_create_program_wgas",
-            func!(store, i32, i32, u32, i32, u32, u64, u32, i32),
-        )?;
-        linker.define(
-            "env",
-            "gr_create_program",
-            func!(store, i32, i32, u32, i32, u32, u64, i32),
-        )?;
-        linker.define("env", "gr_debug", func!(store, i32, u32))?;
-        linker.define("env", "gr_exit", func!(store, i32))?;
-        linker.define("env", "gr_gas_available", func!(store, i32))?;
-        linker.define("env", "gr_leave", func!(store))?;
-        linker.define("env", "gr_message_id", func!(store, i32))?;
-        linker.define("env", "gr_out_of_gas", func!(store))?;
-        linker.define("env", "gr_pay_program_rent", func!(store, i32, i32))?;
-        linker.define("env", "gr_program_id", func!(store, i32))?;
-        linker.define("env", "gr_random", func!(store, i32, i32))?;
-        linker.define("env", "gr_reply_code", func!(store, i32))?;
-        linker.define("env", "gr_reply_commit", func!(store, i32, i32))?;
-        linker.define("env", "gr_reply_deposit", func!(store, i32, u64, i32))?;
-        linker.define("env", "gr_reply_input", func!(store, u32, u32, i32, i32))?;
-        linker.define("env", "gr_reply_push", func!(store, i32, u32, i32))?;
-        linker.define("env", "gr_reply_push_input", func!(store, u32, u32, i32))?;
-        linker.define(
-            "env",
-            "gr_reply_push_input_wgas",
-            func!(store, u32, u32, u64, i32, i32),
-        )?;
-        linker.define("env", "gr_reply_to", func!(store, i32))?;
-        linker.define(
-            "env",
-            "gr_reply_wgas",
-            func!(store, i32, u32, u64, i32, i32),
-        )?;
-        linker.define(
-            "env",
-            "gr_reservation_reply",
-            func!(store, i32, i32, u32, i32),
-        )?;
-        linker.define(
-            "env",
-            "gr_reservation_send_commit",
-            func!(store, u32, i32, u32, i32),
-        )?;
-        linker.define(
-            "env",
-            "gr_reservation_send",
-            func!(store, i32, i32, u32, u32, i32),
-        )?;
-        linker.define("env", "gr_reserve_gas", func!(store, u64, u32, i32))?;
-        linker.define("env", "gr_send", func!(store, i32, i32, u32, u32, i32))?;
-        linker.define("env", "gr_send_commit", func!(store, u32, i32, u32, i32))?;
-        linker.define(
-            "env",
-            "gr_send_commit_wgas",
-            func!(store, u32, i32, u64, u32, i32),
-        )?;
-        linker.define("env", "gr_send_init", func!(store, i32))?;
-        linker.define(
-            "env",
-            "gr_send_input",
-            func!(store, i32, u32, u32, u32, i32),
-        )?;
-        linker.define(
-            "env",
-            "gr_send_input_wgas",
-            func!(store, i32, u32, u32, u64, u32, i32),
-        )?;
-        linker.define("env", "gr_send_push", func!(store, u32, i32, u32, i32))?;
-        linker.define(
-            "env",
-            "gr_send_push_input",
-            func!(store, u32, u32, u32, i32),
-        )?;
-        linker.define(
-            "env",
-            "gr_send_wgas",
-            func!(store, i32, i32, u32, u64, u32, i32),
-        )?;
-        linker.define("env", "gr_signal_code", func!(store, i32))?;
-        linker.define("env", "gr_signal_from", func!(store, i32))?;
-        linker.define("env", "gr_source", func!(store, i32))?;
-        linker.define("env", "gr_system_reserve_gas", func!(store, u64, i32))?;
-        linker.define("env", "gr_unreserve_gas", func!(store, i32, i32))?;
-        linker.define("env", "gr_value", func!(store, i32))?;
-        linker.define("env", "gr_wait", func!(store, u32))?;
-        linker.define("env", "gr_wait_for", func!(store, u32))?;
-        linker.define("env", "gr_wait_up_to", func!(store, u32))?;
-        linker.define("env", "gr_wake", func!(store, i32, u32, i32))?;
+        let mut env = env::Env {
+            linker: &mut linker,
+            store: &mut store,
+            memory,
+        };
+
+        for import in module.imports() {
+            env.define(import.module(), import.name())?;
+        }
     }
 
     let instance = linker
@@ -187,48 +78,151 @@ fn execute(wasm: &[u8], method: &str) -> Result<Vec<u8>> {
         .get_export(&store, method)
         .and_then(Extern::into_func)
         .ok_or_else(|| anyhow!("could not find function \"{}\"", method))?
-        .typed::<(), (), _>(&mut store)?;
+        .typed::<(), ()>(&mut store)?;
 
     metadata.call(&mut store, ())?;
-    Ok(store.state().msg.clone())
+    Ok(store.data().msg.clone())
 }
 
-mod funcs {
+mod env {
     use super::HostState;
+    use anyhow::{anyhow, Result};
     use wasmi::{
-        core::{memory_units::Pages, Trap, TrapCode},
-        AsContext, AsContextMut, Caller, Extern, Func, Memory, Store,
+        core::{Pages, Trap, TrapCode},
+        AsContext, AsContextMut, Caller, Extern, Func, Linker, Memory, Store,
     };
 
-    pub fn alloc(store: &mut Store<HostState>, memory: Memory) -> Extern {
+    /// Environment for the wasm execution.
+    pub struct Env<'e> {
+        pub linker: &'e mut Linker<HostState>,
+        pub store: &'e mut Store<HostState>,
+        pub memory: Memory,
+    }
+
+    macro_rules! func {
+        ($store:tt) => {
+            func!($store,)
+        };
+        ($store:tt, $($ty:tt),* ) => {
+            Extern::Func(Func::wrap(
+                $store,
+                move |_caller: Caller<'_, HostState>, $(_: $ty),*| { Ok(()) },
+            ))
+        };
+        (@result $store:tt, $($ty:tt),* ) => {
+            Extern::Func(Func::wrap(
+                $store,
+                move |_caller: Caller<'_, HostState>, $(_: $ty),*| { 0 },
+            ))
+        };
+    }
+
+    impl Env<'_> {
+        /// Define function in the environment.
+        pub fn define(&mut self, module: &str, name: &str) -> Result<()> {
+            if module != "env" {
+                return Err(anyhow!("module \"{}\" not found", module));
+            }
+
+            let memory = self.memory.clone();
+            let store = &mut self.store;
+
+            let external = match name {
+                "memory" => Extern::Memory(memory),
+                "alloc" => alloc(self.store, memory),
+                "gr_oom_panic" => gr_oom_panic(store),
+                "gr_read" => gr_read(store, memory),
+                "gr_reply" => gr_reply(store, memory),
+                "gr_panic" => gr_panic(store, memory),
+                "gr_size" => gr_size(store, memory),
+                // methods may be used by programs but not required by metadata.
+                "free" => func!(@result store, i32),
+                "gr_block_height" => func!(store, u32),
+                "gr_block_timestamp" => func!(store, u32),
+                "gr_create_program_wgas" => func!(store, i32, i32, u32, i32, u32, u64, u32, i32),
+                "gr_create_program" => func!(store, i32, i32, u32, i32, u32, u64, i32),
+                "gr_debug" => func!(store, i32, u32),
+                "gr_exit" => func!(store, i32),
+                "gr_gas_available" => func!(store, i32),
+                "gr_leave" => func!(store),
+                "gr_message_id" => func!(store, i32),
+                "gr_out_of_gas" => func!(store),
+                "gr_pay_program_rent" => func!(store, i32, i32),
+                "gr_program_id" => func!(store, i32),
+                "gr_random" => func!(store, i32, i32),
+                "gr_reply_code" => func!(store, i32),
+                "gr_reply_commit" => func!(store, i32, i32),
+                "gr_reply_deposit" => func!(store, i32, u64, i32),
+                "gr_reply_input" => func!(store, u32, u32, i32, i32),
+                "gr_reply_push" => func!(store, i32, u32, i32),
+                "gr_reply_push_input" => func!(store, u32, u32, i32),
+                "gr_reply_push_input_wgas" => func!(store, u32, u32, u64, i32, i32),
+                "gr_reply_to" => func!(store, i32),
+                "gr_reply_wgas" => func!(store, i32, u32, u64, i32, i32),
+                "gr_reservation_reply" => func!(store, i32, i32, u32, i32),
+                "gr_reservation_send_commit" => func!(store, u32, i32, u32, i32),
+                "gr_reservation_send" => func!(store, i32, i32, u32, u32, i32),
+                "gr_reserve_gas" => func!(store, u64, u32, i32),
+                "gr_send" => func!(store, i32, i32, u32, u32, i32),
+                "gr_send_commit" => func!(store, u32, i32, u32, i32),
+                "gr_send_commit_wgas" => func!(store, u32, i32, u64, u32, i32),
+                "gr_send_init" => func!(store, i32),
+                "gr_send_input" => func!(store, i32, u32, u32, u32, i32),
+                "gr_send_input_wgas" => func!(store, i32, u32, u32, u64, u32, i32),
+                "gr_send_push" => func!(store, u32, i32, u32, i32),
+                "gr_send_push_input" => func!(store, u32, u32, u32, i32),
+                "gr_send_wgas" => func!(store, i32, i32, u32, u64, u32, i32),
+                "gr_signal_code" => func!(store, i32),
+                "gr_signal_from" => func!(store, i32),
+                "gr_source" => func!(store, i32),
+                "gr_system_reserve_gas" => func!(store, u64, i32),
+                "gr_unreserve_gas" => func!(store, i32, i32),
+                "gr_value" => func!(store, i32),
+                "gr_wait" => func!(store, u32),
+                "gr_wait_for" => func!(store, u32),
+                "gr_wait_up_to" => func!(store, u32),
+                "gr_wake" => func!(store, i32, u32, i32),
+                _ => return Err(anyhow!("export \"{}\" not found in env", name,)),
+            };
+
+            self.linker.define(module, name, external)?;
+
+            Ok(())
+        }
+    }
+
+    fn alloc(store: &mut Store<HostState>, memory: Memory) -> Extern {
         Extern::Func(Func::wrap(
             store,
-            move |mut caller: Caller<'_, HostState>, pages: i32| {
+            move |mut caller: Caller<'_, HostState>, pages: u32| {
                 memory
                     .clone()
-                    .grow(caller.as_context_mut(), Pages(pages as usize))
+                    .grow(
+                        caller.as_context_mut(),
+                        Pages::new(pages).unwrap_or_default(),
+                    )
                     .map_or_else(
                         |err| {
                             log::error!("{err:?}");
                             u32::MAX as i32
                         },
-                        |pages| pages.0 as i32,
+                        |pages| pages.to_bytes().unwrap_or_default() as i32,
                     )
             },
         ))
     }
 
-    pub fn gr_read(ctx: &mut Store<HostState>, memory: Memory) -> Extern {
+    fn gr_read(ctx: &mut Store<HostState>, memory: Memory) -> Extern {
         Extern::Func(Func::wrap(
             ctx,
             move |mut caller: Caller<'_, HostState>, at: u32, len: i32, buff: i32, err: i32| {
                 let (at, len, buff, err) = (at as _, len as usize, buff as _, err as _);
 
-                let msg = &caller.host_data().msg;
+                let msg = &caller.data().msg;
                 let payload = if at + len <= msg.len() {
                     msg[at..(at + len)].to_vec()
                 } else {
-                    return Err(Trap::Code(TrapCode::MemoryAccessOutOfBounds));
+                    return Err(Trap::new(TrapCode::MemoryOutOfBounds.trap_message()));
                 };
 
                 let len: u32 = memory
@@ -243,7 +237,7 @@ mod funcs {
                     .write(caller.as_context_mut(), err, &len.to_le_bytes())
                     .map_err(|e| {
                         log::error!("{:?}", e);
-                        Trap::Code(TrapCode::MemoryAccessOutOfBounds)
+                        Trap::new(TrapCode::MemoryOutOfBounds.trap_message())
                     })?;
 
                 Ok(())
@@ -251,7 +245,7 @@ mod funcs {
         ))
     }
 
-    pub fn gr_reply(ctx: &mut Store<HostState>, memory: Memory) -> Extern {
+    fn gr_reply(ctx: &mut Store<HostState>, memory: Memory) -> Extern {
         Extern::Func(Func::wrap(
             ctx,
             move |mut caller: Caller<'_, HostState>, ptr: u32, len: i32, _value: i32, _err: i32| {
@@ -261,20 +255,20 @@ mod funcs {
                     .read(caller.as_context(), ptr as usize, &mut result)
                     .map_err(|e| {
                         log::error!("{:?}", e);
-                        Trap::Code(TrapCode::MemoryAccessOutOfBounds)
+                        Trap::new(TrapCode::MemoryOutOfBounds.trap_message())
                     })?;
-                caller.host_data_mut().msg = result;
+                caller.data_mut().msg = result;
 
                 Ok(())
             },
         ))
     }
 
-    pub fn gr_size(ctx: &mut Store<HostState>, memory: Memory) -> Extern {
+    fn gr_size(ctx: &mut Store<HostState>, memory: Memory) -> Extern {
         Extern::Func(Func::wrap(
             ctx,
             move |mut caller: Caller<'_, HostState>, size_ptr: u32| {
-                let size = caller.host_data().msg.len() as u32;
+                let size = caller.data().msg.len() as u32;
 
                 memory
                     .clone()
@@ -285,7 +279,7 @@ mod funcs {
                     )
                     .map_err(|e| {
                         log::error!("{:?}", e);
-                        Trap::Code(TrapCode::MemoryAccessOutOfBounds)
+                        Trap::new(TrapCode::MemoryOutOfBounds.trap_message())
                     })?;
 
                 Ok(())
@@ -293,14 +287,14 @@ mod funcs {
         ))
     }
 
-    pub fn gr_panic(ctx: &mut Store<HostState>, memory: Memory) -> Extern {
+    fn gr_panic(ctx: &mut Store<HostState>, memory: Memory) -> Extern {
         Extern::Func(Func::wrap(
             ctx,
             move |caller: Caller<'_, HostState>, ptr: u32, len: i32| {
                 let mut buff = Vec::with_capacity(len as usize);
                 memory.read(caller, ptr as usize, &mut buff).map_err(|e| {
                     log::error!("{e:?}");
-                    Trap::Code(TrapCode::MemoryAccessOutOfBounds)
+                    Trap::new(TrapCode::MemoryOutOfBounds.trap_message())
                 })?;
 
                 log::error!("Panic: {}", String::from_utf8_lossy(&buff));
@@ -309,7 +303,7 @@ mod funcs {
         ))
     }
 
-    pub fn gr_oom_panic(ctx: impl AsContextMut) -> Extern {
+    fn gr_oom_panic(ctx: impl AsContextMut) -> Extern {
         Extern::Func(Func::wrap(ctx, || {
             log::error!("OOM panic occurred");
             Ok(())
