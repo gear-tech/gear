ff5cb0fc758d66776834ba84f2e2418072eabeed
Arseniy Lyashenko <arsenylyashenko.3@gmail.com>
2023-10-29 20:24:08 +0300

    Update cargo-xwin.sh

--- scripts/cargo-xwin.sh
+++ scripts/cargo-xwin.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-XWIN_ARCH="x86_64" CARGO_BUILD_TARGET=x86_64-pc-windows-msvc cargo xwin $@
+XWIN_ARCH="x86_64" CARGO_BUILD_TARGET=x86_64-pc-windows-msvc CARGO_TARGET_DIR="target-xwin" cargo xwin $@
