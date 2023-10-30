2682aec2602c182405c601aa77fa60b786a55002
Vadim Smirnov <ukint-vs@proton.me>
2023-10-30 18:00:58 +0800

    Update weights

--- runtime/vara/src/lib.rs
+++ runtime/vara/src/lib.rs
@@ -241,7 +241,7 @@ impl frame_system::Config for Runtime {
     /// The data to be stored in an account.
     type AccountData = pallet_balances::AccountData<Balance>;
     /// Weight information for the extrinsics of this pallet.
-    type SystemWeightInfo = weights::frame_system::WeightInfo<Runtime>;
+    type SystemWeightInfo = weights::frame_system::SubstrateWeight<Runtime>;
     /// This is used as an identifier of the chain. 42 is the generic substrate prefix.
     type SS58Prefix = SS58Prefix;
     /// The set code logic, just the default since we're not a parachain.
@@ -351,7 +351,7 @@ impl pallet_balances::Config for Runtime {
     type DustRemoval = pallet_gear_staking_rewards::OffsetPoolDust<Self>;
     type ExistentialDeposit = ConstU128<EXISTENTIAL_DEPOSIT>;
     type AccountStore = System;
-    type WeightInfo = ();
+    type WeightInfo = weights::pallet_balances::SubstrateWeight<Runtime>;
     type FreezeIdentifier = ();
     type MaxFreezes = ();
     type HoldIdentifier = ();
