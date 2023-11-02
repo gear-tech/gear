257c133462f8c3ea3963161ebd0b9b1920f5bbda
Gregory Sobol <grishasobol@mail.ru>
2023-11-02 15:45:54 +0100

    append file docs

--- common/numerated/src/tests.rs
+++ common/numerated/src/tests.rs
@@ -1,4 +1,22 @@
-#![allow(clippy::reversed_empty_ranges)]
+// This file is part of Gear.
+
+// Copyright (C) 2023 Gear Technologies Inc.
+// SPDX-License-Identifier: GPL-3.0-or-later WITH Classpath-exception-2.0
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+//! Property testing for Numerated, Interval and IntervalsTree.
 
 use crate::{
     mock::{self, test_interval, test_numerated, IntervalAction, TreeAction},
