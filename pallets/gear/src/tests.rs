e3abf41ac303a53032789b0d9705c29279d0c47d
mqxf <mqxf@mvteam.dev>
2023-11-06 13:49:21 +0100

    Remove autogenerated `use`
    Co-authored-by: Dmitry Novikov <novikov.dm.al@gmail.com>

--- pallets/gear/src/tests.rs
+++ pallets/gear/src/tests.rs
@@ -16,7 +16,6 @@
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-use crate::tests::utils::ProgramCodeKind::Default;
 use crate::{
     internal::HoldBoundBuilder,
     manager::HandleKind,
