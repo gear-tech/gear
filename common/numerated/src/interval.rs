3f9a75556bfe1c4843ffa651f7c5c1d669b72761
Gregory Sobol <grishasobol@mail.ru>
2023-11-24 12:34:43 +0100

    fix docs

--- common/numerated/src/interval.rs
+++ common/numerated/src/interval.rs
@@ -341,7 +341,7 @@ impl<T: Numerated + LowerBounded + UpperBounded> NonEmptyInterval<T> {
     /// Returns size of interval in `T` if it's possible.
     /// - If interval size is bigger than `T` possible elements amount, then returns `None`.
     /// - If interval size is equal to some `T::N`, then returns `T` of corresponding numeration:
-    /// ````
+    /// ````ignore
     ///   { T::N::zero() -> T::min_value(), T::N::one() -> T::min_value() + 1, ... }
     /// ````
     pub fn size(&self) -> Option<T> {
