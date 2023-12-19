c8f5635c4d5430a91b0de8a517c68ad2a46d4996
Vadim Smirnov <ukint-vs@proton.me>
2023-12-19 17:50:53 +0100

    duplicate

--- node/authorship/src/tests.rs
+++ node/authorship/src/tests.rs
@@ -534,7 +534,6 @@ fn block_max_gas_works() {
 
     init_logger();
     gear_runtime_interface::sandbox_init();
-    gear_runtime_interface::sandbox_init();
 
     init!(client, backend, txpool, spawner, genesis_hash);
 
