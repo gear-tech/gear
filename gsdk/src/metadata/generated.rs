e826692a6a47b01d590a1935500b721c5a510d48
Eugene Kovalev <eugene.a.kovalev@gmail.com>
2024-01-18 18:20:41 +0300

    Update gsdk metadata

--- gsdk/src/metadata/generated.rs
+++ gsdk/src/metadata/generated.rs
@@ -717,14 +717,6 @@ pub mod runtime_types {
             pub mod ids {
                 use super::runtime_types;
                 #[derive(
-                    ::subxt::ext::codec::CompactAs,
-                    Debug,
-                    crate::gp::Decode,
-                    crate::gp::DecodeAsType,
-                    crate::gp::Encode,
-                )]
-                pub struct BuiltinId(pub ::core::primitive::u64);
-                #[derive(
                     Copy, Debug, crate::gp::Decode, crate::gp::DecodeAsType, crate::gp::Encode,
                 )]
                 pub struct CodeId(pub [::core::primitive::u8; 32usize]);
@@ -2558,19 +2550,6 @@ pub mod runtime_types {
                     #[doc = "Deposit of funds that will not keep bank account alive."]
                     #[doc = "**Must be unreachable in Gear main protocol.**"]
                     InsufficientDeposit,
-                }
-            }
-        }
-        pub mod pallet_gear_builtin_actor {
-            use super::runtime_types;
-            pub mod pallet {
-                use super::runtime_types;
-                #[derive(Debug, crate::gp::Decode, crate::gp::DecodeAsType, crate::gp::Encode)]
-                #[doc = "Errors for the gear-builtin-actor pallet."]
-                pub enum Error {
-                    #[codec(index = 0)]
-                    #[doc = "`BuiltinId` already existd."]
-                    BuiltinIdAlreadyExists,
                 }
             }
         }
@@ -7575,8 +7554,6 @@ pub mod runtime_types {
                 GearVoucher(runtime_types::pallet_gear_voucher::pallet::Error),
                 #[codec(index = 108)]
                 GearBank(runtime_types::pallet_gear_bank::pallet::Error),
-                #[codec(index = 109)]
-                GearBuiltinActor(runtime_types::pallet_gear_builtin_actor::pallet::Error),
                 #[codec(index = 99)]
                 Sudo(runtime_types::pallet_sudo::pallet::Error),
                 #[codec(index = 199)]
@@ -8667,18 +8644,6 @@ pub mod storage {
             match self {
                 Self::Bank => "Bank",
                 Self::UnusedValue => "UnusedValue",
-            }
-        }
-    }
-    #[doc = "Storage of pallet `GearBuiltinActor`."]
-    pub enum GearBuiltinActorStorage {
-        Actors,
-    }
-    impl StorageInfo for GearBuiltinActorStorage {
-        const PALLET: &'static str = "GearBuiltinActor";
-        fn storage_name(&self) -> &'static str {
-            match self {
-                Self::Actors => "Actors",
             }
         }
     }
