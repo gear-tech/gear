54287f38bbf99fb8f85e29158be7256cba24683a
Sabaun Taraki <taraki.sabaun.rl@gmail.com>
2024-01-20 12:25:36 +0300

    State new `runtime_fuzzer::run` early draft

--- utils/runtime-fuzzer/src/lib.rs
+++ utils/runtime-fuzzer/src/lib.rs
@@ -120,10 +120,3 @@ fn execute_gear_call(sender: AccountId, call: GearCall) -> DispatchResultWithPos
         _ => unimplemented!("Unsupported currently."),
     }
 }
-
-fn get_sha1_string(input: &[u8]) -> String {
-    let mut hasher = sha1::Sha1::new();
-    hasher.update(input);
-
-    hex::encode(hasher.finalize())
-}
---
+++ utils/runtime-fuzzer/src/utils.rs
@@ -0,0 +1,32 @@
+// This file is part of Gear.
+
+// Copyright (C) 2021-2024 Gear Technologies Inc.
+// SPDX-License-Identifier: GPL-3.0-or-later WITH Classpath-exception-2.0
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+use crate::generator::{GearCallsGenerator, GenerationEnvironment};
+use sha1::*;
+
+#[cfg(test)]
+pub fn min_unstructured_input_size() -> usize {
+    GearCallsGenerator::random_data_requirement() + GenerationEnvironment::random_data_requirement()
+}
+
+pub(crate) fn get_sha1_string(input: &[u8]) -> String {
+    let mut hasher = sha1::Sha1::new();
+    hasher.update(input);
+
+    hex::encode(hasher.finalize())
+}
