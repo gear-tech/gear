27e53ddb72b290f6c4184587864fff8cfae72447
StackOverflowExcept1on <109800286+StackOverflowExcept1on@users.noreply.github.com>
2024-01-24 15:50:38 +0300

    add simple test to check avoid wait in init()

--- utils/wasm-gen/src/tests.rs
+++ utils/wasm-gen/src/tests.rs
@@ -172,6 +172,28 @@ fn remove_multiple_recursions() {
 }
 
 #[test]
+fn test_avoid_waits_works() {
+    let mut rng = SmallRng::seed_from_u64(123);
+    let mut buf = vec![0; UNSTRUCTURED_SIZE];
+    rng.fill_bytes(&mut buf);
+    let mut unstructured = Unstructured::new(&buf);
+
+    let mut injection_types = SyscallsInjectionTypes::all_never();
+    injection_types.set(InvocableSyscall::Loose(SyscallName::Wait), 1, 1);
+    let syscalls_config = SyscallsConfigBuilder::new(injection_types)
+        .with_waiting_probability(4)
+        .build();
+
+    let backend_report =
+        execute_wasm_with_custom_configs(&mut unstructured, syscalls_config, None, 1024, false, 0);
+
+    assert_eq!(
+        backend_report.termination_reason,
+        TerminationReason::Actor(ActorTerminationReason::Success)
+    );
+}
+
+#[test]
 fn test_source_as_address_param() {
     let mut rng = SmallRng::seed_from_u64(123);
     let mut buf = vec![0; UNSTRUCTURED_SIZE];
