b447136ce1e98629b3c25b5a2ce5cc9200f3236c
mqxf <mqxf@mvteam.dev>
2024-02-07 14:39:26 +0100

    feat(gtest): Add option to check for if a message panicked on RunResult (#3670)
    Co-authored-by: Shamil <66209982+shamilsan@users.noreply.github.com>
    Co-authored-by: StackOverflowExcept1on <109800286+StackOverflowExcept1on@users.noreply.github.com>
    Co-authored-by: Dmitry Novikov <novikov.dm.al@gmail.com>

--- examples/waiter/tests/mx_lock_access.rs
+++ examples/waiter/tests/mx_lock_access.rs
@@ -1,9 +1,8 @@
 use demo_waiter::{Command, LockContinuation, LockStaticAccessSubcommand, MxLockContinuation};
 use gear_core::ids::MessageId;
 use gtest::{Program, System};
-use utils::{assert_panicked, USER_ID};
 
-mod utils;
+pub const USER_ID: u64 = 10;
 
 #[test]
 fn drop_mx_lock_guard_from_different_msg_fails() {
@@ -39,14 +38,10 @@ fn access_mx_lock_guard_from_different_msg_fails(
     let lock_access_result =
         program.send(USER_ID, Command::MxLockStaticAccess(lock_access_subcommand));
 
-    assert_panicked(
-        &lock_access_result,
-        &format!(
-            "Mutex guard held by message {} is being accessed by message {}",
-            lock_msg_id,
-            lock_access_result.sent_message_id()
-        ),
-    );
+    lock_access_result.assert_panicked_with(format!(
+        "Mutex guard held by message {lock_msg_id} is being accessed by message {}",
+        lock_access_result.sent_message_id(),
+    ));
 }
 
 fn init_fixture(system: &System) -> (Program<'_>, MessageId) {
