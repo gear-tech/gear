5573b63159073445561e78733a998cab6c1e7c9b
clearloop <tianyi.gc@gmail.com>
2024-02-29 19:30:13 +0800

    ci(validation): manage validator checks in checks.yml

--- .github/actions/message/main.js
+++ .github/actions/message/main.js
@@ -3,14 +3,20 @@
  */
 
 const ps = require("child_process");
-const core = require('@actions/core');
-const github = require('@actions/github');
+const core = require("@actions/core");
+const github = require("@actions/github");
 
-const BUILD_LABELS = ["A0-pleasereview", 'A4-insubstantial', 'A2-mergeoncegreen'];
-const CHECKS = ["check", "build"]
-const DEPBOT = "[depbot]";
+const BUILD_LABELS = [
+  "A0-pleasereview",
+  "A4-insubstantial",
+  "A2-mergeoncegreen",
+];
+
+const CHECKS = ["check", "build"];
 const MACOS = "E2-forcemacos";
-const SCCACHE_PREFIX = '/mnt/sccache/';
+const VALIDATOR_LABEL = "check-validator";
+const DEPBOT = "[depbot]";
+const SCCACHE_PREFIX = "/mnt/sccache/";
 const SKIP_CI = "[skip-ci]";
 const SKIP_CACHE = "[skip-cache]";
 const [owner, repo] = ["gear-tech", "gear"];
@@ -47,11 +53,17 @@ async function mock(head_sha) {
  */
 async function main() {
   const {
-    pull_request: { title, head: { sha, ref: branch }, labels: _labels },
-    repository: { full_name: fullName }
+    pull_request: {
+      title,
+      head: { sha, ref: branch },
+      labels: _labels,
+    },
+    repository: { full_name: fullName },
   } = github.context.payload;
-  const labels = _labels.map(l => l.name);
-  const message = ps.execSync(`git log --format=%B -n 1 ${sha}`, { encoding: "utf-8" }).trim();
+  const labels = _labels.map((l) => l.name);
+  const message = ps
+    .execSync(`git log --format=%B -n 1 ${sha}`, { encoding: "utf-8" })
+    .trim();
 
   console.log("message: ", message);
   console.log("head-sha: ", sha);
@@ -61,29 +73,34 @@ async function main() {
 
   // Calculate configurations.
   const isDepbot = fullName === `${owner}/${repo}` && title.includes(DEPBOT);
-  const skipCache = [title, message].some(s => s.includes(SKIP_CACHE));
-  const skipCI = [title, message].some(s => s.includes(SKIP_CI));
-  const build = !skipCI && (isDepbot || BUILD_LABELS.some(label => labels.includes(label)));
+  const skipCache = [title, message].some((s) => s.includes(SKIP_CACHE));
+  const skipCI = [title, message].some((s) => s.includes(SKIP_CI));
+  const build =
+    !skipCI &&
+    (isDepbot || BUILD_LABELS.some((label) => labels.includes(label)));
   const macos = !skipCI && labels.includes(MACOS);
+  const validator = !skipCI && labels.includes(VALIDATOR_LABEL);
   const cache = SCCACHE_PREFIX + branch.replace("/", "_");
 
   // Set outputs
   core.setOutput("build", build);
   core.setOutput("check", !skipCI);
   core.setOutput("macos", macos);
-  !skipCache && core.setOutput("cache", cache)
+  core.setOutput("validator", validator);
+  !skipCache && core.setOutput("cache", cache);
 
   console.log("---");
   console.log("build: ", build);
-  console.log("cache: ", skipCache ? "false" : cache);
   console.log("check: ", !skipCI);
   console.log("macos: ", macos);
+  console.log("validator: ", validator);
+  console.log("cache: ", skipCache ? "false" : cache);
 
   // Mock checks if skipping CI.
   if (skipCI) await mock(sha);
 }
 
-main().catch(err => {
+main().catch((err) => {
   core.error("ERROR: ", err.message);
-  core.error(err.stack)
-})
+  core.error(err.stack);
+});
