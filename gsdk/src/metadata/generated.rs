e229d2d7fdf1a5c5b4dd2dc0ec1f7c8a8e5e9e32
StackOverflowExcept1on <109800286+StackOverflowExcept1on@users.noreply.github.com>
2024-03-12 12:25:39 +0300

    fix ci

--- gsdk/src/metadata/generated.rs
+++ gsdk/src/metadata/generated.rs
@@ -15,6 +15,7 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
+
 #[allow(dead_code, unused_imports, non_camel_case_types)]
 #[allow(clippy::all)]
 #[allow(rustdoc::broken_intra_doc_links)]
