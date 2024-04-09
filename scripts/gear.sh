c5b3629d348f5ea62c25a47b3ea4441ffc169cb9
Georgy Shepelev <geo.shepelev@gmail.com>
2024-04-09 20:01:06 +0200

    chore: Show error output on failure in measurements (#3874)

--- scripts/gear.sh
+++ scripts/gear.sh
@@ -64,6 +64,7 @@ gear_usage() {
     init           initializes and updates packages and toolchains
     run            run gear node
     test           test tool
+    check_extensions checks the required cargo extensions and installs if necessary
 
   Try ./gear.sh <COMMAND> -h (or --help) to learn more about each command.
 
@@ -326,6 +327,9 @@ case "$COMMAND" in
         test_usage
         exit 1; ;;
     esac;;
+
+  check_extensions)
+    check_extensions ;;
 
   *)
     header "Unknown option: '$COMMAND'"
