0dbc1c2b79bb005f19a3ae74b27c9c03c806384c
Gregory Sobol <grishasobol@mail.ru>
2024-04-27 16:00:56 +0200

    fix(pallet-gear-program): Migrations version (#3903)

--- pallets/gear-program/src/lib.rs
+++ pallets/gear-program/src/lib.rs
@@ -168,7 +168,7 @@ pub mod pallet {
     use sp_runtime::DispatchError;
 
     /// The current storage version.
-    pub(crate) const PROGRAM_STORAGE_VERSION: StorageVersion = StorageVersion::new(4);
+    pub(crate) const PROGRAM_STORAGE_VERSION: StorageVersion = StorageVersion::new(5);
 
     #[pallet::config]
     pub trait Config: frame_system::Config {
