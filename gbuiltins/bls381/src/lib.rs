2c78769f1ffcb5607d9920ae518a1e8117ec6be3
Georgy Shepelev <geo.shepelev@gmail.com>
2024-05-07 11:26:09 +0400

    set explicit codec index for response variants
    adjust test

--- gbuiltins/bls381/src/lib.rs
+++ gbuiltins/bls381/src/lib.rs
@@ -101,16 +101,22 @@ pub enum Request {
 #[codec(crate = codec)]
 pub enum Response {
     /// Result of the multi Miller loop, encoded: [`ArkScale<Bls12_381::TargetField>`].
+    #[codec(index = 0)]
     MultiMillerLoop(Vec<u8>),
     /// Result of the final exponentiation, encoded: [`ArkScale<Bls12_381::TargetField>`].
+    #[codec(index = 1)]
     FinalExponentiation(Vec<u8>),
     /// Result of the multi scalar multiplication, encoded: [`ArkScaleProjective<G1Projective>`].
+    #[codec(index = 2)]
     MultiScalarMultiplicationG1(Vec<u8>),
     /// Result of the multi scalar multiplication, encoded: [`ArkScaleProjective<G2Projective>`].
+    #[codec(index = 3)]
     MultiScalarMultiplicationG2(Vec<u8>),
     /// Result of the projective multiplication, encoded: [`ArkScaleProjective<G1Projective>`].
+    #[codec(index = 4)]
     ProjectiveMultiplicationG1(Vec<u8>),
     /// Result of the projective multiplication, encoded: [`ArkScaleProjective<G2Projective>`].
+    #[codec(index = 5)]
     ProjectiveMultiplicationG2(Vec<u8>),
 }
 
@@ -125,17 +131,19 @@ mod tests {
     // The following test checks that scale codec indexes of variants are set correctly.
     #[test]
     fn codec_enum_indexes() {
-        for (index, (variant, request)) in [
+        for (index, (variant, request, response)) in [
             (
                 REQUEST_MULTI_MILLER_LOOP,
                 Request::MultiMillerLoop {
                     a: vec![],
                     b: vec![],
                 },
+                Response::MultiMillerLoop(vec![]),
             ),
             (
                 REQUEST_FINAL_EXPONENTIATION,
                 Request::FinalExponentiation { f: vec![] },
+                Response::FinalExponentiation(vec![]),
             ),
             (
                 REQUEST_MULTI_SCALAR_MULTIPLICATION_G1,
@@ -143,6 +151,7 @@ mod tests {
                     bases: vec![],
                     scalars: vec![],
                 },
+                Response::MultiScalarMultiplicationG1(vec![]),
             ),
             (
                 REQUEST_MULTI_SCALAR_MULTIPLICATION_G2,
@@ -150,6 +159,7 @@ mod tests {
                     bases: vec![],
                     scalars: vec![],
                 },
+                Response::MultiScalarMultiplicationG2(vec![]),
             ),
             (
                 REQUEST_PROJECTIVE_MULTIPLICATION_G1,
@@ -157,6 +167,7 @@ mod tests {
                     base: vec![],
                     scalar: vec![],
                 },
+                Response::ProjectiveMultiplicationG1(vec![]),
             ),
             (
                 REQUEST_PROJECTIVE_MULTIPLICATION_G2,
@@ -164,15 +175,19 @@ mod tests {
                     base: vec![],
                     scalar: vec![],
                 },
+                Response::ProjectiveMultiplicationG2(vec![]),
             ),
         ]
         .into_iter()
         .enumerate()
         {
             assert_eq!(index, variant.into());
-            let encoded = request.encode();
 
-            assert!(matches!(encoded.first().copied(), Some(v) if v == variant));
+            let request = request.encode();
+            assert!(matches!(request.first().copied(), Some(v) if v == variant));
+
+            let response = response.encode();
+            assert!(matches!(response.first().copied(), Some(v) if v == variant));
         }
     }
 }
