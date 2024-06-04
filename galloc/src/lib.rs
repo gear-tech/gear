ff141c8109e73d9110835f528fe4f9e5a201dc7d
Vadim Smirnov <ukint-vs@proton.me>
2024-06-04 12:57:35 +0400

    rm cfg_attr

--- galloc/src/lib.rs
+++ galloc/src/lib.rs
@@ -17,7 +17,6 @@
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 #![no_std]
-#![cfg_attr(feature = "strict", deny(warnings))]
 #![doc(html_logo_url = "https://docs.gear.rs/logo.svg")]
 #![doc(html_favicon_url = "https://gear-tech.io/favicons/favicon.ico")]
 
