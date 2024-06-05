948cdce083deced218ee9a31742fc2724a447732
mertwole <mertwole@gmail.com>
2024-06-05 08:26:50 +0000

    docker-compose for local testnet

--- runtime/vara/src/constants.rs
+++ runtime/vara/src/constants.rs
@@ -79,7 +79,7 @@ pub mod time {
 
     // NOTE: Currently it is not possible to change the epoch duration after the chain has started.
     //       Attempting to do so will brick block production.
-    pub const EPOCH_DURATION_IN_BLOCKS: BlockNumber = 2 * HOURS;
+    pub const EPOCH_DURATION_IN_BLOCKS: BlockNumber = 5 * MINUTES;
     pub const EPOCH_DURATION_IN_SLOTS: u64 = {
         const SLOT_FILL_RATE: f64 = MILLISECS_PER_BLOCK as f64 / SLOT_DURATION as f64;
 
