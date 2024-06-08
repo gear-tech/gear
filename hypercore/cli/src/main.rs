6fc9213704185a32df64771b317ad27af111eef1
Vadim Smirnov <ukint-vs@proton.me>
2024-06-08 17:05:01 +0400

    feat(hypercore-cli): Print info, parse extra command (#72)
    * feat(hypercore-cli): Print info, parse extra command with file conf
    
    * eth symbol

--- hypercore/cli/src/main.rs
+++ hypercore/cli/src/main.rs
@@ -24,33 +24,47 @@ mod service;
 use crate::{args::Args, config::Config, service::Service};
 use anyhow::Context;
 use clap::Parser;
+use env_logger::Env;
 use std::{env, fs};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let optional_config_path = env::current_dir()?.join(".hypercore.toml");
-    let args: Args = if fs::metadata(&optional_config_path).is_ok() {
-        let str = fs::read_to_string(optional_config_path)?;
-        toml::from_str(&str)?
-    } else {
-        Args::parse()
+    let args = {
+        let cli_args = Args::parse();
+        if fs::metadata(&optional_config_path).is_ok() {
+            let str = fs::read_to_string(optional_config_path)?;
+            let mut file_args: Args = toml::from_str(&str)?;
+            file_args.extra_command = cli_args.extra_command;
+            file_args
+        } else {
+            cli_args
+        }
     };
 
     let config =
         Config::try_from(args.clone()).with_context(|| "Failed to create configuration")?;
 
-    env_logger::try_init().with_context(|| "Failed to initialize logger")?;
+    env_logger::Builder::from_env(Env::default().default_filter_or("info"))
+        .try_init()
+        .with_context(|| "Failed to initialize logger")?;
+
+    print_info(&config);
 
     if let Some(extra_command) = args.extra_command {
         extra_command.run(&config)?;
     } else {
-        log::info!("Ethereum observer RPC: {}", config.ethereum_rpc);
-        log::info!("Database directory: {:?}", config.database_path);
-
         let service = Service::new(&config).await?;
 
         service.run().await?;
     }
 
     Ok(())
+}
+
+fn print_info(config: &Config) {
+    log::info!("💾 Database: {}", config.database_path.display());
+    log::info!("🔑 Key directory: {}", config.key_path.display());
+    log::info!("🛜 Network directory: {}", config.network_path.display());
+    log::info!("⧫  Ethereum observer RPC: {}", config.ethereum_rpc);
 }
