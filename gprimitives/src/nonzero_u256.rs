c1fe0ce39460abdd38d1e6d9f11c1f8b779e1460
vobradovich <vadik.on@gmail.com>
2024-06-17 17:20:15 +0200

    impl scale_info::Decode,Encode

--- gprimitives/src/nonzero_u256.rs
+++ gprimitives/src/nonzero_u256.rs
@@ -26,7 +26,11 @@ use core::{
     mem::transmute,
     num::{NonZeroU128, NonZeroU16, NonZeroU32, NonZeroU64, NonZeroU8},
 };
-use scale_info::TypeInfo;
+use scale_info::{
+    prelude::vec::Vec,
+    scale::{Decode, Encode, EncodeLike, Error, Input, Output},
+    TypeInfo,
+};
 
 /// A value that is known not to equal zero.
 #[derive(Clone, Copy, TypeInfo)]
@@ -239,6 +243,41 @@ impl TryFrom<u128> for NonZeroU256 {
         NonZeroU256::new(U256::from(value)).ok_or("integer value is zero")
     }
 }
+
+macro_rules! impl_for_non_zero {
+	( $( $name:ty ),* $(,)? ) => {
+		$(
+			impl Encode for $name {
+				fn size_hint(&self) -> usize {
+					self.get().size_hint()
+				}
+
+				fn encode_to<W: Output + ?Sized>(&self, dest: &mut W) {
+					self.get().encode_to(dest)
+				}
+
+				fn encode(&self) -> Vec<u8> {
+					self.get().encode()
+				}
+
+				fn using_encoded<R, F: FnOnce(&[u8]) -> R>(&self, f: F) -> R {
+					self.get().using_encoded(f)
+				}
+			}
+
+			impl EncodeLike for $name {}
+
+			impl Decode for $name {
+				fn decode<I: Input>(input: &mut I) -> Result<Self, Error> {
+					Self::new(Decode::decode(input)?)
+						.ok_or_else(|| Error::from("cannot create non-zero number from 0"))
+				}
+			}
+		)*
+	}
+}
+
+impl_for_non_zero!(NonZeroU256);
 
 #[cfg(test)]
 mod tests {
