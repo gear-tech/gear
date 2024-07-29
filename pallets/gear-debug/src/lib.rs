2a6fd51c0ebcf13b0af3489e7b306ee572566308
Roman Maslennikov <m3angreen@gmail.com>
2024-07-30 02:23:02 +0400

    gear-debug

--- pallets/gear-debug/src/lib.rs
+++ pallets/gear-debug/src/lib.rs
@@ -199,7 +199,17 @@ pub mod pallet {
     impl<T: Config> pallet_gear::DebugInfo for Pallet<T> {
         fn do_snapshot() {
             let dispatch_queue = QueueOf::<T>::iter()
-                .map(|v| v.unwrap_or_else(|e| unreachable!("Message queue corrupted! {:?}", e)))
+                .map(|v| {
+                    v.unwrap_or_else(|e| {
+                        let err_msg = format!(
+                            "DebugInfo::do_snapshot: Message queue corrupted. \
+                            Got error - {e:?}"
+                        );
+
+                        log::error!("{err_msg}");
+                        unreachable!("{err_msg}")
+                    })
+                })
                 .collect();
 
             let programs = T::ProgramStorage::iter()
