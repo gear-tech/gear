ef1f23c8afe3ba6f0978eb5581fa8bf4072df858
vobradovich <vadik.on@gmail.com>
2024-08-01 18:56:55 +0200

    remove licence, add check for_reply_3 handle_reply executed

--- examples/async-reply-hook/src/wasm.rs
+++ examples/async-reply-hook/src/wasm.rs
@@ -1,21 +1,3 @@
-// This file is part of Gear.
-
-// Copyright (C) 2023-2024 Gear Technologies Inc.
-// SPDX-License-Identifier: GPL-3.0-or-later WITH Classpath-exception-2.0
-
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU General Public License for more details.
-
-// You should have received a copy of the GNU General Public License
-// along with this program. If not, see <https://www.gnu.org/licenses/>.
-
 use gstd::{critical, debug, exec, msg, prelude::*, ActorId};
 
 #[gstd::async_main]
@@ -32,20 +14,24 @@ async fn main() {
         .up_to(Some(5))
         .expect("Failed to set timeout")
         .handle_reply(|| {
+            // This should be called in gas / 100 blocks, but the program exits by that time
             unreachable!("This should not be called");
         })
         .expect("Failed to set reply hook");
 
     // Case 3: Message with reply hook and we reply to it
+    let for_reply_3 = gstd::rc::Rc::new(core::cell::RefCell::new(false));
+    let for_reply_3_clone = for_reply_3.clone();
     let m3 = gstd::msg::send_bytes_for_reply(source, b"for_reply_3", 0, 1_000_000_000)
         .expect("Failed to send message")
-        .handle_reply(|| {
+        .handle_reply(move || {
             debug!("reply message_id: {:?}", msg::id());
             debug!("reply payload: {:?}", msg::load_bytes());
 
             assert_eq!(msg::load_bytes().unwrap(), [3]);
 
             msg::send_bytes(msg::source(), b"saw_reply_3", 0);
+            for_reply_3_clone.replace(true);
         })
         .expect("Failed to set reply hook");
 
@@ -65,11 +51,16 @@ async fn main() {
         .expect("Failed to set reply hook");
 
     m1.await.expect("Received error reply");
+
     assert_eq!(
         m2.await.expect_err("Should receive timeout"),
         gstd::errors::Error::Timeout(8, 8)
     );
+
     m3.await.expect("Received error reply");
+    // check for_reply_3 handle_reply executed
+    assert!(for_reply_3.replace(false));
+
     assert_eq!(
         m4.await.expect_err("Should receive timeout"),
         gstd::errors::Error::Timeout(8, 8)
