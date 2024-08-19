8c17db40f2374db7c549dfac1d6d2d39de80328a
Roman Maslennikov <m3angreen@gmail.com>
2024-08-19 18:23:09 +0400

    Fix docs II

--- gtest/src/lib.rs
+++ gtest/src/lib.rs
@@ -435,7 +435,7 @@
 //!
 //! // To give the balance to the program you should use [`System::transfer`] method:
 //! let mut prog = Program::current(&sys);
-//! sys.transfer(user_id, prog.id(), 1000);
+//! sys.transfer(user_id, prog.id(), 1000, true);
 //! assert_eq!(prog.balance(), 1000);
 //! ```
 //!
