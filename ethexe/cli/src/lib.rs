1b4e05a76ddb532a9a8e1c6c450fc2598fc94dc0
clearloop <tianyi.gc@gmail.com>
2024-08-23 05:42:55 +0800

    chore(ethexe): remove global feature stmt_expr_attributes

--- ethexe/cli/src/lib.rs
+++ ethexe/cli/src/lib.rs
@@ -15,7 +15,6 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
-#![feature(stmt_expr_attributes)]
 
 pub mod args;
 pub mod chain_spec;
