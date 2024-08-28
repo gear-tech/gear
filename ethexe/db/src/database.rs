f33632c27469a96d8df38d44b6dcd3dd66da562e
Gregory Sobol <grishasobol@mail.ru>
2024-08-28 08:48:41 +0200

    fix fir clippy

--- ethexe/db/src/database.rs
+++ ethexe/db/src/database.rs
@@ -299,11 +299,7 @@ impl CodesStorage for Database {
 
         self.kv
             .iter_prefix(&key_prefix)
-            .map(|(key, code_id)| {
-                // Avoid unused variable warning in release mode.
-                #[allow(unused_variables)]
-                let code_id = code_id;
-
+            .map(|#[allow(unused_variables)] (key, code_id)| {
                 let (splitted_key_prefix, program_id) = key.split_at(key_prefix.len());
                 debug_assert_eq!(splitted_key_prefix, key_prefix);
                 let program_id =
