4a7adb9cc3d3c935b2a12afa9af0c0244ca59117
Arsenii Lyashenko <arsenylyashenko.3@gmail.com>
2024-09-11 18:19:30 +0300

    feat(ethexe): Add TCP fallback transport (#4234)

--- ethexe/cli/src/chain_spec.rs
+++ ethexe/cli/src/chain_spec.rs
@@ -17,7 +17,9 @@ pub fn testnet_config() -> ChainSpec {
         ethereum_router_address: "0xf90d2956B6F36194fbac181Fb95b2f67274821da".to_string(),
         bootnodes: vec![
             "/ip4/54.183.94.171/udp/20333/quic-v1/p2p/12D3KooWQ5kJQs2WK5kzmBMShCNidpySDuLjt7aqZoVimdyCPRDz".parse().unwrap(),
-            "/ip4/54.183.94.171/udp/20334/quic-v1/p2p/12D3KooWAivseD2rweVeS2fyNuVFP1hWZ2gXRtMzrNK1ci4mmoMj".parse().unwrap()
+            "/ip4/54.183.94.171/udp/20334/quic-v1/p2p/12D3KooWAivseD2rweVeS2fyNuVFP1hWZ2gXRtMzrNK1ci4mmoMj".parse().unwrap(),
+            "/ip4/54.183.94.171/tcp/20333/p2p/12D3KooWQ5kJQs2WK5kzmBMShCNidpySDuLjt7aqZoVimdyCPRDz".parse().unwrap(),
+            "/ip4/54.183.94.171/tcp/20334/p2p/12D3KooWAivseD2rweVeS2fyNuVFP1hWZ2gXRtMzrNK1ci4mmoMj".parse().unwrap(),
         ],
     }
 }
