766ed04ff3c725f76a2edd2bec5fb948f01ed94c
Dmitry Novikov <novikov.dm.al@gmail.com>
2024-09-24 17:14:15 +0400

    impl Mailbox storage item

--- ethexe/runtime/src/wasm/storage.rs
+++ ethexe/runtime/src/wasm/storage.rs
@@ -20,7 +20,7 @@ use super::interface::database_ri;
 use alloc::{collections::BTreeMap, vec::Vec};
 use core_processor::configs::BlockInfo;
 use ethexe_runtime_common::{
-    state::{Allocations, MemoryPages, MessageQueue, ProgramState, Storage, Waitlist},
+    state::{Allocations, Mailbox, MemoryPages, MessageQueue, ProgramState, Storage, Waitlist},
     RuntimeInterface,
 };
 use gear_core::{memory::PageBuf, message::Payload, pages::GearPage};
@@ -64,6 +64,10 @@ impl Storage for RuntimeInterfaceStorage {
         database_ri::read_unwrapping(&hash)
     }
 
+    fn read_mailbox(&self, hash: H256) -> Option<Mailbox> {
+        database_ri::read_unwrapping(&hash)
+    }
+
     fn write_allocations(&self, allocations: Allocations) -> H256 {
         database_ri::write(allocations)
     }
@@ -94,6 +98,10 @@ impl Storage for RuntimeInterfaceStorage {
 
     fn write_waitlist(&self, waitlist: Waitlist) -> H256 {
         database_ri::write(waitlist)
+    }
+
+    fn write_mailbox(&self, mailbox: Mailbox) -> H256 {
+        database_ri::write(mailbox)
     }
 }
 
