e1c746a91610cc786153411938c5ac3f9f657818
Arsenii Lyashenko <arsenylyashenko.3@gmail.com>
2024-10-06 05:13:54 +0300

    Add debug options to cargo-xwin.sh

--- scripts/cargo-xwin.sh
+++ scripts/cargo-xwin.sh
@@ -1,3 +1,17 @@
 #!/usr/bin/env bash
 
-XWIN_ARCH="x86_64" CARGO_BUILD_TARGET=x86_64-pc-windows-msvc CARGO_TARGET_DIR="target-xwin" cargo xwin $@
+export CARGO_BUILD_TARGET="x86_64-pc-windows-msvc"
+export XWIN_ARCH="x86_64"
+export CARGO_TARGET_DIR="target-xwin"
+
+if [ "$1" = "--gdb" ]; then
+  export CARGO_TARGET_X86_64_PC_WINDOWS_MSVC_RUNNER="winedbg --gdb --no-start"
+  shift
+fi
+
+if [ "$1" = "--lldb" ]; then
+  export CARGO_TARGET_X86_64_PC_WINDOWS_MSVC_RUNNER="/opt/homebrew/opt/llvm/bin/lldb-server g :1234 -- wine"
+  shift
+fi
+
+cargo xwin $@
