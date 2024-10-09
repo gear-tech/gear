124fe9dcfb956f539b0fd0b3107a94dc8dd065e5
ByteNacked <ByteNacked@users.noreply.github.com>
2024-10-09 17:32:21 +0000

    deploy: 5bced58dced2f35c352c18a1fea0514a0d9e9ea6

---
+++ pr-4282/trait.impl/numerated/numerated/trait.Numerated.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"gear_core":[["impl&lt;const SIZE: <a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.u32.html\">u32</a>&gt; Numerated for <a class=\"struct\" href=\"gear_core/pages/struct.Page.html\" title=\"struct gear_core::pages::Page\">Page</a>&lt;SIZE&gt;"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ pr-4282/trait.impl/parity_scale_codec/codec/trait.Output.js
@@ -0,0 +1,4 @@
+(function() {var implementors = {
+"gclient":[],
+"gstd":[["impl Output for <a class=\"struct\" href=\"gstd/msg/struct.MessageHandle.html\" title=\"struct gstd::msg::MessageHandle\">MessageHandle</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
