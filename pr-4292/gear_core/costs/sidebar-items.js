0d64a46392083e658bf4a8c99a5a1aeeff23a17d
breathx <breathx@users.noreply.github.com>
2024-10-18 13:56:15 +0000

    deploy: 0d8c1d193ff5ec36a4482e92ec780fc1479b97e2

---
+++ pr-4292/gear_core/costs/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":["CostToken"],"struct":["BlocksAmount","BytesAmount","CallsAmount","CostOf","ExtCosts","InstantiationCosts","LazyPagesCosts","ProcessCosts","RentCosts","SyscallCosts"]};
---
+++ pr-4292/gsdk/metadata/runtime_types/pallet_identity/types/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"enum":["Data","Judgement"],"struct":["RegistrarInfo","Registration"]};
