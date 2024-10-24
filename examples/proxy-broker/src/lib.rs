73929e309d40aceec8da761b8b2313eab7ea41a4
Sabaun Taraki <taraki.sabaun.rl@gmail.com>
2024-10-24 20:05:39 +0300

    feat(pallet-gear-builtin): Add proxy builtin (#4259)
    Co-authored-by: Dmitry Novikov <novikov.dm.al@gmail.com>

--- examples/proxy-broker/src/lib.rs
+++ examples/proxy-broker/src/lib.rs
@@ -1,25 +1,33 @@
 // This file is part of Gear.
-
-// Copyright (C) 2021-2024 Gear Technologies Inc.
+//
+// Copyright (C) 2024 Gear Technologies Inc.
 // SPDX-License-Identifier: GPL-3.0-or-later WITH Classpath-exception-2.0
-
+//
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-
+//
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU General Public License for more details.
-
+//
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-//! Builtin actor pallet tests.
+#![cfg_attr(not(feature = "std"), no_std)]
 
-mod bad_builtin_ids;
-mod basic;
-mod bls381;
-mod proxy;
-mod staking;
+#[cfg(feature = "std")]
+mod code {
+    include!(concat!(env!("OUT_DIR"), "/wasm_binary.rs"));
+}
+
+#[cfg(feature = "std")]
+pub use code::WASM_BINARY_OPT as WASM_BINARY;
+
+#[cfg(not(feature = "std"))]
+pub const WASM_BINARY: &[u8] = &[];
+
+#[cfg(not(feature = "std"))]
+pub mod wasm;
