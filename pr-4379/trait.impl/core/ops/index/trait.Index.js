d65037549e21df275645c7a68fa48eff3a3f11f1
ark0f <ark0f@users.noreply.github.com>
2024-12-07 12:44:57 +0000

    deploy: 97f509280dd32b3195f7c101bf82460cccc7db64

---
+++ pr-4379/trait.impl/core/ops/index/trait.Index.js
@@ -0,0 +1,5 @@
+(function() {var implementors = {
+"gclient":[],
+"gear_common":[["impl&lt;Balance&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/index/trait.Index.html\" title=\"trait core::ops::index::Index\">Index</a>&lt;<a class=\"enum\" href=\"gear_common/enum.LockId.html\" title=\"enum gear_common::LockId\">LockId</a>&gt; for <a class=\"struct\" href=\"gear_common/gas_provider/struct.NodeLock.html\" title=\"struct gear_common::gas_provider::NodeLock\">NodeLock</a>&lt;Balance&gt;"]],
+"gstd":[]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
