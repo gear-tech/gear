ab91c0c7899694a8744182240ae11e7bbe66601c
Lazark0x <Lazark0x@users.noreply.github.com>
2024-12-07 23:27:32 +0000

    deploy: c898be988d844c995c340db90066ea31204cd5f9

---
+++ pr-4381/trait.impl/core/fmt/trait.Write.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"gstd":[]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
