c440d7f1aa2ce9eb7213e2dcc212179c8a1c4f7c
Vadim Smirnov <ukint-vs@proton.me>
2024-12-18 16:57:32 +0700

    setup router

--- ethexe/ethereum/src/lib.rs
+++ ethexe/ethereum/src/lib.rs
@@ -154,6 +154,7 @@ impl Ethereum {
                     _mirror: mirror_address,
                     _mirrorProxy: mirror_proxy_address,
                     _wrappedVara: wvara_address,
+                    _middleware: Address::default(),
                     _eraDuration: U256::from(24 * 60 * 60),
                     _electionDuration: U256::from(2 * 60 * 60),
                     _validators: validators,
