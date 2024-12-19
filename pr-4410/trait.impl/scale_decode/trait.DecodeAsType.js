869968f1ffa38dcd4caa4bad3dcc10c7bccddbe2
ukint-vs <ukint-vs@users.noreply.github.com>
2024-12-19 12:51:06 +0000

    deploy: f8960934b9fadeb08a279eae60455257b2e24fa1

---
+++ pr-4410/trait.impl/scale_decode/trait.DecodeAsType.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"gsdk":[]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
