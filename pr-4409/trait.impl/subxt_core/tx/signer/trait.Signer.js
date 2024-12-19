737ab5709d43468d8d10d76cbc77c07dacffad5e
Lazark0x <Lazark0x@users.noreply.github.com>
2024-12-19 09:43:28 +0000

    deploy: 7255ecf52e4722e3d86680a7601f96c115e50d50

---
+++ pr-4409/trait.impl/subxt_core/tx/signer/trait.Signer.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"gsdk":[["impl&lt;T, Pair&gt; Signer&lt;T&gt; for <a class=\"struct\" href=\"gsdk/signer/struct.PairSigner.html\" title=\"struct gsdk::signer::PairSigner\">PairSigner</a>&lt;T, Pair&gt;<div class=\"where\">where\n    T: Config,\n    Pair: PairT,\n    Pair::Signature: <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.Into.html\" title=\"trait core::convert::Into\">Into</a>&lt;T::Signature&gt;,</div>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
