befd40483be2b0d20663e3e9804dacae30e8407a
Dmitrii Novikov <novikov.dm.al@gmail.com>
2025-01-13 14:36:00 +0400

    fixit

--- examples/constructor/src/arg.rs
+++ examples/constructor/src/arg.rs
@@ -1,8 +1,26 @@
+// This file is part of Gear.
+
+// Copyright (C) 2021-2024 Gear Technologies Inc.
+// SPDX-License-Identifier: GPL-3.0-or-later WITH Classpath-exception-2.0
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+// NOTE: Don't use `gstd` here without `wasm-wrapper` feature.
 use alloc::{
     string::{String, ToString},
     vec::Vec,
 };
-use gstd::prelude::*;
 use parity_scale_codec::{Codec, Decode, Encode};
 
 #[derive(Clone, Debug, Decode, Encode)]
@@ -94,6 +112,7 @@ impl From<&'static str> for Arg<bool> {
 #[cfg(not(feature = "wasm-wrapper"))]
 mod wasm {
     use super::*;
+    use gstd::prelude::*;
 
     impl<T: 'static + Clone + Codec> Arg<T> {
         pub(crate) fn value(self) -> T {
