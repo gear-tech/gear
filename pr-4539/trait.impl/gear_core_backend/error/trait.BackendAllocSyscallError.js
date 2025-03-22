497d23872f45f3c309ffad26d62294e6bdc39c44
ark0f <ark0f@users.noreply.github.com>
2025-03-22 13:30:45 +0000

    deploy: fc0a2d10589bd83e4bdcbb7fcb0d825c683f9740

---
+++ pr-4539/trait.impl/gear_core_backend/error/trait.BackendAllocSyscallError.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["gear_core_processor",[["impl BackendAllocSyscallError for <a class=\"enum\" href=\"gear_core_processor/enum.AllocExtError.html\" title=\"enum gear_core_processor::AllocExtError\">AllocExtError</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[202]}
