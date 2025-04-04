d199ef3691b9cce8aa9e3140975ce1aec1645158
ecol-master <ecol-master@users.noreply.github.com>
2025-04-04 13:50:18 +0000

    deploy: d6b45b39d6ba8aac6064d05ed440d24de6558a07

---
+++ pr-4572/trait.impl/bounded_collections/trait.TypedGet.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["gclient",[]],["gear_common",[["impl TypedGet for <a class=\"struct\" href=\"gear_common/pallet_tests/struct.DbWeight.html\" title=\"struct gear_common::pallet_tests::DbWeight\">DbWeight</a>"],["impl TypedGet for <a class=\"struct\" href=\"gear_common/pallet_tests/struct.MinimumPeriod.html\" title=\"struct gear_common::pallet_tests::MinimumPeriod\">MinimumPeriod</a>"],["impl TypedGet for <a class=\"struct\" href=\"gear_common/pallet_tests/struct.RuntimeBlockWeights.html\" title=\"struct gear_common::pallet_tests::RuntimeBlockWeights\">RuntimeBlockWeights</a>"],["impl TypedGet for <a class=\"struct\" href=\"gear_common/pallet_tests/struct.SS58Prefix.html\" title=\"struct gear_common::pallet_tests::SS58Prefix\">SS58Prefix</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[14,724]}
