dfdc956a2e23954d22472f83d69cce02590fd7d6
Arsenii Lyashenko <arsenii.lyashenko@gmail.com>
2025-04-11 21:33:42 +0300

    Add command args

--- utils/instrument-whole-network/src/main.rs
+++ utils/instrument-whole-network/src/main.rs
@@ -17,6 +17,7 @@
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 use anyhow::Context;
+use clap::Parser;
 use futures::{stream, StreamExt, TryStreamExt};
 use gcli::cmd::config::Network;
 use gear_core::{
@@ -31,85 +32,84 @@ use tokio::task;
 use tracing::Level;
 use tracing_subscriber::EnvFilter;
 
-const EXCLUDED_CODES: &[(Network, &[[u8; 32]])] = &[
-    (Network::Mainnet, &[]),
-    (
-        Network::Testnet,
-        &[
-            // `gr_leave` somehow imported twice
-            hex!("4dd9c141603a668127b98809742cf9f0819d591fe6f44eff63edf2b529a556bd"),
-            hex!("90b021503f01db60d0ba00eac970d5d6845f1a757c667232615b5d6c0ff800cc"),
-            // `init` entrypoint has invalid signature
-            hex!("8990159f0730dfed622031af63c453d2bcd5644482cac651796bf229f25d23b6"),
-            // `handle` entrypoint has invalid signature
-            hex!("e8378f125ec82bb7f399d81b3481d5d62bb5d65749f47fea6cd65f7a48e9c24c"),
-            hex!("d815332c3980386e58d0d191c5161d33824d8a6356a355ccb3528e6428551ab3"),
-            // `init` export directly references `gr_leave` import
-            hex!("ec0cc5d401606415c8ed31bfd347865d19fd277eec7d7bc62c164070eb8c241a"),
-            // `gr_error` has been removed
-            hex!("10d92d804fc4d42341d5eb2ca04b59e8534fd196621bd3908e1eda0a54f00ab9"),
-            hex!("7ae2b90c96fd65439cd3c72d0c1de985b42400c5ad376d34d1a4cb070191ed2c"),
-            // `delay` argument in `gr_reply` was removed
-            hex!("2477bc4f927a3ae8c3534a824d6c5aec9fa9b0f4747a1f1d4ae5fabbe885b111"),
-            hex!("7daa1b4f3a4891bda3c6b669ca896fa12b83ce4c4e840cf1d88d473a330c35fc"),
-            // `gr_pay_program_rent` has been removed
-            hex!("4a0bd89b42de7071a527c13ed52527e941dcda92578585e1139562cdf8a1063e"),
-            hex!("d483a0e542ad20996b38a2efb1f41e8d863cc1659f1ceb89a79065849fadfeb5"),
-            // `ext_logging_log_version_1` import somehow occurred
-            hex!("75e61ed8f08379ff9ea7f69d542dceabf5f30bfcdf95db55eb6cab77ab3ddb56"),
-            hex!("164dfe52b1438c7e38d010bc28efc85bd307128859d745e801c9099cbd82bd4f"),
-            hex!("f92585a339751d7ba9da70a0536936cd8659df29bad777db13e1c7b813c1a301"),
-            // `ext_misc_print_utf8_version_1` import somehow occurred
-            hex!("c88b00cfd30d1668ebb50283b4785fd945ac36a4783f8eab39dec2819e06a6c9"),
-        ],
-    ),
+#[rustfmt::skip]
+const TESTNET_EXCLUDED_CODES: &[CodeId] = &[
+    // `gr_leave` somehow imported twice
+    CodeId::new(hex!("4dd9c141603a668127b98809742cf9f0819d591fe6f44eff63edf2b529a556bd")),
+    CodeId::new(hex!("90b021503f01db60d0ba00eac970d5d6845f1a757c667232615b5d6c0ff800cc")),
+    // `init` entrypoint has invalid signature
+    CodeId::new(hex!("8990159f0730dfed622031af63c453d2bcd5644482cac651796bf229f25d23b6")),
+    // `handle` entrypoint has invalid signature
+    CodeId::new(hex!("e8378f125ec82bb7f399d81b3481d5d62bb5d65749f47fea6cd65f7a48e9c24c")),
+    CodeId::new(hex!("d815332c3980386e58d0d191c5161d33824d8a6356a355ccb3528e6428551ab3")),
+    // `init` export directly references `gr_leave` import
+    CodeId::new(hex!("ec0cc5d401606415c8ed31bfd347865d19fd277eec7d7bc62c164070eb8c241a")),
+    // `gr_error` has been removed
+    CodeId::new(hex!("10d92d804fc4d42341d5eb2ca04b59e8534fd196621bd3908e1eda0a54f00ab9")),
+    CodeId::new(hex!("7ae2b90c96fd65439cd3c72d0c1de985b42400c5ad376d34d1a4cb070191ed2c")),
+    // `delay` argument in `gr_reply` was removed
+    CodeId::new(hex!("2477bc4f927a3ae8c3534a824d6c5aec9fa9b0f4747a1f1d4ae5fabbe885b111")),
+    CodeId::new(hex!("7daa1b4f3a4891bda3c6b669ca896fa12b83ce4c4e840cf1d88d473a330c35fc")),
+    // `gr_pay_program_rent` has been removed
+    CodeId::new(hex!("4a0bd89b42de7071a527c13ed52527e941dcda92578585e1139562cdf8a1063e")),
+    CodeId::new(hex!("d483a0e542ad20996b38a2efb1f41e8d863cc1659f1ceb89a79065849fadfeb5")),
+    // `ext_logging_log_version_1` import somehow occurred
+    CodeId::new(hex!("75e61ed8f08379ff9ea7f69d542dceabf5f30bfcdf95db55eb6cab77ab3ddb56")),
+    CodeId::new(hex!("164dfe52b1438c7e38d010bc28efc85bd307128859d745e801c9099cbd82bd4f")),
+    CodeId::new(hex!("f92585a339751d7ba9da70a0536936cd8659df29bad777db13e1c7b813c1a301")),
+    // `ext_misc_print_utf8_version_1` import somehow occurred
+    CodeId::new(hex!("c88b00cfd30d1668ebb50283b4785fd945ac36a4783f8eab39dec2819e06a6c9")),
 ];
 
-struct InstrumentationResult {
-    code_id: CodeId,
-    original_code: Vec<u8>,
-    res: Result<Code, CodeError>,
+#[derive(Debug, Parser)]
+struct Args {
+    /// Network to fetch codes from
+    #[clap(long)]
+    network: Network,
+    /// How many concurrent requests to make
+    #[clap(long, default_value_t = 16)]
+    concurrency: usize,
 }
 
-impl InstrumentationResult {
-    async fn fetch_and_instrument(
-        api: Api,
-        latest_block: H256,
-        code_id: CodeId,
-    ) -> anyhow::Result<Self> {
-        let original_code = api
-            .original_code_storage_at(code_id, latest_block)
-            .await
-            .with_context(|| format!("Failed to fetch original code {code_id}"))?;
+struct FailedCode {
+    code_id: CodeId,
+    original_code: Vec<u8>,
+    err: CodeError,
+}
 
-        let original_code_clone = original_code.clone();
-        let res = task::spawn_blocking(move || {
-            let schedule = Schedule::default();
-            Code::try_new(
-                original_code_clone,
-                schedule.instruction_weights.version,
-                |module| schedule.rules(module),
-                schedule.limits.stack_height,
-                schedule.limits.data_segments_amount.into(),
-            )
-        })
+async fn fetch_and_instrument(
+    api: Api,
+    latest_block: H256,
+    code_id: CodeId,
+) -> anyhow::Result<Option<FailedCode>> {
+    let original_code = api
+        .original_code_storage_at(code_id, latest_block)
         .await
-        .context("`gear_core::Code` panicked")?;
+        .with_context(|| format!("Failed to fetch original code {code_id}"))?;
 
-        Ok(Self {
-            code_id,
-            original_code,
-            res,
-        })
-    }
+    let original_code_clone = original_code.clone();
+    let res = task::spawn_blocking(move || {
+        let schedule = Schedule::default();
+        Code::try_new(
+            original_code_clone,
+            schedule.instruction_weights.version,
+            |module| schedule.rules(module),
+            schedule.limits.stack_height,
+            schedule.limits.data_segments_amount.into(),
+        )
+    })
+    .await
+    .context("`gear_core::Code` panicked")?;
 
-    fn err(self) -> Option<(CodeId, Vec<u8>, CodeError)> {
-        if let Err(err) = self.res {
-            Some((self.code_id, self.original_code, err))
-        } else {
-            None
-        }
-    }
+    let Err(err) = res else {
+        return Ok(None);
+    };
+
+    Ok(Some(FailedCode {
+        code_id,
+        original_code,
+        err,
+    }))
 }
 
 #[tokio::main]
@@ -122,16 +122,16 @@ async fn main() -> anyhow::Result<()> {
         )
         .init();
 
-    let network = Network::Testnet;
-    let excluded_codes = EXCLUDED_CODES
-        .iter()
-        .find_map(|(n, codes)| (network == *n).then_some(*codes))
-        .unwrap_or_default();
-    let excluded_codes: Vec<CodeId> = excluded_codes
-        .into_iter()
-        .copied()
-        .map(CodeId::new)
-        .collect();
+    let Args {
+        network,
+        concurrency,
+    } = Args::parse();
+
+    let excluded_codes = if let Network::Testnet = network {
+        TESTNET_EXCLUDED_CODES
+    } else {
+        &[]
+    };
 
     let api = Api::new(Some(network.as_ref()))
         .await
@@ -165,9 +165,9 @@ async fn main() -> anyhow::Result<()> {
     let failed_codes = stream::iter(keys)
         .map(|code_id| {
             let api = api.clone();
-            InstrumentationResult::fetch_and_instrument(api, latest_block, code_id)
+            fetch_and_instrument(api, latest_block, code_id)
         })
-        .buffer_unordered(16)
+        .buffer_unordered(concurrency)
         .chunks(100)
         .scan(0, |counter, chunk| {
             *counter += chunk.len();
@@ -175,8 +175,8 @@ async fn main() -> anyhow::Result<()> {
             future::ready(Some(stream::iter(chunk)))
         })
         .flatten()
+        .try_filter_map(|maybe_code| future::ready(Ok(maybe_code)))
         .try_filter(|res| future::ready(!excluded_codes.contains(&res.code_id)))
-        .try_filter_map(|res| future::ready(Ok(res.err())))
         .try_collect::<Vec<_>>()
         .await?;
 
@@ -192,7 +192,12 @@ async fn main() -> anyhow::Result<()> {
         );
     }
 
-    for (code_id, original_code, err) in failed_codes {
+    for FailedCode {
+        code_id,
+        original_code,
+        err,
+    } in failed_codes
+    {
         let mut wasm = temp_dir.join(code_id.to_string());
         wasm.set_extension("wasm");
         fs::write(&wasm, &original_code)
