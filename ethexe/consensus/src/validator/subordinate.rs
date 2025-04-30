a39390da1d9b8cb05e618c7b0d6ded1bca034104
Gregory Sobol <grishasobol@mail.ru>
2025-04-30 17:05:36 +0200

    add comment for MAX_PENDING_EVENTS

--- ethexe/consensus/src/validator/subordinate.rs
+++ ethexe/consensus/src/validator/subordinate.rs
@@ -25,6 +25,10 @@ use ethexe_signer::{Address, SignedData};
 use gprimitives::H256;
 use std::mem;
 
+
+/// In order to avoid too big size of pending events queue,
+/// subordinate state handler removes redundant pending events
+/// and also removes old events if we overflow this limit:
 const MAX_PENDING_EVENTS: usize = 10;
 
 #[derive(Debug, Display)]
