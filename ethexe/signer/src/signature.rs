bb78154c25abfab9ad00316d82450c0dcb6fc136
Gregory Sobol <grishasobol@mail.ru>
2025-05-05 13:41:52 +0200

    feat!(ethexe): block producer (#4591)

--- ethexe/signer/src/signature.rs
+++ ethexe/signer/src/signature.rs
@@ -18,7 +18,7 @@
 
 //! Secp256k1 signature types and utilities.
 
-use crate::{Digest, PrivateKey, PublicKey};
+use crate::{Address, Digest, PrivateKey, PublicKey, ToDigest};
 use anyhow::{Error, Result};
 use parity_scale_codec::{Decode, Encode};
 use secp256k1::{
@@ -123,6 +123,14 @@ impl Signature {
             .verify_ecdsa(&message, &signature.to_standard(), &secp256k1_public_key)
             .map_err(Into::into)
     }
+
+    pub fn verify_address(&self, address: Address, digest: Digest) -> Result<()> {
+        let public_key = self.recover_from_digest(digest)?;
+        if public_key.to_address() != address {
+            anyhow::bail!("signature verification failed: addresses mismatch");
+        }
+        self.verify(public_key, digest)
+    }
 }
 
 impl From<RawSignature> for Signature {
@@ -175,5 +183,76 @@ impl TryFrom<Signature> for RecoverableSignature {
             RecoveryId::try_from((sig.0[64] - 27) as i32)?,
         )
         .map_err(Into::into)
+    }
+}
+
+/// A signed data structure that contains the data and its signature.
+#[derive(Clone, Debug, Encode, Decode, PartialEq, Eq)]
+pub struct SignedData<T: Sized> {
+    data: T,
+    signature: Signature,
+}
+
+impl<T: Sized> SignedData<T> {
+    pub(crate) fn new(data: T, signature: Signature) -> Self {
+        Self { data, signature }
+    }
+
+    pub fn data(&self) -> &T {
+        &self.data
+    }
+
+    pub fn signature(&self) -> &Signature {
+        &self.signature
+    }
+
+    pub fn into_parts(self) -> (T, Signature) {
+        (self.data, self.signature)
+    }
+}
+
+impl<T: ToDigest + Sized> SignedData<T> {
+    /// Verify the signature with public key recovery from the signature.
+    pub fn verify_with_public_key_recover(&self) -> Result<()> {
+        self.signature
+            .verify_with_public_key_recover(self.data.to_digest())
+    }
+
+    /// Recovers public key which was used to create the signature.
+    pub fn recover(&self) -> Result<PublicKey> {
+        self.signature.recover_from_digest(self.data.to_digest())
+    }
+
+    /// Verifies that signed data is signed by the public key.
+    pub fn verify(&self, public_key: PublicKey) -> Result<()> {
+        self.signature.verify(public_key, self.data.to_digest())
+    }
+
+    /// Verifies that signed data is signed by the public key and the address matches the public key.
+    pub fn verify_address(&self, address: Address) -> Result<()> {
+        self.signature
+            .verify_address(address, self.data.to_digest())
+    }
+}
+
+impl SignedData<Digest> {
+    /// Verify the signature with public key recovery from the signature.
+    pub fn verify_with_public_key_recover(&self) -> Result<()> {
+        self.signature.verify_with_public_key_recover(self.data)
+    }
+
+    /// Recovers public key which was used to create the signature.
+    pub fn recover(&self) -> Result<PublicKey> {
+        self.signature.recover_from_digest(self.data)
+    }
+
+    /// Verifies that signed data is signed by the public key.
+    pub fn verify(&self, public_key: PublicKey) -> Result<()> {
+        self.signature.verify(public_key, self.data)
+    }
+
+    /// Verifies that signed data is signed by the public key and the address matches the public key.
+    pub fn verify_address(&self, address: Address) -> Result<()> {
+        self.signature.verify_address(address, self.data)
     }
 }
