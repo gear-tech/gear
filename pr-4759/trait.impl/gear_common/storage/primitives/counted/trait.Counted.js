2e285302680e9e833a5c96deb6fc572bf1efb4db
techraed <techraed@users.noreply.github.com>
2025-06-26 14:25:50 +0000

    deploy: 210bae3ee0dcf88041ffaf19c8a289fadf9f5c7b

---
+++ pr-4759/trait.impl/gear_common/storage/primitives/counted/trait.Counted.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["gear_common",[]],["pallet_gear_messenger",[["impl&lt;T: <a class=\"trait\" href=\"pallet_gear_messenger/pallet/trait.Config.html\" title=\"trait pallet_gear_messenger::pallet::Config\">Config</a>&gt; Counted for <a class=\"struct\" href=\"pallet_gear_messenger/pallet/struct.DispatchesWrap.html\" title=\"struct pallet_gear_messenger::pallet::DispatchesWrap\">DispatchesWrap</a>&lt;T&gt;"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[18,375]}
