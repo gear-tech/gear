dc0d612a44d2b307c498568b6745301e58b6edee
ark0f <ark0f@users.noreply.github.com>
2025-07-03 23:57:44 +0000

    deploy: 3a2c0290c8c51f212c54614618d2e248691cf1bc

---
+++ pr-4709/gear_core/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"mod":["buffer","code","costs","env","env_vars","gas","gas_metering","ids","memory","message","pages","percent","program","reservation","rpc","str","tasks","utils"]};
