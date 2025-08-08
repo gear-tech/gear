2603a7da7bd83832d281d3880ae3102ac4d6fae3
StackOverflowExcept1on <StackOverflowExcept1on@users.noreply.github.com>
2025-08-08 16:23:55 +0000

    deploy: c009c1d7b148f3645a55d60ed6a70630ca61e618

---
+++ pr-4816/trait.impl/gear_core/env/trait.Externalities.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["gear_core_processor",[["impl&lt;LP: LazyPagesInterface&gt; <a class=\"trait\" href=\"gear_core/env/trait.Externalities.html\" title=\"trait gear_core::env::Externalities\">Externalities</a> for <a class=\"struct\" href=\"gear_core_processor/struct.Ext.html\" title=\"struct gear_core_processor::Ext\">Ext</a>&lt;LP&gt;"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[324]}
