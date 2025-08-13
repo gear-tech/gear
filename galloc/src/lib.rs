ec4be83ff682cf144995e63184ee4f00cd3a9b4f
Arsenii Lyashenko <arsenii.lyashenko@gmail.com>
2025-08-14 01:48:10 +0300

    Revert "Use talc"
    This reverts commit 966125a4a6afedcf54f67909cd80d57890405836.

--- galloc/src/lib.rs
+++ galloc/src/lib.rs
@@ -26,7 +26,7 @@ static mut ALLOC: dlmalloc::GlobalDlmalloc = dlmalloc::GlobalDlmalloc;
 
 #[cfg(target_arch = "wasm32")]
 #[global_allocator]
-static mut ALLOC: wasm::GlobalGearTalc = wasm::GlobalGearTalc::new();
+static mut ALLOC: wasm::GlobalGearAlloc = wasm::GlobalGearAlloc;
 
 pub mod prelude;
 
@@ -34,13 +34,13 @@ pub mod prelude;
 mod wasm {
     use core::{
         alloc::{GlobalAlloc, Layout},
-        cell::OnceCell,
+        cell::Cell,
         fmt,
         fmt::Write,
         mem::MaybeUninit,
+        ptr,
     };
-    use dlmalloc::Allocator as _;
-    use talc::{OomHandler, Span, Talc, Talck, locking::AssumeUnlockable};
+    use dlmalloc::{Allocator as _, Dlmalloc};
 
     pub fn stack_debug(args: fmt::Arguments<'_>) {
         const MAX_BUFFER_SIZE: usize = 128;
@@ -78,147 +78,148 @@ mod wasm {
         });
     }
 
-    pub struct GlobalGearTalc {
-        inner: Talck<AssumeUnlockable, GearOomHandler>,
-        preinstalled_memory_init: OnceCell<()>,
-    }
+    static mut ALLOC: Dlmalloc<GearAlloc> = Dlmalloc::new_with_allocator(GearAlloc::new());
 
-    impl GlobalGearTalc {
-        pub const fn new() -> Self {
-            Self {
-                inner: Talc::new(GearOomHandler::new()).lock(),
-                preinstalled_memory_init: OnceCell::new(),
-            }
-        }
+    pub struct GlobalGearAlloc;
 
+    unsafe impl GlobalAlloc for GlobalGearAlloc {
         #[inline]
-        fn page_to_ptr(page: u32) -> *mut u8 {
-            (page as usize * GearOomHandler::PAGE_SIZE) as *mut u8
-        }
-
-        #[inline]
-        fn ptr_to_page(ptr: *mut u8) -> u32 {
-            (ptr as usize / GearOomHandler::PAGE_SIZE) as u32
-        }
-
-        fn init_preinstalled_memory(&self) {
-            unsafe extern "C" {
-                static __heap_base: i32;
-            }
-
-            self.preinstalled_memory_init.get_or_init(|| {
-                let heap_base = unsafe { &__heap_base as *const i32 as *mut u8 };
-                let page_begin = Self::ptr_to_page(heap_base);
-                let page_begin = Self::page_to_ptr(page_begin);
-                let remaining_space =
-                    page_begin as usize + GearOomHandler::PAGE_SIZE - heap_base as usize;
-                let heap_acme = unsafe { heap_base.add(remaining_space) };
-
-                let mut inner = self.inner.lock();
-                if page_begin == heap_base {
-                    // no additional memory is available
-                } else if let Ok(heap) = unsafe { inner.claim(Span::new(heap_base, heap_acme)) } {
-                    inner.oom_handler.prev_heap = heap;
-                }
-            });
-        }
-    }
-
-    unsafe impl GlobalAlloc for GlobalGearTalc {
         unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
-            self.init_preinstalled_memory();
-            unsafe { self.inner.alloc(layout) }
+            debug("GlobalGearAlloc::alloc");
+            let alloc = ptr::addr_of_mut!(ALLOC);
+            unsafe {
+                let ptr = (*alloc).malloc(layout.size(), layout.align());
+                (*alloc).trim(0);
+                ptr
+            }
         }
 
+        #[inline]
         unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
-            stack_debug(format_args!("dealloc({:?}, {:?})", ptr, layout));
-            unsafe { self.inner.dealloc(ptr, layout) }
+            debug("GlobalGearAlloc::dealloc");
+            let alloc = ptr::addr_of_mut!(ALLOC);
+            unsafe { (*alloc).free(ptr, layout.size(), layout.align()) }
         }
 
+        #[inline]
         unsafe fn alloc_zeroed(&self, layout: Layout) -> *mut u8 {
-            self.init_preinstalled_memory();
-            unsafe { self.inner.alloc_zeroed(layout) }
+            debug("GlobalGearAlloc::alloc_zeroed");
+            let alloc = ptr::addr_of_mut!(ALLOC);
+            unsafe { (*alloc).calloc(layout.size(), layout.align()) }
         }
 
+        #[inline]
         unsafe fn realloc(&self, ptr: *mut u8, layout: Layout, new_size: usize) -> *mut u8 {
-            unsafe { self.inner.realloc(ptr, layout, new_size) }
+            debug("GlobalGearAlloc::realloc");
+            let alloc = ptr::addr_of_mut!(ALLOC);
+            unsafe { (*alloc).realloc(ptr, layout.size(), layout.align(), new_size) }
         }
     }
 
-    pub struct GearOomHandler {
-        prev_heap: Span,
+    struct GearAlloc {
+        preinstalled_memory: Cell<bool>,
     }
 
-    impl GearOomHandler {
-        const PAGE_SIZE: usize = 1024 * 64;
-
+    impl GearAlloc {
         const fn new() -> Self {
             Self {
-                prev_heap: Span::empty(),
+                preinstalled_memory: Cell::new(false),
             }
+        }
+
+        #[inline]
+        fn page_to_ptr(&self, page: u32) -> *mut u8 {
+            (page as usize * self.page_size()) as *mut u8
+        }
+
+        #[inline]
+        fn ptr_to_page(&self, ptr: *mut u8) -> u32 {
+            (ptr as usize / self.page_size()) as u32
         }
     }
 
-    impl OomHandler for GearOomHandler {
-        fn handle_oom(talc: &mut Talc<Self>, layout: Layout) -> Result<(), ()> {
-            stack_debug(format_args!("handle_oom({:?})", layout));
+    fn debug(data: &str) {
+        unsafe { gsys::gr_debug(data.as_ptr(), data.len() as u32) }
+    }
 
-            let required = layout.size();
-            let mut delta_pages = required.div_ceil(Self::PAGE_SIZE);
+    unsafe impl dlmalloc::Allocator for GearAlloc {
+        fn alloc(&self, size: usize) -> (*mut u8, usize, u32) {
+            stack_debug(format_args!("GearAlloc::alloc({size})"));
 
-            let prev = 'prev: {
-                // This performs a scan, trying to find a smaller possible
-                // growth if the previous one was unsuccessful. Return
-                // any successful allocated to memory.
-                // If not quite enough, talc will invoke handle_oom again.
+            let pages = size.div_ceil(self.page_size());
+            let size = pages * self.page_size();
 
-                // if we're about to fail because of allocation failure
-                // we may as well try as hard as we can to probe what's permissable
-                // which can be done with a log2(n)-ish algorithm
-                // (factoring in repeated called to handle_oom)
-                while delta_pages != 0 {
-                    // use `core::arch::wasm` instead once it doesn't
-                    // require the unstable feature wasm_simd64?
-                    let result = unsafe { gsys::alloc(delta_pages as u32) };
-
-                    if result != u32::MAX {
-                        break 'prev result as usize;
-                    } else {
-                        delta_pages >>= 1;
-                        continue;
-                    }
+            if !self.preinstalled_memory.get() {
+                unsafe extern "C" {
+                    static __heap_base: i32;
                 }
 
-                return Err(());
-            };
+                let heap_base = unsafe { &__heap_base as *const i32 as *mut u8 };
+                let page_begin = self.ptr_to_page(heap_base);
+                let page_begin = self.page_to_ptr(page_begin);
+                let remaining_space = page_begin as usize + self.page_size() - heap_base as usize;
 
-            let prev_heap_acme = (prev * Self::PAGE_SIZE) as *mut u8;
-            let new_heap_acme = prev_heap_acme.wrapping_add(delta_pages * Self::PAGE_SIZE);
-
-            // try to get base & acme, which will fail if prev_heap is empty
-            // otherwise the allocator has been initialized previously
-            if let Some((prev_base, prev_acme)) = talc.oom_handler.prev_heap.get_base_acme()
-                && prev_acme == prev_heap_acme
-            {
-                talc.oom_handler.prev_heap = unsafe {
-                    talc.extend(
-                        talc.oom_handler.prev_heap,
-                        Span::new(prev_base, new_heap_acme),
-                    )
-                };
-
-                return Ok(());
+                if page_begin == heap_base {
+                    // no additional memory is available
+                    self.preinstalled_memory.set(true);
+                } else if pages == 1 {
+                    self.preinstalled_memory.set(true);
+                    return (heap_base, remaining_space, 0);
+                }
             }
 
-            talc.oom_handler.prev_heap = unsafe {
-                // delta_pages is always greater than zero
-                // thus one page is enough space for metadata
-                // therefore we can unwrap the result
-                talc.claim(Span::new(prev_heap_acme, new_heap_acme))
-                    .unwrap()
-            };
+            let page_no = unsafe { gsys::alloc(pages as u32) };
+            if page_no == u32::MAX {
+                return (ptr::null_mut(), 0, 0);
+            }
+            let ptr = self.page_to_ptr(page_no);
 
-            Ok(())
+            (ptr, size, 0)
+        }
+
+        fn remap(
+            &self,
+            _ptr: *mut u8,
+            _oldsize: usize,
+            _newsize: usize,
+            _can_move: bool,
+        ) -> *mut u8 {
+            debug("GearAlloc::remap");
+            ptr::null_mut()
+        }
+
+        fn free_part(&self, ptr: *mut u8, oldsize: usize, newsize: usize) -> bool {
+            debug("GearAlloc::free_part");
+
+            unsafe {
+                let start = self.ptr_to_page(ptr.add(newsize));
+                let end = self.ptr_to_page(ptr.add(oldsize)) - 1;
+
+                if start <= end {
+                    gsys::free_range(start, end) == 0
+                } else {
+                    false
+                }
+            }
+        }
+
+        fn free(&self, ptr: *mut u8, size: usize) -> bool {
+            debug("GearAlloc::free");
+            let start = self.ptr_to_page(ptr);
+            let end = unsafe { self.ptr_to_page(ptr.add(size)) };
+            unsafe { gsys::free_range(start, end) == 0 }
+        }
+
+        fn can_release_part(&self, _flags: u32) -> bool {
+            true
+        }
+
+        fn allocates_zeros(&self) -> bool {
+            true
+        }
+
+        fn page_size(&self) -> usize {
+            64 * 1024
         }
     }
 }
