67ed10bcba6dd99f4f67426240b4be89df19c6ff
techraed <techraed@users.noreply.github.com>
2025-08-15 15:46:34 +0000

    deploy: d3935f8486cb8d36e3a5dd6d9646502133a9f504

---
+++ pr-4605/gsdk/metadata/runtime_types/gear_core/memory/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"struct":["IntoPageBufError","PageBuf"]};
---
+++ pr-4605/trait.impl/gear_common/storage/primitives/triple_map/trait.TripleMapStorage.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["pallet_gear_program",[["impl&lt;T: <a class=\"trait\" href=\"pallet_gear_program/pallet/trait.Config.html\" title=\"trait pallet_gear_program::pallet::Config\">Config</a>&gt; TripleMapStorage for <a class=\"struct\" href=\"pallet_gear_program/pallet/struct.MemoryPageStorageWrap.html\" title=\"struct pallet_gear_program::pallet::MemoryPageStorageWrap\">MemoryPageStorageWrap</a>&lt;T&gt;"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[394]}
