eff661ad367c748f8e291df24f950852ae54e294
Arsenii Lyashenko <arsenii.lyashenko@gmail.com>
2025-09-02 16:46:28 +0300

    Initial symbol leaking fix

--- gcore/src/ctor.rs
+++ gcore/src/ctor.rs
@@ -16,6 +16,8 @@
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
+pub use paste::paste;
+
 use crate::{static_mut, static_ref};
 use arrayvec::ArrayVec;
 use core::{mem, ptr};
@@ -28,33 +30,46 @@ type AtExitFn = unsafe extern "C" fn();
 #[macro_export]
 macro_rules! ctor {
     (
-        unsafe extern "C" fn() {
+        unsafe extern "C" fn $priority:literal() {
             $($body:tt)*
         }
     ) => {
         const _: () = {
-            #[unsafe(link_section = ".init_array")]
-            #[used]
-            static _FUNC: unsafe extern "C" fn() = {
-                unsafe extern "C" fn ctor() {
-                    $($body)*
-                }
+            $crate::ctor::paste! {
+                #[unsafe(link_section = ".init_array." $priority )]
+                #[used]
+                static _FUNC: unsafe extern "C" fn() = {
+                    unsafe extern "C" fn ctor() {
+                        $($body)*
+                    }
 
-                ctor
-            };
+                    ctor
+                };
+            }
         };
     };
-}
-
-#[macro_export]
-macro_rules! dtor {
     (
         unsafe extern "C" fn() {
             $($body:tt)*
         }
     ) => {
         $crate::ctor! {
-            unsafe extern "C" fn() {
+            unsafe extern "C" fn 65535() {
+                $($body)*
+            }
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! dtor {
+    (
+        unsafe extern "C" fn $($priority:literal)?() {
+            $($body:tt)*
+        }
+    ) => {
+        $crate::ctor! {
+            unsafe extern "C" fn $($priority)?() {
                 unsafe extern "C" fn dtor() {
                     $($body)*
                 }
@@ -65,16 +80,23 @@ macro_rules! dtor {
     };
 }
 
-#[unsafe(no_mangle)]
-unsafe extern "C" fn __wasm_call_dtors() {
-    let dtors = unsafe { static_ref!(DTORS) };
-    for &(dtor, arg) in dtors {
-        unsafe { dtor(arg) }
+unsafe extern "C" {
+    fn __gcore_set_fns(
+        cxa_atexit: unsafe extern "C" fn(Dtor, *mut (), *mut ()) -> i32,
+        dtors: unsafe extern "C" fn(),
+    );
+
+    pub fn atexit(func: AtExitFn) -> i32;
+}
+
+ctor! {
+    unsafe extern "C" fn 10() {
+        unsafe { __gcore_set_fns(cxa_atexit, dtors) };
+        unsafe { static_mut!(DTORS).clear() };
     }
 }
 
-#[unsafe(no_mangle)]
-unsafe extern "C" fn __cxa_atexit(func: Dtor, arg: *mut (), _dso: *mut ()) -> i32 {
+unsafe extern "C" fn cxa_atexit(func: Dtor, arg: *mut (), _dso: *mut ()) -> i32 {
     let dtors = unsafe { static_mut!(DTORS) };
 
     if let Err(_) = dtors.try_push((func, arg)) {
@@ -84,12 +106,9 @@ unsafe extern "C" fn __cxa_atexit(func: Dtor, arg: *mut (), _dso: *mut ()) -> i3
     0
 }
 
-unsafe extern "C" fn call(arg: *mut ()) {
-    let f: AtExitFn = unsafe { mem::transmute::<*mut (), AtExitFn>(arg) };
-    unsafe { f() }
-}
-
-#[unsafe(no_mangle)]
-pub unsafe extern "C" fn atexit(func: AtExitFn) -> i32 {
-    unsafe { __cxa_atexit(call, func as *mut (), ptr::null_mut()) }
+unsafe extern "C" fn dtors() {
+    let dtors = unsafe { static_ref!(DTORS) };
+    for &(dtor, arg) in dtors.iter().rev() {
+        unsafe { dtor(arg) }
+    }
 }
