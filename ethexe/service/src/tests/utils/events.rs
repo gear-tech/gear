f9e0df53630f0f7f7c6fa392d6207d5eaba4937a
Arsenii Lyashenko <arsenii.lyashenko@gmail.com>
2025-09-07 23:25:57 +0300

    Remove unused `#[allow(dead_code)]` attribute in `TestingNetworkEvent` enum

--- ethexe/service/src/tests/utils/events.rs
+++ ethexe/service/src/tests/utils/events.rs
@@ -40,7 +40,6 @@ pub type TestingEventSender = Sender<TestingEvent>;
 pub type TestingEventReceiver = Receiver<TestingEvent>;
 
 #[derive(Debug, Clone, Eq, PartialEq)]
-#[allow(dead_code)]
 pub(crate) enum TestingNetworkEvent {
     Message {
         data: Vec<u8>,
