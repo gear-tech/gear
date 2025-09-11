aab7461f333a6406ac6e1d7810a663777d570f6a
Gleb Smirnov <glebsmirnov0708@gmail.com>
2025-09-11 20:41:26 +0300

    Fix accidencally doubled header in `str.rs`

--- core/src/limited/str.rs
+++ core/src/limited/str.rs
@@ -1,25 +1,5 @@
 // This file is part of Gear.
 
-// Copyright (C) 2021-2025 Gear Technologies Inc.
-// SPDX-License-Identifier: GPL-3.0-or-later WITH Classpath-exception-2.0
-
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU General Public License for more details.
-
-// You should have received a copy of the GNU General Public License
-// along with this program. If not, see <https://www.gnu.org/licenses/>.
-
-//! This module provides type for string with statically limited length.
-
-// This file is part of Gear.
-
 // Copyright (C) 2023-2025 Gear Technologies Inc.
 // SPDX-License-Identifier: GPL-3.0-or-later WITH Classpath-exception-2.0
 
@@ -36,7 +16,7 @@
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-//! This module provides type for string with limited length.
+//! This module provides type for string with statically limited length.
 
 use alloc::{borrow::Cow, string::String};
 use derive_more::{AsRef, Deref, Display, Into};
