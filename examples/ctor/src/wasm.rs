45494d2295dcbd0435c094ca9c0d31b3eb2e8798
Arsenii Lyashenko <arsenii.lyashenko@gmail.com>
2025-09-16 18:54:00 +0300

    Remove duplicated headers

--- examples/ctor/src/wasm.rs
+++ examples/ctor/src/wasm.rs
@@ -16,22 +16,6 @@
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-// Copyright (C) 2023-2025 Gear Technologies Inc.
-// SPDX-License-Identifier: GPL-3.0-or-later WITH Classpath-exception-2.0
-
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU General Public License for more details.
-
-// You should have received a copy of the GNU General Public License
-// along with this program. If not, see <https://www.gnu.org/licenses/>.
-
 use gstd::{prelude::*, static_mut, static_ref};
 
 static mut CTORS: u64 = 0;
