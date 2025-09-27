1e8aa9897e15c14c7fea2a32bb338da868257220
Vadim Smirnov <ukint-vs@proton.me>
2025-09-27 12:06:53 +0500

    update args

--- pallets/gear/src/lib.rs
+++ pallets/gear/src/lib.rs
@@ -584,6 +584,8 @@ pub mod pallet {
                         |module| schedule.rules(module),
                         schedule.limits.stack_height,
                         schedule.limits.data_segments_amount.into(),
+                        schedule.limits.type_section_len.into(),
+                        schedule.limits.parameters.into(),
                     ) {
                         Ok(_) => {}
                         Err(e) => {
