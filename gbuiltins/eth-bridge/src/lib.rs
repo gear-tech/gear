dbf190a46363a7d851667e33d794b8d7a4f6a5c4
Sabaun Taraki <sabaun.dev@gmail.com>
2025-10-02 12:19:00 +0500

    fix builds

--- gbuiltins/eth-bridge/src/lib.rs
+++ gbuiltins/eth-bridge/src/lib.rs
@@ -16,7 +16,8 @@
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-#[cfg_attr(not(feature = "std"), no_std)]
+#![cfg_attr(not(feature = "std"), no_std)]
+
 extern crate alloc;
 
 use alloc::vec::Vec;
