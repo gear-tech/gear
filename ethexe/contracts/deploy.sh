70a548f7ba7b28e3fd632bb1ffb20d005db9f632
Dmitry Kuzmin <82306830+ecol-master@users.noreply.github.com>
2025-10-22 08:32:12 +0300

    delete cfg files

--- ethexe/contracts/deploy.sh
+++
@@ -1,7 +0,0 @@
-# Deployment Vara.eth contracts
-export RPC="https://hoodi-reth-rpc.gear-tech.io"
-export PRIVATE_KEY="0x26f7a24ff7e226a118ea4eea21a12d9cc3bd43033a7efddf415f83d8bd58a98b"
-
-
-forge script script/Deployment.s.sol:DeploymentScript \
-    --rpc-url $RPC --broadcast --private-key $PRIVATE_KEY --slow
