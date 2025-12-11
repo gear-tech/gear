29f7f4b61aa1acd806d13b82cdb5993654863222
Gleb Smirnov <glebsmirnov0708@gmail.com>
2025-12-11 16:19:46 +0300

    Add initial payload to `upload_program` benchmark

--- pallets/gear/src/benchmarking/mod.rs
+++ pallets/gear/src/benchmarking/mod.rs
@@ -534,7 +534,9 @@ benchmarks! {
     upload_program {
         let c in 0 .. Perbill::from_percent(49).mul_ceil(T::Schedule::get().limits.code_len) / 1024;
         let s in 0 .. MAX_SALT_SIZE_BYTES;
+        let p in 0 .. MAX_PAYLOAD_LEN;
         let salt = vec![42u8; s as usize];
+        let init_payload = vec![42u8; p as usize];
         let value = CurrencyOf::<T>::minimum_balance();
         let caller = whitelisted_caller();
         CurrencyOf::<T>::make_free_balance_be(&caller, caller_funding::<T>());
@@ -542,7 +544,7 @@ benchmarks! {
         let origin = RawOrigin::Signed(caller);
 
         init_block::<T>(None);
-    }: _(origin, code, salt, vec![], 100_000_000_u64, value, false)
+    }: _(origin, code, salt, init_payload, 100_000_000_u64, value, false)
     verify {
         assert!(matches!(QueueOf::<T>::dequeue(), Ok(Some(_))));
     }
