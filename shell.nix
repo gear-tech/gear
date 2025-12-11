473733219b973063c312dd892132e43bd9e1ebc4
Gleb Smirnov <glebsmirnov0708@gmail.com>
2025-12-11 17:41:49 +0300

    build(nix): sort packages in each section of `shell.nix`

--- shell.nix
+++ shell.nix
@@ -27,27 +27,27 @@ pkgs.mkShell.override { stdenv = pkgs.llvmPackages.stdenv; } {
     pkgs.just
 
     # Maintenance tools and script dependencies
+    pkgs.cargo-hakari
+    pkgs.cargo-shear
     pkgs.jq
     pkgs.typos
-    pkgs.cargo-shear
-    pkgs.cargo-hakari
 
     # Build tools
-    pkgs.protobuf
     pkgs.binaryen
-    pkgs.foundry
     pkgs.cmake
+    pkgs.foundry
+    pkgs.nodejs
     pkgs.perl
     pkgs.pkg-config
-    pkgs.nodejs
+    pkgs.protobuf
 
     # Testing tools
-    pkgs.cargo-nextest
     pkgs.cargo-hack
+    pkgs.cargo-nextest
 
     # Fuzzing tools
-    pkgs.cargo-fuzz
     pkgs.cargo-binutils
+    pkgs.cargo-fuzz
     oldPkgs.rustfilt
   ];
 
