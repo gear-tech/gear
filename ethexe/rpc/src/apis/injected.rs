90626cd9a3fe50fcb54591e8dc6be11afc7cab34
Dmitry Kuzmin <dmitrykuzmin@MacBook-Pro-Dmitry.local>
2025-12-18 10:59:02 +0300

    fix log level

--- ethexe/rpc/src/apis/injected.rs
+++ ethexe/rpc/src/apis/injected.rs
@@ -123,7 +123,7 @@ impl InjectedApi {
 
     pub fn send_promise(&self, promise: SignedPromise) {
         let Some((_, promise_sender)) = self.promise_waiters.remove(&promise.data().tx_hash) else {
-            tracing::trace!(promise = ?promise, "receive unregistered promise");
+            tracing::warn!(promise = ?promise, "receive unregistered promise");
             return;
         };
 
