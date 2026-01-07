444f858d27db10ea86894c46e8c920884055ee27
playX18 <adel.prokurov281@outlook.com>
2026-01-08 06:30:35 +0700

    refactor(db): move announces from BlockMeta to AnnounceStorage

--- ethexe/common/src/db.rs
+++ ethexe/common/src/db.rs
@@ -42,9 +42,6 @@ pub struct BlockMeta {
     /// Block has been prepared, meaning:
     /// all metadata is ready, all predecessors till start block are prepared too.
     pub prepared: bool,
-    // TODO: #4945 remove announces from here
-    /// Set of announces included in the block.
-    pub announces: Option<BTreeSet<HashOf<Announce>>>,
     /// Queue of code ids waiting for validation status commitment on-chain.
     pub codes_queue: Option<VecDeque<CodeId>>,
     /// Last committed on-chain batch hash.
@@ -57,7 +54,6 @@ impl BlockMeta {
     pub fn default_prepared() -> Self {
         Self {
             prepared: true,
-            announces: Some(Default::default()),
             codes_queue: Some(Default::default()),
             last_committed_batch: Some(Default::default()),
             last_committed_announce: Some(Default::default()),
@@ -157,11 +153,13 @@ pub trait AnnounceStorageRO {
     fn announce_outcome(&self, announce_hash: HashOf<Announce>) -> Option<Vec<StateTransition>>;
     fn announce_schedule(&self, announce_hash: HashOf<Announce>) -> Option<Schedule>;
     fn announce_meta(&self, announce_hash: HashOf<Announce>) -> AnnounceMeta;
+    fn block_announces(&self, block_hash: H256) -> Option<BTreeSet<HashOf<Announce>>>;
 }
 
 #[auto_impl::auto_impl(&)]
 pub trait AnnounceStorageRW: AnnounceStorageRO {
     fn set_announce(&self, announce: Announce) -> HashOf<Announce>;
+    fn set_block_announces(&self, block_hash: H256, announces: BTreeSet<HashOf<Announce>>);
     fn set_announce_program_states(
         &self,
         announce_hash: HashOf<Announce>,
@@ -174,6 +172,11 @@ pub trait AnnounceStorageRW: AnnounceStorageRO {
         &self,
         announce_hash: HashOf<Announce>,
         f: impl FnOnce(&mut AnnounceMeta),
+    );
+    fn mutate_block_announces(
+        &self,
+        block_hash: H256,
+        f: impl FnOnce(&mut BTreeSet<HashOf<Announce>>),
     );
 }
 
