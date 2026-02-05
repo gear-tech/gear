664e49096adb79c18b2ca9945ff5cae397411dbd
Dmitry Kuzmin <82306830+ecol-master@users.noreply.github.com>
2026-02-05 20:28:36 +0700

    ethexe(contracts): remove allocations, add custom errors, update deps (#5088)
    Co-authored-by: StackOverflowExcept1on <109800286+StackOverflowExcept1on@users.noreply.github.com>

--- ethexe/contracts/script/upgrades/WrappedVara.s.sol
+++ ethexe/contracts/script/upgrades/WrappedVara.s.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.28;
 
-import {Upgrades} from "openzeppelin-foundry-upgrades/Upgrades.sol";
-import {Script} from "forge-std/Script.sol";
 import {WrappedVara} from "../../src/WrappedVara.sol";
+import {Script} from "forge-std/Script.sol";
+import {Upgrades} from "openzeppelin-foundry-upgrades/Upgrades.sol";
 
 contract WrappedVaraScript is Script {
     function setUp() public {}
