87908f9ff424c4a650dc52308c85458af45c18db
Gregory Sobol <grishasobolo@gmail.com>
2026-02-05 11:10:22 +0700

    review

--- ethexe/service/src/tests/mod.rs
+++ ethexe/service/src/tests/mod.rs
@@ -133,7 +133,9 @@ async fn basics() {
     config.rpc = Some(RpcConfig {
         listen_addr: SocketAddr::new(Ipv4Addr::LOCALHOST.into(), 9944),
         cors: None,
-        gas_allowance: DEFAULT_BLOCK_GAS_LIMIT_MULTIPLIER * config.node.block_gas_limit,
+        gas_allowance: DEFAULT_BLOCK_GAS_LIMIT_MULTIPLIER
+            .checked_mul(config.node.block_gas_limit)
+            .unwrap(),
         chunk_size: config.node.chunk_processing_threads,
     });
 
