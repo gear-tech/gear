fa1591c857bfca97a8c3bf3d1ef973d8ea767707
Gleb Smirnov <glebsmirnov0708@gmail.com>
2026-03-04 14:41:02 +0300

    feat(ethexe): limit max size of injected transaction payload and salt (#5183)

--- gsigner/src/schemes/secp256k1/address.rs
+++ gsigner/src/schemes/secp256k1/address.rs
@@ -51,6 +51,7 @@ use serde::{Deserialize, Deserializer, Serialize, Serializer};
     derive(
         parity_scale_codec::Encode,
         parity_scale_codec::Decode,
+        parity_scale_codec::MaxEncodedLen,
         scale_info::TypeInfo
     )
 )]
