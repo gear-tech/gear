9d31e4c7f64f191efe077f6198eed657cfcfc9ec
playX18 <adel.prokurov281@outlook.com>
2026-04-06 22:06:33 +0700

    fmt

--- ethexe/compute/src/tests.rs
+++ ethexe/compute/src/tests.rs
@@ -87,7 +87,10 @@ impl ProcessorExt for MockProcessor {
     }
 
     fn process_code(&mut self, code_and_id: CodeAndIdUnchecked) -> Result<ProcessedCodeInfo> {
-        self.process_code_calls.lock().unwrap().push(code_and_id.clone());
+        self.process_code_calls
+            .lock()
+            .unwrap()
+            .push(code_and_id.clone());
 
         Ok(self
             .process_codes_result
