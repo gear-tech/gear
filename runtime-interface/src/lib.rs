9d3ca40fd73de4f3de168b175c79e126da5d0d73
Arsenii Lyashenko <arsenii.lyashenko@gmail.com>
2026-05-11 19:16:27 +0300

    Revert "Implement lazy-pages host API"
    This reverts commit 6821c86daa27bc48a880d1435017a4776d10e2f2.

--- runtime-interface/src/lib.rs
+++ runtime-interface/src/lib.rs
@@ -23,24 +23,17 @@
 
 extern crate alloc;
 
-#[cfg(feature = "std")]
-pub use {
-    gear_lazy_pages_host_api::{
-        self as lazy_pages_detail, LazyPagesInitContext, LazyPagesProgramContext,
-    },
-    gear_sandbox_interface::sandbox,
-    gear_sandbox_interface::{
-        Instantiate, SandboxBackend, detail as sandbox_detail, init as sandbox_init,
-    },
-};
-
 use gear_core::{
     gas::GasLeft,
+    limited::LimitedStr,
     memory::{HostPointer, MemoryInterval},
 };
-use gear_lazy_pages_common::Status;
+use gear_lazy_pages_common::{GlobalsAccessConfig, Status};
 use parity_scale_codec::{Decode, Encode};
-use sp_runtime_interface::runtime_interface;
+use sp_runtime_interface::{
+    pass_by::{Codec, PassBy},
+    runtime_interface,
+};
 use sp_std::{result::Result, vec::Vec};
 #[cfg(feature = "std")]
 use {
@@ -49,10 +42,81 @@ use {
     gear_lazy_pages_common::ProcessAccessError,
 };
 
+#[cfg(feature = "sandbox")]
+pub use gear_sandbox_interface::sandbox;
+#[cfg(feature = "sandbox")]
+pub use gear_sandbox_interface::{
+    Instantiate, SandboxBackend, detail as sandbox_detail, init as sandbox_init,
+};
+
 const _: () = assert!(size_of::<HostPointer>() >= size_of::<usize>());
 
 // Domain Separation Tag for signatures on G2.
 pub const DST_G2: &[u8] = b"BLS_SIG_BLS12381G2_XMD:SHA-256_SSWU_RO_POP_";
+
+#[derive(Debug, Clone, Encode, Decode)]
+pub struct LazyPagesProgramContext {
+    /// Wasm program memory addr.
+    pub wasm_mem_addr: Option<HostPointer>,
+    /// Wasm program memory size.
+    pub wasm_mem_size: u32,
+    /// Wasm program stack end page.
+    pub stack_end: Option<u32>,
+    /// The field contains prefix to a program's memory pages, i.e.
+    /// `program_id` + `memory_infix`.
+    pub program_key: Vec<u8>,
+    /// Globals config to access globals inside lazy-pages.
+    pub globals_config: GlobalsAccessConfig,
+    /// Lazy-pages access costs.
+    pub costs: Vec<u64>,
+}
+
+impl PassBy for LazyPagesProgramContext {
+    type PassBy = Codec<LazyPagesProgramContext>;
+}
+
+#[derive(Debug, Clone, Encode, Decode)]
+pub struct LazyPagesInitContext {
+    pub page_sizes: Vec<u32>,
+    pub global_names: Vec<LimitedStr<'static>>,
+    pub pages_storage_prefix: Vec<u8>,
+}
+
+impl From<gear_lazy_pages_common::LazyPagesInitContext> for LazyPagesInitContext {
+    fn from(ctx: gear_lazy_pages_common::LazyPagesInitContext) -> Self {
+        let gear_lazy_pages_common::LazyPagesInitContext {
+            page_sizes,
+            global_names,
+            pages_storage_prefix,
+        } = ctx;
+
+        Self {
+            page_sizes,
+            global_names,
+            pages_storage_prefix,
+        }
+    }
+}
+
+impl From<LazyPagesInitContext> for gear_lazy_pages_common::LazyPagesInitContext {
+    fn from(ctx: LazyPagesInitContext) -> Self {
+        let LazyPagesInitContext {
+            page_sizes,
+            global_names,
+            pages_storage_prefix,
+        } = ctx;
+
+        Self {
+            page_sizes,
+            global_names,
+            pages_storage_prefix,
+        }
+    }
+}
+
+impl PassBy for LazyPagesInitContext {
+    type PassBy = Codec<LazyPagesInitContext>;
+}
 
 #[cfg(feature = "std")]
 #[derive(Debug, Default)]
@@ -83,41 +147,40 @@ pub trait GearRI {
     #[version(2)]
     fn pre_process_memory_accesses(reads: &[u8], writes: &[u8], gas_bytes: &mut [u8; 8]) -> u8 {
         let mut gas_counter = u64::from_le_bytes(*gas_bytes);
-        let res =
-            gear_lazy_pages_host_api::pre_process_memory_accesses(reads, writes, &mut gas_counter);
+        let res = lazy_pages_detail::pre_process_memory_accesses(reads, writes, &mut gas_counter);
         gas_bytes.copy_from_slice(&gas_counter.to_le_bytes());
         res
     }
 
     fn lazy_pages_status() -> (Status,) {
-        gear_lazy_pages_host_api::lazy_pages_status()
+        lazy_pages_detail::lazy_pages_status()
     }
 
     /// Init lazy-pages.
     /// Returns whether initialization was successful.
-    fn init_lazy_pages(ctx: gear_lazy_pages_host_api::LazyPagesInitContext) -> bool {
-        gear_lazy_pages_host_api::init_lazy_pages(ctx, SpIoProgramStorage)
+    fn init_lazy_pages(ctx: LazyPagesInitContext) -> bool {
+        lazy_pages_detail::init_lazy_pages(ctx)
     }
 
     /// Init lazy pages context for current program.
     /// Panic if some goes wrong during initialization.
-    fn init_lazy_pages_for_program(ctx: gear_lazy_pages_host_api::LazyPagesProgramContext) {
-        gear_lazy_pages_host_api::init_lazy_pages_for_program(ctx)
+    fn init_lazy_pages_for_program(ctx: LazyPagesProgramContext) {
+        lazy_pages_detail::init_lazy_pages_for_program(ctx)
     }
 
     /// Mprotect all wasm mem buffer except released pages.
     /// If `protect` argument is true then restrict all accesses to pages,
     /// else allows read and write accesses.
     fn mprotect_lazy_pages(protect: bool) {
-        gear_lazy_pages_host_api::mprotect_lazy_pages(protect)
+        lazy_pages_detail::mprotect_lazy_pages(protect)
     }
 
     fn change_wasm_memory_addr_and_size(addr: Option<HostPointer>, size: Option<u32>) {
-        gear_lazy_pages_host_api::change_wasm_memory_addr_and_size(addr, size)
+        lazy_pages_detail::change_wasm_memory_addr_and_size(addr, size)
     }
 
     fn write_accessed_pages() -> Vec<u32> {
-        gear_lazy_pages_host_api::write_accessed_pages()
+        lazy_pages_detail::write_accessed_pages()
     }
 
     /* Below goes deprecated runtime interface functions. */
@@ -149,6 +212,97 @@ pub trait GearRI {
             Err(ProcessAccessError::GasLimitExceeded) => {
                 (gas_left, Err(ProcessAccessErrorVer1::GasLimitExceeded))
             }
+        }
+    }
+}
+
+#[cfg(feature = "std")]
+pub mod lazy_pages_detail {
+    use super::*;
+
+    pub fn pre_process_memory_accesses(reads: &[u8], writes: &[u8], gas_counter: &mut u64) -> u8 {
+        let mem_interval_size = size_of::<MemoryInterval>();
+        let reads_len = reads.len();
+        let writes_len = writes.len();
+
+        let mut reads_intervals = Vec::with_capacity(reads_len / mem_interval_size);
+        deserialize_mem_intervals(reads, &mut reads_intervals);
+        let mut writes_intervals = Vec::with_capacity(writes_len / mem_interval_size);
+        deserialize_mem_intervals(writes, &mut writes_intervals);
+
+        gear_lazy_pages::pre_process_memory_accesses(
+            &reads_intervals,
+            &writes_intervals,
+            gas_counter,
+        )
+        .map(|_| 0)
+        .unwrap_or_else(|err| err.into())
+    }
+
+    pub fn lazy_pages_status() -> (Status,) {
+        (gear_lazy_pages::status()
+            .unwrap_or_else(|err| unreachable!("Cannot get lazy-pages status: {err}")),)
+    }
+
+    /// Init lazy-pages.
+    /// Returns whether initialization was successful.
+    pub fn init_lazy_pages(ctx: LazyPagesInitContext) -> bool {
+        use gear_lazy_pages::LazyPagesVersion;
+
+        gear_lazy_pages::init(LazyPagesVersion::Version1, ctx.into(), SpIoProgramStorage)
+            .map_err(|err| log::error!("Cannot initialize lazy-pages: {err}"))
+            .is_ok()
+    }
+
+    /// Init lazy pages context for current program.
+    /// Panic if some goes wrong during initialization.
+    pub fn init_lazy_pages_for_program(ctx: LazyPagesProgramContext) {
+        let wasm_mem_addr = ctx.wasm_mem_addr.map(|addr| {
+            usize::try_from(addr)
+                .unwrap_or_else(|err| unreachable!("Cannot cast wasm mem addr to `usize`: {}", err))
+        });
+
+        gear_lazy_pages::initialize_for_program(
+            wasm_mem_addr,
+            ctx.wasm_mem_size,
+            ctx.stack_end,
+            ctx.program_key,
+            Some(ctx.globals_config),
+            ctx.costs,
+        )
+        .map_err(|e| e.to_string())
+        .expect("Cannot initialize lazy pages for current program");
+    }
+
+    /// Mprotect all wasm mem buffer except released pages.
+    /// If `protect` argument is true then restrict all accesses to pages,
+    /// else allows read and write accesses.
+    pub fn mprotect_lazy_pages(protect: bool) {
+        if protect {
+            gear_lazy_pages::set_lazy_pages_protection()
+        } else {
+            gear_lazy_pages::unset_lazy_pages_protection()
+        }
+        .map_err(|err| err.to_string())
+        .expect("Cannot set/unset mprotection for lazy pages");
+    }
+
+    pub fn change_wasm_memory_addr_and_size(addr: Option<HostPointer>, size: Option<u32>) {
+        // `as usize` is safe, because of const assert above.
+        gear_lazy_pages::change_wasm_mem_addr_and_size(addr.map(|addr| addr as usize), size)
+            .unwrap_or_else(|err| unreachable!("Cannot set new wasm addr and size: {err}"));
+    }
+
+    pub fn write_accessed_pages() -> Vec<u32> {
+        gear_lazy_pages::write_accessed_pages()
+            .unwrap_or_else(|err| unreachable!("Cannot get write accessed pages: {err}"))
+    }
+
+    fn deserialize_mem_intervals(bytes: &[u8], intervals: &mut Vec<MemoryInterval>) {
+        let mem_interval_size = size_of::<MemoryInterval>();
+        for chunk in bytes.chunks_exact(mem_interval_size) {
+            // can't panic because of chunks_exact
+            intervals.push(MemoryInterval::try_from_bytes(chunk).unwrap());
         }
     }
 }
