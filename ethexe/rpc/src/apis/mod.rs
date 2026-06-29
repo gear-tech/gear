124d0d1104b2a27d9892bea685dc955e681e6865
Arsenii Lyashenko <arsenii.lyashenko@gmail.com>
2026-06-29 04:04:08 +0300

    Revert rpc/src/apis/mod.rs

--- ethexe/rpc/src/apis/mod.rs
+++ ethexe/rpc/src/apis/mod.rs
@@ -15,7 +15,7 @@ pub use crate::apis::{
     block::BlockClient,
     code::CodeClient,
     dev::DevClient,
-    info::InfoClient,
+    info::{InfoClient, RPC_VERSION},
     injected::InjectedClient,
     program::{CalculateReplyForHandleResult, FullProgramState, ProgramBestState, ProgramClient},
 };
@@ -25,7 +25,6 @@ pub use block::{BlockApi, BlockServer};
 pub use code::{CodeApi, CodeServer};
 #[cfg(feature = "server")]
 pub use dev::{DevApi, DevServer};
-pub use info::RPC_VERSION;
 #[cfg(feature = "server")]
 pub use info::{InfoApi, InfoServer};
 #[cfg(feature = "server")]
